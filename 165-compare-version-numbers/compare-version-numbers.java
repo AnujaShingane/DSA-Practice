d1b73312e0f4caca919f5954f61776df0221f4e7
AnujaShingane <anuja.shingane@cumminscollege.in>
2025-02-15 18:11:37 +0530

    Time: 1 ms (71.14%) | Memory: 41.4 MB (89.68%) - LeetSync

--- 165-compare-version-numbers/compare-version-numbers.java
+++ 165-compare-version-numbers/compare-version-numbers.java
@@ -7,21 +7,35 @@ class Solution {
         List<Integer> v1 = new ArrayList<>();
         List<Integer> v2 = new ArrayList<>();
         
-        for (String part : v1Parts) v1.add(Integer.parseInt(part)); // Convert to int
-        for (String part : v2Parts) v2.add(Integer.parseInt(part)); // Convert to int
+        for (String part : v1Parts) v1.add(Integer.parseInt(part));
+        for (String part : v2Parts) v2.add(Integer.parseInt(part));         
         
         // Find the max length
         int maxLength = Math.max(v1.size(), v2.size());
 
         // Compare each version part
         for (int i = 0; i < maxLength; i++) {
-            int num1 = (i < v1.size()) ? v1.get(i) : 0; // If missing, assume 0
-            int num2 = (i < v2.size()) ? v2.get(i) : 0;
-            
+            // int num1 = (i < v1.size()) ? v1.get(i) : 0;
+            // int num2 = (i < v2.size()) ? v2.get(i) : 0;
+
+            int num1 , num2;
+
+            if(i < v1.size()){
+                num1 = v1.get(i);
+            }else{
+                num1 = 0;
+            }
+
+            if(i < v2.size()){
+                num2 = v2.get(i);
+            }else{
+                num2 = 0;
+            }
+
             if (num1 > num2) return 1;
             if (num1 < num2) return -1;
         }
         
-        return 0; // Both versions are equal
+        return 0;
     }
 }
