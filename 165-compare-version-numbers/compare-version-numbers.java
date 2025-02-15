885bf5852117a185d500080fd637b26dcce3291b
AnujaShingane <anuja.shingane@cumminscollege.in>
2025-02-16 00:08:00 +0530

    Time: 1 ms (71.14%) | Memory: 41.5 MB (78.97%) - LeetSync

--- 165-compare-version-numbers/compare-version-numbers.java
+++ 165-compare-version-numbers/compare-version-numbers.java
@@ -1,41 +1,26 @@
 class Solution {
     public int compareVersion(String version1, String version2) {
-        String[] v1Parts = version1.split("\\.");
-        String[] v2Parts = version2.split("\\.");
-        
-        // Convert to ArrayLists of Integers
-        List<Integer> v1 = new ArrayList<>();
-        List<Integer> v2 = new ArrayList<>();
-        
-        for (String part : v1Parts) v1.add(Integer.parseInt(part));
-        for (String part : v2Parts) v2.add(Integer.parseInt(part));         
-        
-        // Find the max length
-        int maxLength = Math.max(v1.size(), v2.size());
+        String[] v1 = version1.split("\\.");
+        String[] v2 = version2.split("\\.");
 
-        // Compare each version part
-        for (int i = 0; i < maxLength; i++) {
-            // int num1 = (i < v1.size()) ? v1.get(i) : 0;
-            // int num2 = (i < v2.size()) ? v2.get(i) : 0;
+        int maxLength = Math.max(v1.length , v2.length);
 
-            int num1 , num2;
+        for(int i = 0 ; i < maxLength ; i++){
+            int num1, num2;
 
-            if(i < v1.size()){
-                num1 = v1.get(i);
-            }else{
-                num1 = 0;
+            if(i < v1.length){
+                num1 = Integer.parseInt(v1[i]);
+            }else num1=0;
+            if(i < v2.length){
+                num2 = Integer.parseInt(v2[i]);
+            }else num2=0;
+
+            if(num1 < num2){
+                return -1;
+            }else if(num1 > num2){
+                return 1;
             }
-
-            if(i < v2.size()){
-                num2 = v2.get(i);
-            }else{
-                num2 = 0;
-            }
-
-            if (num1 > num2) return 1;
-            if (num1 < num2) return -1;
         }
-        
         return 0;
     }
 }
