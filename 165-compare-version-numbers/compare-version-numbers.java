1c3c56d41ddf17e092735a0708638d958bfd01f7
AnujaShingane <anuja.shingane@cumminscollege.in>
2025-02-16 00:12:41 +0530

    Time: 1 ms (71.14%) | Memory: 41.6 MB (46.44%) - LeetSync

--- 165-compare-version-numbers/compare-version-numbers.java
+++ 165-compare-version-numbers/compare-version-numbers.java
@@ -6,20 +6,11 @@ class Solution {
         int maxLength = Math.max(v1.length , v2.length);
 
         for(int i = 0 ; i < maxLength ; i++){
-            int num1, num2;
-
-            if(i < v1.length){
-                num1 = Integer.parseInt(v1[i]);
-            }else num1=0;
-            if(i < v2.length){
-                num2 = Integer.parseInt(v2[i]);
-            }else num2=0;
-
-            if(num1 < num2){
-                return -1;
-            }else if(num1 > num2){
-                return 1;
-            }
+            int num1 = (i < v1.length) ? Integer.parseInt(v1[i]) : 0;
+            int num2 = (i < v2.length) ? Integer.parseInt(v2[i]) : 0;
+            
+            if(num1 < num2) return -1;
+            if(num1 > num2) return 1;
         }
         return 0;
     }
