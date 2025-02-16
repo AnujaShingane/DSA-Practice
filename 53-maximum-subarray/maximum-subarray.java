ca788496c1bc35205a484f10c610f4c00dc460b1
AnujaShingane <anuja.shingane@cumminscollege.in>
2025-02-16 22:03:47 +0530

    Time: 1 ms (99.51%) | Memory: 57 MB (55.89%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int cs = 0;
+        int ms = Integer.MIN_VALUE;
+
+        for(int num : nums){
+            cs += num;
+            ms = Math.max(ms,cs);
+            if(cs < 0){
+                cs = 0;
+            }
+        }
+        return ms;
+    }
+}
