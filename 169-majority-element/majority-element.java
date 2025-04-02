16bba8fae14c3c040e3a361d620977a647dc155a
AnujaShingane <anuja.shingane@cumminscollege.in>
2025-04-02 21:26:56 +0530

    Time: 12 ms (35.64%) | Memory: 49.7 MB (79.77%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,27 +1,18 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        // Moore's Voting Algorithm
-        int ele = 0, cnt = 0, n = nums.length;
+        HashMap<Integer,Integer> hmap = new HashMap<>();
 
-        for (int i = 0; i < n; i++) {
-            if (cnt == 0) {
-                ele = nums[i];
-                cnt = 1;
-            } else if (nums[i] == ele) {
-                cnt++;
-            } else {
-                cnt--;
+        int n = nums.length/2;
+        
+        for (int num : nums) {
+            hmap.put(num, hmap.getOrDefault(num, 0) + 1);
+        }
+
+        for(int key : hmap.keySet()){
+            if(hmap.get(key) > n){
+                return key;
             }
         }
-
-        // Verify the majority element
-        int cnt1 = 0;
-        for (int num : nums) {
-            if (num == ele) cnt1++;
-        }
-        if (cnt1 > n / 2) {  // Fixing the verification step
-            return ele;
-        }
-        return -1;  // This line is optional since the problem guarantees a majority element.
+        return -1;
     }
 }
