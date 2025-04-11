bea2393c9edb0f63580d4485ed407d24e92478ec
AnujaShingane <anuja.shingane@cumminscollege.in>
2025-04-11 11:29:27 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (46.48%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,5 +1,23 @@
 class Solution {
     public void sortColors(int[] nums) {
-        Arrays.sort(nums);
+        int countZeros = 0;
+        int countOne = 0;
+        int countTwo = 0;
+
+        for(int i = 0 ; i < nums.length ; i++){
+            if(nums[i] == 0) countZeros++;
+            else if(nums[i] == 1) countOne++;
+            else if(nums[i] == 2) countTwo++;
+        }
+
+        for(int i = 0 ; i < countZeros ; i++){
+            nums[i] = 0;
+        }
+        for(int i = countZeros ; i < countZeros+countOne ; i++){
+            nums[i] = 1;
+        }
+        for(int i = countZeros+countOne ; i < nums.length ; i++){
+            nums[i] = 2;
+        }
     }
 }
