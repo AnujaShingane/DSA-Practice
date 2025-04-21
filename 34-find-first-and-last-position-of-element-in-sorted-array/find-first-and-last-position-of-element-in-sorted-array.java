60287be3907c6ddd65e61fa418a3c5d07386f6e4
AnujaShingane <anuja.shingane@cumminscollege.in>
2025-04-22 00:40:13 +0530

    Time: 0 ms (100.00%) | Memory: 46.3 MB (16.80%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -2,16 +2,17 @@ class Solution {
     public int lowerBound(int[] nums, int target){
         int n = nums.length;
         int low = 0;
-        int high = n - 1;
+        int high = n-1;
         int ans = n;
 
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (nums[mid] >= target) {
+        while(low <= high){
+            int mid = low + (high-low)/2;
+
+            if(nums[mid] >= target){
                 ans = mid;
-                high = mid - 1;
-            } else {
-                low = mid + 1;
+                high = mid-1;
+            }else{
+                low = mid+1;
             }
         }
         return ans;
@@ -20,34 +21,34 @@ class Solution {
     public int upperBound(int[] nums, int target){
         int n = nums.length;
         int low = 0;
-        int high = n - 1;
+        int high = n-1;
         int ans = n;
 
-        while (low <= high) {
-            int mid = low + (high - low) / 2;  // Fixed mid calculation
-            if (nums[mid] > target) {
-                ans = mid; // Track last occurrence of target
-                high = mid - 1;
-            } else {
-                low = mid + 1;
+        while(low <= high){
+            int mid = low + (high-low)/2;
+
+            if(nums[mid] > target){
+                ans = mid;
+                high = mid-1;
+            }else{
+                low = mid+1;
             }
         }
         return ans;
     }
 
     public int[] searchRange(int[] nums, int target) {
-        int[] res = new int[2];
         int n = nums.length;
+        int lb = lowerBound(nums,target);
+        int ub = upperBound(nums,target);
 
-        int lb = lowerBound(nums, target);
-        int ub = upperBound(nums, target); 
-        if (lb == n || nums[lb] != target) {
+        int[] res = new int[2];
+        
+        if(lb == n || nums[lb] != target){
             res[0] = -1;
             res[1] = -1;
             return res;
         }
-
-        // int ub = upperBound(nums, target);
 
         res[0] = lb;
         res[1] = ub-1;
