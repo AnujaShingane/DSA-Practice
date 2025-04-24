203dbb75387e5f298ba72f31be51c30ab3e4d452
AnujaShingane <anuja.shingane@cumminscollege.in>
2025-04-24 18:43:52 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (68.36%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -11,14 +11,14 @@ class Solution {
             }
             //left sorted
             if(nums[low] <= nums[mid]){
-                if(nums[low] <= target && nums[mid] >= target){
+                if(nums[low] <= target && target <= nums[mid]){
                     high = mid-1;
                 }else{
                     low = mid+1;
                 }
             }//right sorted
             else{
-                if(nums[mid] <= target && nums[high] >= target){
+                if(nums[mid] <= target && target <= nums[high]){
                     low = mid+1;
                 }else{
                     high = mid-1;
