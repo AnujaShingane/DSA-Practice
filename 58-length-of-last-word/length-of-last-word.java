9785208479d35646dfc9d7e272169d7fcb970c22
AnujaShingane <anuja.shingane@cumminscollege.in>
2025-04-26 12:42:14 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (47.77%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -1,7 +1,16 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        String[] str = s.trim().split("\\s+");
+        int length = 0;
+        int i = s.length()-1;
 
-        return str[str.length-1].length();
+        while(i >= 0 && s.charAt(i) == ' '){
+            i--;
+        }
+
+        while(i >= 0 && s.charAt(i) != ' '){
+            length++;
+            i--;
+        }
+        return length;
     }
 }
