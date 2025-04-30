594a27a001d8fdbb531b8c97527e0b1f9be60c03
AnujaShingane <anuja.shingane@cumminscollege.in>
2025-04-30 22:18:56 +0530

    Time: 1 ms (100.00%) | Memory: 42.6 MB (49.22%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -1,30 +1,30 @@
 class Solution {
     public int myAtoi(String s) {
         s = s.trim();
-        if (s.isEmpty()) return 0;
-
-        int sign = 1;
+        if(s.isEmpty()) return 0;
         int start = 0;
-        long result = 0;
+        long ans = 0;
+        int sign = 1;
 
-        if (s.charAt(0) == '-') {
+        if(s.charAt(0) == '-'){
             sign = -1;
             start = 1;
-        } else if (s.charAt(0) == '+') {
+        }else if(s.charAt(0) == '+'){
+            sign = 1;
             start = 1;
         }
 
-        for (int i = start; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            if (ch < '0' || ch > '9') break;
+        for(int i = start ; i < s.length() ; i++){
+            int ch = s.charAt(i);
+            if(ch < '0' || ch > '9') break;
 
-            int num = ch - '0';
-            result = result * 10 + num;
+            int num = ch-'0';
+            ans = ans * 10 + num;
 
-            if (sign * result > Integer.MAX_VALUE) return Integer.MAX_VALUE;
-            if (sign * result < Integer.MIN_VALUE) return Integer.MIN_VALUE;
+            if(sign*ans < Integer.MIN_VALUE) return Integer.MIN_VALUE;
+            if(sign*ans > Integer.MAX_VALUE) return Integer.MAX_VALUE;
         }
 
-        return (int)(sign * result);
+        return (int)(ans*sign);
     }
 }
