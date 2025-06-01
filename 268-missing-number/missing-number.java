511acde9905ebd32c8e74660b003c5a4d405f095
AnujaShingane <anuja.shingane@cumminscollege.in>
2025-06-01 11:17:48 +0530

    Time: 5 ms (25.56%) | Memory: 45.7 MB (21.80%) - LeetSync

---
+++ 268-missing-number/missing-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums);
+        if(nums[0]==1)return 0;
+        for(int i = 0 ; i < n-1 ; i++){
+            if(nums[i+1] != nums[i]+1){
+                return nums[i]+1;
+            }
+        }
+        return nums[n-1]+1;
+    }
+}
