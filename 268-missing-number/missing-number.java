a21121f48e60c1ccf2c62d02579707717fac95d0
AnujaShingane <anuja.shingane@cumminscollege.in>
2025-06-01 11:27:20 +0530

    Time: 0 ms (100.00%) | Memory: 45 MB (92.98%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -1,13 +1,23 @@
 class Solution {
-    public int missingNumber(int[] nums) {
-        int n = nums.length;
-        Arrays.sort(nums);
-        if(nums[0]==1)return 0;
-        for(int i = 0 ; i < n-1 ; i++){
-            if(nums[i+1] != nums[i]+1){
-                return nums[i]+1;
-            }
+    public int missingNumber(int[] arr) {
+        // int n = nums.length;
+        // int sum = (n-1)*(n-1+1)/2;
+        // int count =  0;
+
+        // for(int i = 0 ; i < n ; i++){
+        //     count += nums[i];
+        // }
+        // Solution 2: Time Complexity: O(N) & Space Complexity: O(1)
+        int sum = 0;
+        int N = arr.length;
+        // calculating sum of all elements of the array.
+        for(int i = 0; i < arr.length; i++){
+            sum += arr[i];
         }
-        return nums[n-1]+1;
+        // calculation sum till 0 to N - 1
+        int sum2 = (N * (N + 1))/2;
+        // We will get our ans by subtracting
+        int ans = sum2 - sum;
+        return ans;
     }
 }
