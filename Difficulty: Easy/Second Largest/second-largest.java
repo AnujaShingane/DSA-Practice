9f13bf59bccf32dba2d5a2670df83b96b167bf57
AnujaShingane <anuja.shingane@cumminscollege.in>
2025-06-02 12:16:58 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Second Largest/second-largest.java
+++ Difficulty: Easy/Second Largest/second-largest.java
@@ -1,45 +1,19 @@
-//{ Driver Code Starts
-// Initial Template for Java
-import java.io.*;
-import java.util.*;
-import java.util.stream.Collectors;
-
-public class Main {
-
-    public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
-        int t = Integer.parseInt(sc.nextLine());
-        while (t-- > 0) {
-            String[] arr1Str = sc.nextLine().split(" ");
-            int[] arr = Arrays.stream(arr1Str).mapToInt(Integer::parseInt).toArray();
-            Solution ob = new Solution();
-            int ans = ob.getSecondLargest(arr);
-            System.out.println(ans);
-
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
-// User function Template for Java
-
 class Solution {
     public int getSecondLargest(int[] arr) {
-        int max = Integer.MIN_VALUE;
-        for(int i = 0; i < arr.length; i++){
-            if(max < arr[i]){
-                max = arr[i];
+        PriorityQueue<Integer> pq = new PriorityQueue<>(); // Min-heap by default
+        HashSet<Integer> set = new HashSet<>();
+        // for(int i = 0 ; i < n ; i ++){
+        //     set.add(arr[i]);
+        // }
+
+        for(int i = 0; i < arr.length; i++) {
+            if(!set.contains(arr[i])) pq.add(arr[i]);               // Add every element
+            set.add(arr[i]);
+            if(pq.size() > 2) {
+                pq.remove();              // Remove the smallest element when size > k
             }
         }
-        int secondMax = -1;
-        for(int i = 0; i < arr.length; i++){
-            if(secondMax < arr[i] && arr[i] < max){
-                secondMax = arr[i];
-            }
-        }
-        return secondMax;
+        if(pq.size()<2) return -1;
+        return pq.peek(); // kth largest element will be at the top
     }
 }
