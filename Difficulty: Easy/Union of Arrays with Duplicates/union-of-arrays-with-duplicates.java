3cfd9662b74f7f3e9aff2bdd46c53d5b84f5c123
AnujaShingane <anuja.shingane@cumminscollege.in>
2025-06-02 17:18:17 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Union of Arrays with Duplicates/union-of-arrays-with-duplicates.java
+++ Difficulty: Easy/Union of Arrays with Duplicates/union-of-arrays-with-duplicates.java
@@ -1,16 +1,42 @@
 // User function Template for Java
 
 class Solution {
-    public static int findUnion(int a[], int b[]) {
-        HashSet<Integer> set = new HashSet<>();
-        
-        for(int i = 0 ; i < a.length ; i++){
-            set.add(a[i]);
+    public static int findUnion(int a[], int b[]){
+        Arrays.sort(a);
+        Arrays.sort(b);
+        int n = a.length;
+        int m = b.length;
+        ArrayList<Integer> list = new ArrayList<>();
+        int i = 0 , j = 0;
+
+        while(i < n && j < m){
+            if(a[i] <= b[j]){
+                if(list.isEmpty() || list.get(list.size()-1) != a[i]){
+                    list.add(a[i]);
+                }
+                i++;
+            }else if(a[i] > b[j]){
+                if(list.isEmpty() || list.get(list.size()-1) != b[j]){
+                    list.add(b[j]);
+                }
+                j++;
+            }
         }
-        for(int i = 0 ; i < b.length ; i++){
-            set.add(b[i]);
+        
+        while(i < n){
+            if(list.isEmpty() || list.get(list.size()-1) != a[i]){
+                list.add(a[i]);
+            }
+            i++;
         }
         
-        return set.size();
+        while(j < m){
+            if(list.isEmpty() || list.get(list.size()-1) != b[j]){
+                list.add(b[j]);
+            }
+            j++;
+        }
+        
+        return list.size();
     }
 }
