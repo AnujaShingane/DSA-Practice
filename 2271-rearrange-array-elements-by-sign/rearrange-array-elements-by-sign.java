56e0eb71a753ee1cafc11ba0158016ee6284c53d
AnujaShingane <anuja.shingane@cumminscollege.in>
2025-06-05 13:53:57 +0530

    Time: 10 ms (15.04%) | Memory: 83.4 MB (14.21%) - LeetSync

---
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] rearrangeArray(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+        ArrayList<Integer> pos = new ArrayList<>();
+        ArrayList<Integer> neg = new ArrayList<>();
+
+        for(int i = 0 ; i < n ; i++){
+            if(nums[i] >= 0){
+                pos.add(nums[i]);
+            }else{
+                neg.add(nums[i]);
+            }
+        }
+
+        for (int i = 0; i < n / 2; i++) {
+            res[2*i] = pos.get(i);     // even index -> positive
+            res[2*i + 1] = neg.get(i); // odd index -> negative
+        }
+
+        return res;
+    }
+}
