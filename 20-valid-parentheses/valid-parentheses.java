594ebd0f6c0f0ddbb7840f6ae40cd5c1c5219330
AnujaShingane <anuja.shingane@cumminscollege.in>
2025-06-06 19:22:02 +0530

    Time: 3 ms (41.07%) | Memory: 42 MB (32.03%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -2,24 +2,24 @@ class Solution {
     public boolean isValid(String s) {
         Stack<Character> st = new Stack<>();
 
-        for(char ch : s.toCharArray()){
-            if(ch == '{' || ch == '(' || ch == '['){
-                st.push(ch);
+        for(int i = 0 ; i < s.length() ; i++){
+            if(s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '['){
+                st.push(s.charAt(i));
             }else{
-                if(st.isEmpty()){
-                    return false;
-                }else{
-                    char pop = st.pop();
-                    if(
-                        ((ch == '}' && pop != '{')||
-                        (ch == ')' && pop != '(')||
-                        (ch == ']' && pop != '[')) 
-                    ){
-                        return false;
-                    }
+                if(st.isEmpty()) return false;
+                if(s.charAt(i) == ')'){
+                    if(st.peek() == '(')st.pop();
+                    else return false;
+                }if(s.charAt(i) == ']'){
+                    if(st.peek() == '[')st.pop();
+                    else return false;
+                }if(s.charAt(i) == '}'){
+                    if(st.peek() == '{')st.pop();
+                    else return false;
                 }
             }
         }
-        return st.isEmpty();
+        if(st.isEmpty())return true;
+        return false;
     }
 }
