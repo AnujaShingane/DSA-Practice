991c15cc50580b51c6fefbb59eca0b4a339172b2
AnujaShingane <anuja.shingane@cumminscollege.in>
2025-06-07 09:11:04 +0530

    Time: 55 ms (16.75%) | Memory: 114.7 MB (53.25%) - LeetSync

--- 146-lru-cache/lru-cache.java
+++ 146-lru-cache/lru-cache.java
@@ -1,54 +1,64 @@
-class Node {
-        int key, value;
-        Node prev, next;
-        Node(int key, int value) {
-            this.key = key;
-            this.value = value;
-        }
-    }
-    
-class LRUCache {
-    Node head = new Node(-1, -1), tail = new Node(-1, -1);
-    Map<Integer, Node> map = new HashMap<>();
-    int capacity;
+class Node{
+    int key,value;
+    Node prev,next;
 
+    Node(int key, int value){
+        this.key = key;
+        this.value = value;
+    }
+}
+
+class LRUCache {
+    Node head = new Node(-1,-1);
+    Node tail = new Node(-1,-1);
+    Map<Integer,Node> map = new HashMap<>();
+    int capacity;
+    
     public LRUCache(int capacity) {
         this.capacity = capacity;
         head.next = tail;
         tail.prev = head;
     }
-
+    
     public int get(int key) {
-        if (map.containsKey(key)) {
+        if(map.containsKey(key)){
             Node node = map.get(key);
-            remove(node);
-            insert(node);
+            remove(map.get(key));
+            insertAfterHead(node);
             return node.value;
         }
         return -1;
     }
-
+    
     public void put(int key, int value) {
-        if (map.containsKey(key)) {
+        Node node = new Node(key,value);
+        if(map.containsKey(key)){
             remove(map.get(key));
         }
-        if (map.size() == capacity) {
+        if(map.size() == capacity){
             remove(tail.prev);
         }
-        insert(new Node(key, value));
+        insertAfterHead(node);
     }
 
-    private void remove(Node node) {
+    void remove(Node node){
         map.remove(node.key);
         node.prev.next = node.next;
         node.next.prev = node.prev;
     }
 
-    private void insert(Node node) {
-        map.put(node.key, node);
+    void insertAfterHead(Node node){
+        map.put(node.key,node);
         node.next = head.next;
-        node.prev = head;
         head.next.prev = node;
+        node.prev = head;
         head.next = node;
     }
 }
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
