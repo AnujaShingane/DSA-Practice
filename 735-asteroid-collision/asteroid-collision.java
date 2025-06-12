6fef13d777af0e40f1516a9b2274809984fca99b
AnujaShingane <anuja.shingane@cumminscollege.in>
2025-06-12 10:36:51 +0530

    Time: 8 ms (17.55%) | Memory: 45.3 MB (72.19%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.java
+++ 735-asteroid-collision/asteroid-collision.java
@@ -5,13 +5,14 @@ class Solution {
 
         for(int i = 0 ; i < n ; i++){
             int val = asteroids[i];
+
             if(val > 0){
                 st.push(val);
             }else{
-                while(!st.isEmpty() && st.peek() > 0 && Math.abs(val) > st.peek()){
+                while(!st.isEmpty() && st.peek() > 0 && st.peek() < -val){
                     st.pop();
                 }
-                if(!st.isEmpty() && st.peek() > 0 && Math.abs(val) == st.peek()){
+                if(!st.isEmpty() && st.peek() > 0 && st.peek() == -val){
                     st.pop();
                 }else if(st.isEmpty() || st.peek() < 0){
                     st.push(val);
@@ -19,10 +20,10 @@ class Solution {
             }
         }
 
-        int[] ans = new int[st.size()];
-        for(int i = st.size() -1 ; i >= 0 ; i--){
-            ans[i] = st.pop();
+        int[] res = new int[st.size()];
+        for(int i = st.size()-1 ; i >= 0 ; i--){
+            res[i] = st.pop();
         }
-        return ans;
+        return res;
     }
 }
