d83ccfdabef8cc265b6985d5a89698e353509dc1
AnujaShingane <anuja.shingane@cumminscollege.in>
2025-07-05 23:42:09 +0530

    Time: 469 ms (32.44%) | Memory: 0B (100.00%) - LeetSync

--- 177-nth-highest-salary/nth-highest-salary.sql
+++ 177-nth-highest-salary/nth-highest-salary.sql
@@ -1,17 +1,40 @@
 CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
-DETERMINISTIC
 BEGIN
-  DECLARE result INT;
-  DECLARE offset_val INT;
-
-  SET offset_val = N - 1;
-
-  SET result = (
-      SELECT DISTINCT salary
-      FROM Employee
-      ORDER BY salary DESC
-      LIMIT 1 OFFSET offset_val
+    DECLARE offset1 INT;
+    SET offset1 = N-1;
+  RETURN (
+        SELECT DISTINCT salary
+        FROM Employee
+        ORDER BY salary DESC
+        LIMIT 1 OFFSET offset1
   );
+END
 
-  RETURN result;
-END;
+-- CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+-- BEGIN
+
+-- SET N=N-1;
+
+--   RETURN (
+--       SELECT DISTINCT salary FROM Employee ORDER BY salary DESC 
+--       LIMIT 1 OFFSET N      
+--   );
+-- END
+
+-- CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+-- DETERMINISTIC
+-- BEGIN
+--   DECLARE result INT;
+--   DECLARE offset_val INT;
+
+--   SET offset_val = N - 1;
+
+--   SET result = (
+--       SELECT DISTINCT salary
+--       FROM Employee
+--       ORDER BY salary DESC
+--       LIMIT 1 OFFSET offset_val
+--   );
+
+--   RETURN result;
+-- END;
