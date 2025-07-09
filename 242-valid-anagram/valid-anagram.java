19c5f358de1db14ab2ab2a5efca9ec52ca029373
AnujaShingane <anuja.shingane@cumminscollege.in>
2025-07-10 00:54:33 +0530

    Time: 24 ms (7.95%) | Memory: 45 MB (17.18%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -7,7 +7,7 @@ class Solution {
             smap.put(s.charAt(i),smap.getOrDefault(s.charAt(i),0)+1);
         }
         for(int i = 0 ; i < t.length() ; i++){
-            tmap.put(t.charAt(i),tmap.getOrDefault(t.charAt(i),0)+1);
+            tmap.put(t.charAt(i),tmap.getOrDefault(t.charAt(i),0)+1); 
         }
         
         if(smap.equals(tmap))return true;
