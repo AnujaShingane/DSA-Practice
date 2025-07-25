09fd5cd7596c2875d3e1a4d47f1f848185686561
AnujaShingane <anuja.shingane@cumminscollege.in>
2025-07-25 18:40:20 +0530

    Time: 32 ms (30.38%) | Memory: 45.4 MB (47.60%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
@@ -6,7 +6,7 @@ class Solution {
             map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
         }
 
-        for(int i = 0 ; i < s.length(); i++){
+        for(int i = 0 ; i < s.length() ; i++){
             if(map.get(s.charAt(i)) == 1){
                 return i;
             }
