908506f600751ff6a19bc10e178af5daedd4c6bd
AnujaShingane <anuja.shingane@cumminscollege.in>
2025-07-26 16:50:35 +0530

    Time: 13 ms (76.48%) | Memory: 45.6 MB (88.77%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,37 +1,24 @@
-class Pair implements Comparable<Pair>{
-    int key;
-    int value;
-
-    Pair(int key , int value){
-        this.key = key;
-        this.value = value;
-    }
-
-    @Override
-    public int compareTo(Pair o){
-        return this.value-o.value;
-    }
-}
-
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         int n = nums.length;
-        HashMap<Integer, Integer> map = new HashMap<>();
+
+        HashMap<Integer,Integer> map = new HashMap<>();
         for(int i = 0 ; i < n ; i++){
-            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+            map.put(nums[i], map.getOrDefault(nums[i],0) + 1);
         }
 
-        PriorityQueue<Pair> pq = new PriorityQueue<>();
-        for(int key : map.keySet()){
-            pq.add(new Pair(key,map.get(key)));
-             if(pq.size() > k){
-                pq.remove();
-            }
-        }
+        PriorityQueue<Map.Entry<Integer,Integer>> pq = new PriorityQueue<>(
+            (a,b) -> b.getValue() - a.getValue()
+        );
+
+        pq.addAll(map.entrySet());
 
         int[] res = new int[k];
-        for(int i = 0 ; i < k ; i++){
-            res[i] = pq.remove().key;
+        int ind = 0;
+        while(k != 0){
+            Map.Entry<Integer,Integer> entry = pq.poll();
+            res[ind++] = entry.getKey();
+            k--;
         }
         return res;
     }
