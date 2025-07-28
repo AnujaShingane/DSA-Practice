bb68a832beee1a1d8894059b3fe796e08cc7cab9
AnujaShingane <anuja.shingane@cumminscollege.in>
2025-07-28 17:45:41 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (46.29%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -2,18 +2,18 @@ class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
         List<Integer> list = new ArrayList<>();
-        subsequences(0,nums,list,res);
+        subsequences(0,list,nums,res);
         return res;
     }
 
-    public void subsequences(int ind , int[] nums , List<Integer> list,List<List<Integer>> res) {
-        if(ind == nums.length){
+    public void subsequences(int ind , List<Integer> list , int[] nums , List<List<Integer>> res) {
+        if(ind >= nums.length){
             res.add(new ArrayList<>(list));
             return;
         }
-        list.add(nums[ind]);
-        subsequences(ind+1, nums, list,res);
+        list.add(nums[ind]); // take a element
+        subsequences(ind+1,list,nums,res);
         list.remove(list.size()-1);
-        subsequences(ind+1, nums, list,res);
+        subsequences(ind+1,list,nums,res);
     }
 }
