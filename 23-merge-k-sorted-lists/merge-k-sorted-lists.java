28f9334f2441195ce6d752284944c98f4eacbe2b
AnujaShingane <anuja.shingane@cumminscollege.in>
2025-07-29 00:55:40 +0530

    Time: 4 ms (78.34%) | Memory: 44.9 MB (20.06%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -1,35 +1,24 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        PriorityQueue<ListNode> pq = new PriorityQueue<>((a,b) -> a.val-b.val);
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
 
-        for(ListNode node : lists){
-            if(node != null){
-                pq.add(node);
+        // Step 1: Traverse each linked list and add all node values into the min-heap
+        for (ListNode head : lists) {
+            while (head != null) {
+                pq.add(head.val);
+                head = head.next;
             }
         }
 
-        ListNode dummy = new ListNode(-1);
-        ListNode curr = dummy;
+        // Step 2: Build the merged linked list from min-heap
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
 
-        while(!pq.isEmpty()){
-            ListNode rem = pq.remove();
-            curr.next = rem;
-            curr = curr.next;
-
-            if(rem.next != null){
-                pq.add(rem.next);
-            }
+        while (!pq.isEmpty()) {
+            current.next = new ListNode(pq.poll());
+            current = current.next;
         }
+
         return dummy.next;
     }
 }
