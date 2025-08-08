c39e730701e0a7be61debbb593d5adbe4b689cbb
AnujaShingane <anuja.shingane@cumminscollege.in>
2025-08-08 13:30:07 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Tree Boundary Traversal/tree-boundary-traversal.java
+++ Difficulty: Medium/Tree Boundary Traversal/tree-boundary-traversal.java
@@ -1,91 +1,3 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-class Node {
-    int data;
-    Node left, right;
-
-    public Node(int d) {
-        data = d;
-        left = right = null;
-    }
-}
-
-class GFG {
-    static Node buildTree(String str) {
-        // Corner Case
-        if (str.length() == 0 || str.equals('N')) return null;
-        String[] s = str.split(" ");
-
-        Node root = new Node(Integer.parseInt(s[0]));
-        Queue<Node> q = new LinkedList<Node>();
-        q.add(root);
-
-        // Starting from the second element
-        int i = 1;
-        while (!q.isEmpty() && i < s.length) {
-            // Get and remove the front of the queue
-            Node currNode = q.remove();
-
-            // Get the current node's value from the string
-            String currVal = s[i];
-
-            // If the left child is not null
-            if (!currVal.equals("N")) {
-
-                // Create the left child for the current node
-                currNode.left = new Node(Integer.parseInt(currVal));
-
-                // Push it to the queue
-                q.add(currNode.left);
-            }
-
-            // For the right child
-            i++;
-            if (i >= s.length) break;
-            currVal = s[i];
-
-            // If the right child is not null
-            if (!currVal.equals("N")) {
-
-                // Create the right child for the current node
-                currNode.right = new Node(Integer.parseInt(currVal));
-
-                // Push it to the queue
-                q.add(currNode.right);
-            }
-
-            i++;
-        }
-
-        return root;
-    }
-
-    public static void main(String args[]) throws IOException {
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine().trim());
-        while (t > 0) {
-            String s = br.readLine();
-            Node root = buildTree(s);
-
-            Solution T = new Solution();
-
-            ArrayList<Integer> res = T.boundaryTraversal(root);
-            for (Integer num : res) System.out.print(num + " ");
-            System.out.println();
-            t--;
-
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
-// User function Template for Java
 /*
 class Node
 {
@@ -101,48 +13,60 @@ class Node
 */
 
 class Solution {
-    public static boolean isLeaf(Node node) {
-        return node.left == null && node.right == null;
+    ArrayList<Integer> boundaryTraversal(Node node) {
+        ArrayList<Integer> res = new ArrayList<>();
+        if (node == null) return res;
+
+        if (!isLeaf(node)) res.add(node.data);
+
+        // 1. Left boundary (excluding leaves)
+        addLeftBoundary(node.left, res);
+
+        // 2. Leaf nodes
+        addLeaves(node, res);
+
+        // 3. Right boundary (excluding leaves, reversed)
+        addRightBoundary(node.right, res);
+
+        return res;
     }
 
-    public static void addLeftBoundry(Node root, ArrayList<Integer> res) {
-        Node curr = root.left;
-        while (curr != null) {
-            if (!isLeaf(curr)) res.add(curr.data);
-            if (curr.left != null) curr = curr.left;
-            else curr = curr.right;
+    private boolean isLeaf(Node node) {
+        return node != null && node.left == null && node.right == null;
+    }
+
+    private void addLeftBoundary(Node node, ArrayList<Integer> res) {
+        while (node != null) {
+            if (!isLeaf(node)) res.add(node.data);
+            if (node.left != null)
+                node = node.left;
+            else
+                node = node.right;
         }
     }
 
-    public static void addRightBoundry(Node root, ArrayList<Integer> res) {        
-        ArrayList<Integer> temp = new ArrayList<>();
-        Node curr = root.right;
-        while (curr != null) {
-            if (!isLeaf(curr)) temp.add(curr.data);
-            if (curr.right != null) curr = curr.right;
-            else curr = curr.left;
-        }
-        for (int i = temp.size() - 1; i >= 0; i--) {
-            res.add(temp.get(i));
-        }
-    }
-
-    public static void addLeaves(Node root, ArrayList<Integer> res) {
-        if (isLeaf(root)) {
-            res.add(root.data);
+    private void addLeaves(Node node, ArrayList<Integer> res) {
+        if (node == null) return;
+        if (isLeaf(node)) {
+            res.add(node.data);
             return;
         }
-        if (root.left != null) addLeaves(root.left, res);
-        if (root.right != null) addLeaves(root.right, res);
+        addLeaves(node.left, res);
+        addLeaves(node.right, res);
     }
-    
-    ArrayList<Integer> boundaryTraversal(Node node) {
-        ArrayList<Integer> ans = new ArrayList<>();
-        if (isLeaf(node) == false) ans.add(node.data);
-        addLeftBoundry(node, ans);
-        addLeaves(node, ans);
-        addRightBoundry(node, ans);
-        return ans;
+
+    private void addRightBoundary(Node node, ArrayList<Integer> res) {
+        Stack<Integer> stack = new Stack<>();
+        while (node != null) {
+            if (!isLeaf(node)) stack.push(node.data);
+            if (node.right != null)
+                node = node.right;
+            else
+                node = node.left;
+        }
+
+        while (!stack.isEmpty()) {
+            res.add(stack.pop());
+        }
     }
 }
-
