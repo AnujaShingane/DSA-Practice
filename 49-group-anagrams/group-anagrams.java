34317c2b3471185491bde43c43b62a28d275e7b9
AnujaShingane <anuja.shingane@cumminscollege.in>
2025-08-14 23:01:44 +0530

    Time: 7 ms (64.67%) | Memory: 48.1 MB (43.31%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,35 +1,20 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        List<List<String>> list = new ArrayList<>();
+        List<List<String>> res = new ArrayList<>();
+        HashMap<String,List<String>> map = new HashMap<>();
 
-        for (int i = 0; i < strs.length; i++) {
-            if (strs[i] == null) continue;  // skip already grouped strings
+        for(String str : strs){
+            char[] ch = str.toCharArray();
+            Arrays.sort(ch);
+            String str1 = new String(ch);
 
-            List<String> sublist = new ArrayList<>();
-            sublist.add(strs[i]);  // always add the current string
-
-            for (int j = i + 1; j < strs.length; j++) {
-                if (strs[j] != null && isAnagram(strs[i], strs[j])) {
-                    sublist.add(strs[j]);
-                    strs[j] = null;  // mark it as grouped
-                }
-            }
-            list.add(sublist);
+            map.putIfAbsent(str1,new ArrayList<>());
+            map.get(str1).add(str);
         }
-        return list;
-    }
 
-    public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) return false;
-
-        HashMap<Character, Integer> smap = new HashMap<>();
-        HashMap<Character, Integer> tmap = new HashMap<>();
-
-        for (int i = 0; i < s.length(); i++) {
-            smap.put(s.charAt(i), smap.getOrDefault(s.charAt(i), 0) + 1);
-            tmap.put(t.charAt(i), tmap.getOrDefault(t.charAt(i), 0) + 1);
+        for(List<String> list : map.values()){
+            res.add(list);
         }
-        
-        return smap.equals(tmap);
+        return res;
     }
 }
