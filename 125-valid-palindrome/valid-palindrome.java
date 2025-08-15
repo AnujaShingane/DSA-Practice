77733a5848e32583407a5d1ece0700e6db51ff5c
AnujaShingane <anuja.shingane@cumminscollege.in>
2025-08-15 10:02:11 +0530

    Time: 17 ms (13.00%) | Memory: 45.3 MB (19.56%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,13 +1,14 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        String str = s.replaceAll("\\s+", "");
-        str = str.replaceAll("[^a-zA-Z0-9]", "");
-        str = str.toLowerCase();
-
+        s = s.trim();
+        s = s.replaceAll("\\s+","");
+        s = s.replaceAll("[^a-zA-Z0-9]","");
+        s = s.toLowerCase();
         int i = 0;
-        int j = str.length() - 1;
+        int j = s.length()-1;
+
         while(i < j){
-            if(str.charAt(i) != str.charAt(j)) return false;
+            if(s.charAt(i) != s.charAt(j))return false;
             i++;
             j--;
         }
