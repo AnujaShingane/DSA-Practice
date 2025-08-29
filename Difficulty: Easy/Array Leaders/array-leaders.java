f23003aeaa4012a01aca0567339c7279f672368c
AnujaShingane <anuja.shingane@cumminscollege.in>
2025-08-29 13:39:30 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Array Leaders/array-leaders.java
+++ Difficulty: Easy/Array Leaders/array-leaders.java
@@ -1,6 +1,15 @@
 class Solution {
     static ArrayList<Integer> leaders(int arr[]) {
+        int n = arr.length;
         // code here
-        
+        ArrayList<Integer> res = new ArrayList<>();
+        res.add(arr[n-1]);
+        for(int i = n-2 ; i >= 0 ; i--){
+            if(res.get(res.size()-1) <= arr[i]){
+                res.add(arr[i]);
+            }
+        }
+        Collections.reverse(res);
+        return res;
     }
 }
