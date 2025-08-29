ad7e9bb2fb404a1c1517e7d66ee652b18f433263
AnujaShingane <anuja.shingane@cumminscollege.in>
2025-08-29 17:03:00 +0530

    Time: 1 ms (99.51%) | Memory: 57.2 MB (31.21%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -1,40 +1,13 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int maxSum = Integer.MIN_VALUE;
-        int currSum = 0;
+        int n = nums.length;
+        int currSum = nums[0];
+        int maxSum = nums[0];
 
-        for(int i = 0 ; i < nums.length ; i++){
-            currSum += nums[i];
-            if(currSum > maxSum){
-                maxSum = Math.max(maxSum,currSum);
-            }
-            if(currSum < 0){
-                currSum = 0;
-            }
+        for(int i = 1 ; i < n ; i++){
+            currSum = Math.max(nums[i],nums[i]+currSum);
+            maxSum = Math.max(currSum,maxSum);
         }
         return maxSum;
     }
 }
-
-
-//Moore's Voting Algorithm
-
-// class Solution {
-//     public int majorityElement(int[] nums) {
-//         int n = nums.length;
-//         int element = 0, count = 0;
-
-//         for(int i = 0 ; i < n ; i++){
-//             if(count == 0){
-//                 element = nums[i];
-//                 count = 1;
-//             }else if(nums[i] != element){
-//                 count--;
-//             }else{
-//                 count++;
-//             }
-//         }
-
-//         return element; 
-//     }
-// }
