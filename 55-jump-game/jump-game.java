433075a1075ea1e417cbe1cde6eb76ce5ea26aa8
AnujaShingane <anuja.shingane@cumminscollege.in>
2025-08-30 17:40:30 +0530

    Time: 2 ms (83.65%) | Memory: 45.7 MB (56.18%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,26 +1,11 @@
 class Solution {
     public boolean canJump(int[] nums) {
         int n = nums.length;
-        int maxInd = 0;
-        
-        if(nums.length == 1 && nums[0] == 0)return true;
-        if(nums[0] == 0)return false;
-
-        boolean allPos = true;
-        for(int num : nums){
-            if(num == 0){
-                allPos = false;
-                break;
-            }
-        }
-        if(allPos == true)return true;
-
+        int maxLen = 0;
         for(int i = 0 ; i < n ; i++){
-            if(i > maxInd){
-                return false;
-            }
-            maxInd = Math.max(maxInd,nums[i]+i);
-            if(maxInd >= n)return true;
+            if(i > maxLen)return false;
+            maxLen = Math.max(maxLen,i+nums[i]);
+            if(maxLen >= nums.length)return true;
         }
         return true;
     }
