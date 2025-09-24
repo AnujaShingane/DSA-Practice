255cce4875612cb8ab25fa676a4ddc37a66f17d7
AnujaShingane <anuja.shingane@cumminscollege.in>
2025-09-25 00:55:29 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (43.33%) - LeetSync

---
+++ 780-max-chunks-to-make-sorted/max-chunks-to-make-sorted.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxChunksToSorted(int[] arr) {
+        int n = arr.length;
+        int[] sorted = arr.clone();
+        Arrays.sort(sorted);
+        int chunks = 0;
+        int sum1 = 0, sum2 = 0;
+
+        for(int i = 0 ; i < n ; i++){
+            sum1 += arr[i];
+            sum2 += sorted[i];
+
+            if(sum1 == sum2){
+                chunks++;
+            }
+        }
+        return chunks;
+    }
+}
