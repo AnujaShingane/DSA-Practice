15176e56712039047f1416d0e16295f5070d4b0a
AnujaShingane <anuja.shingane@cumminscollege.in>
2025-09-25 15:03:41 +0530

    Time: 1 ms (100.00%) | Memory: 56 MB (98.55%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.java
+++ 41-first-missing-positive/first-missing-positive.java
@@ -1,28 +1,21 @@
-import java.util.*;
-
 class Solution {
     public int firstMissingPositive(int[] nums) {
         int n = nums.length;
+        
+        int[] arr = new int[n+1];
+        Arrays.fill(arr,-1);
 
-        // Step 1: Make a presence array only of size n+1
-        int[] present = new int[n + 1];
-        Arrays.fill(present, -1);
-
-        // Step 2: Mark valid positives in range [1..n]
-        for (int num : nums) {
-            if (num > 0 && num <= n) {
-                present[num] = num;
+        for(int num : nums){
+            if(num > 0 && num <= n){
+                arr[num] = num;
             }
         }
 
-        // Step 3: Find the first missing positive
-        for (int i = 1; i <= n; i++) {
-            if (present[i] == -1) {
+        for(int i = 1 ; i <= n ; i++){
+            if(arr[i] == -1){
                 return i;
             }
         }
-
-        // Step 4: If all present, answer is n+1
-        return n + 1;
+        return n+1;
     }
 }
