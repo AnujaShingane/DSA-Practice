08c147e81a6d570f4c0f6208233ba614591b555f
AnujaShingane <anuja.shingane@cumminscollege.in>
2025-09-29 23:35:29 +0530

    Time: 3 ms (60.33%) | Memory: 44.7 MB (84.06%) - LeetSync

--- 383-ransom-note/ransom-note.java
+++ 383-ransom-note/ransom-note.java
@@ -1,19 +1,17 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        int[] ransomNote1 = new int[26];
-        int[] magazine1 = new int[26];
-        Arrays.fill(ransomNote1,0);
-        Arrays.fill(magazine1,0);
+        int[] ransomNot = new int[26];
+        int[] magazin = new int[26];
         
         for(int i = 0 ; i < ransomNote.length() ; i++){
-            ransomNote1[ransomNote.charAt(i) - 'a']++;
+            ransomNot[ransomNote.charAt(i)-'a']++;
         }
         for(int i = 0 ; i < magazine.length() ; i++){
-            magazine1[magazine.charAt(i) - 'a']++;
+            magazin[magazine.charAt(i)-'a']++;
         }
 
-        for(int i = 0 ; i < 26 ; i++){
-            if(ransomNote1[i]>magazine1[i])return false;
+        for(int i = 0 ; i < 26 ;i++){
+            if(ransomNot[i]>magazin[i])return false;
         }
         return true;
     }
