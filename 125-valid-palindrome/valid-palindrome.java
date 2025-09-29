3e9baf99204af5e92921f7ddfdfbfd84c5cc4e47
AnujaShingane <anuja.shingane@cumminscollege.in>
2025-09-30 00:03:24 +0530

    Time: 18 ms (11.12%) | Memory: 45.4 MB (15.41%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,14 +1,17 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        s = s.trim();
-        s = s.replaceAll("\\s+","");
-        s = s.replaceAll("[^a-zA-Z0-9]","");
-        s = s.toLowerCase();
-        int i = 0;
-        int j = s.length()-1;
+        s=s.trim();
+        s=s.replaceAll("\\s+","");
+        s=s.replaceAll("[^a-zA-Z0-9]","");
+        s=s.toLowerCase();
 
-        while(i < j){
-            if(s.charAt(i) != s.charAt(j))return false;
+        int n = s.length();
+        int i = 0;
+        int j = n-1;
+        while(i<j){
+            if(s.charAt(i)!=s.charAt(j)){
+                return false;
+            }
             i++;
             j--;
         }
