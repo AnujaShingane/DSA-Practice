102e5d0503ee40361deb232df3efad241c75e4f4
AnujaShingane <anuja.shingane@cumminscollege.in>
2025-10-03 21:58:34 +0530

    Time: 1 ms (76.85%) | Memory: 41.5 MB (78.46%) - LeetSync

--- 807-custom-sort-string/custom-sort-string.java
+++ 807-custom-sort-string/custom-sort-string.java
@@ -1,17 +1,24 @@
 class Solution {
     public String customSortString(String order, String s) {
-        int[] freq = new int[26];
-        for (char c : s.toCharArray()) freq[c - 'a']++;
-
+        int[] arr = new int[26];
+        Arrays.fill(arr,0);
         StringBuilder sb = new StringBuilder();
-        // put chars that appear in 'order' first, in that order
-        for (char c : order.toCharArray()) {
-            while (freq[c - 'a']-- > 0) sb.append(c);
+
+        for(int i = 0 ; i < s.length() ; i++){
+            arr[s.charAt(i)-'a']++;
         }
-        // append leftovers (not in 'order') in any deterministic order
+
+        for(int i = 0 ; i < order.length() ; i++){
+            while(arr[order.charAt(i)-'a']!=0){
+                sb.append(order.charAt(i));
+                arr[order.charAt(i)-'a']--;
+            }
+        }
+
         for (char c = 'a'; c <= 'z'; c++) {
-            while (freq[c - 'a']-- > 0) sb.append(c);
+            while (arr[c - 'a']-- > 0) sb.append(c);
         }
-        return sb.toString(); 
+
+        return sb.toString();
     }
 }
