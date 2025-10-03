291d94da3f0819df38fbaf0deeb2b742d8bcb043
AnujaShingane <anuja.shingane@cumminscollege.in>
2025-10-03 16:33:43 +0530

    Time: 2 ms (22.24%) | Memory: 44 MB (84.39%) - LeetSync

--- 443-string-compression/string-compression.java
+++ 443-string-compression/string-compression.java
@@ -1,19 +1,26 @@
 class Solution {
     public int compress(char[] chars) {
-        int n = chars.length, i = 0;
+        int n = chars.length;
         StringBuilder sb = new StringBuilder();
+        int cnt = 0;
+        int i = 0;
 
-        while (i < n) {
+        while(i<n){
             int j = i;
             while (j < n && chars[j] == chars[i]) j++;
-            sb.append(chars[i]);                 // char of the run
-            int cnt = j - i;
-            if (cnt > 1) sb.append(cnt);        // writes all digits
+            cnt = j - i;
+            if(cnt==1)sb.append(chars[i]);
+            if (cnt > 1){
+                sb.append(chars[i]);
+                sb.append(cnt);  
+            }      // writes all digits
             i = j;
         }
 
-        // write back in-place
-        for (i = 0; i < sb.length(); i++) chars[i] = sb.charAt(i);
-        return sb.length();
+        String str = sb.toString();
+        for(i=0; i<str.length();i++){
+            chars[i] = str.charAt(i);
+        }
+        return str.length();
     }
 }
