70bda49279475920d94a95f7575baaddc57f90b2
AnujaShingane <anuja.shingane@cumminscollege.in>
2025-10-06 15:15:17 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Print Matrix in snake Pattern/print-matrix-in-snake-pattern.java
@@ -0,0 +1,24 @@
+// User function Template for Java
+
+class Solution {
+    // Function to return list of integers visited in snake pattern in matrix.
+    static ArrayList<Integer> snakePattern(int matrix[][]) {
+        // code here
+        int n=matrix.length;
+        int m=matrix[0].length;
+        ArrayList<Integer> res = new ArrayList<>();
+        
+        for(int i=0;i<n;i++){
+            if(i%2==0){
+                for(int j=0;j<m;j++){
+                    res.add(matrix[i][j]);
+                }
+            }else{
+                for(int j=m-1;j>=0;j--){
+                    res.add(matrix[i][j]);
+                }
+            }
+        }
+        return res;
+    }
+}
