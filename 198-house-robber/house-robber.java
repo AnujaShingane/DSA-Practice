20197c477c68e15d68247d289a616fe1f1aa11a4
AnujaShingane <anuja.shingane@cumminscollege.in>
2025-10-27 19:42:53 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (62.39%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -2,18 +2,16 @@ class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
         int[] dp = new int[n];
-        Arrays.fill(dp,-1);
 
-        return func(n-1,nums,dp);
-    }
+        if(n==0)return 0;
+        if(n==1)return nums[0];
 
-    public int func(int ind, int[] nums , int[] dp){
-        if(ind<0)return 0;
-        if(dp[ind]!=-1)return dp[ind];
-        
-        int take = nums[ind]+func(ind-2,nums,dp);
-        int nottake = func(ind-1,nums,dp);
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0],nums[1]);
 
-        return dp[ind]=Math.max(take , nottake);
+        for(int i = 2 ; i < n ; i++){
+            dp[i] = Math.max(dp[i-1],nums[i]+dp[i-2]);
+        }
+        return dp[n-1];
     }
 }
