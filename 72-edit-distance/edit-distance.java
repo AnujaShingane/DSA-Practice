ac3c46bf34582233d97147ebde64bf121534e4d1
AnujaShingane <anuja.shingane@cumminscollege.in>
2025-11-08 23:02:59 +0530

    Time: 4 ms (71.42%) | Memory: 46.3 MB (10.04%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -2,21 +2,24 @@ class Solution {
     public int minDistance(String word1, String word2) {
         int n = word1.length();
         int m = word2.length();
-        int[][] dp = new int[n+1][m+1];
+        int[] prev = new int[m+1];
+        int[] curr = new int[m+1];
 
-        for(int i = 0 ; i <= n ; i++)dp[i][0]=i;
-        for(int j = 0 ; j <= m ; j++)dp[0][j]=j;
+        for(int i = 0 ; i <= n ; i++)curr[0]=i;
+        for(int j = 0 ; j <= m ; j++)prev[j]=j;
 
         for(int i = 1 ; i <= n ; i++){
+            curr[0]=i;
             for(int j = 1 ; j <= m ; j++){
                 if(word1.charAt(i-1)==word2.charAt(j-1)){
-                    dp[i][j]=dp[i-1][j-1];
+                    curr[j]=prev[j-1];
                 }
 
-                else dp[i][j]=1+Math.min(dp[i][j-1],Math.min(dp[i-1][j],dp[i-1][j-1]));
+                else curr[j]=1+Math.min(curr[j-1],Math.min(prev[j],prev[j-1]));
             }
+            prev =curr.clone();
         }
-        return dp[n][m];
+        return prev[m];
     }
 
     // public int func(int i,int j , String s1, String s2,int[][] dp) {
