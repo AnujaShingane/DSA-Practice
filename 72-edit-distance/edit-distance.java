50a7822c6d9d9b8d7ef0cedf9a0b6ac0c6b67dff
AnujaShingane <anuja.shingane@cumminscollege.in>
2025-11-08 22:56:52 +0530

    Time: 5 ms (57.92%) | Memory: 47.2 MB (5.03%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -3,22 +3,32 @@ class Solution {
         int n = word1.length();
         int m = word2.length();
         int[][] dp = new int[n+1][m+1];
-        for(int[] row : dp){
-            Arrays.fill(row,-1);
+
+        for(int i = 0 ; i <= n ; i++)dp[i][0]=i;
+        for(int j = 0 ; j <= m ; j++)dp[0][j]=j;
+
+        for(int i = 1 ; i <= n ; i++){
+            for(int j = 1 ; j <= m ; j++){
+                if(word1.charAt(i-1)==word2.charAt(j-1)){
+                    dp[i][j]=dp[i-1][j-1];
+                }
+
+                else dp[i][j]=1+Math.min(dp[i][j-1],Math.min(dp[i-1][j],dp[i-1][j-1]));
+            }
         }
-        return func(n,m,word1,word2,dp);
+        return dp[n][m];
     }
 
-    public int func(int i,int j , String s1, String s2,int[][] dp) {
-        if(i==0)return j;
-        if(j==0)return i;
+    // public int func(int i,int j , String s1, String s2,int[][] dp) {
+    //     if(i==0)return j;
+    //     if(j==0)return i;
 
-        if(dp[i][j] != -1)return dp[i][j];
+    //     if(dp[i][j] != -1)return dp[i][j];
 
-        if(s1.charAt(i-1)==s2.charAt(j-1)){
-            return dp[i][j]=func(i-1,j-1,s1,s2,dp);
-        }
+    //     if(s1.charAt(i-1)==s2.charAt(j-1)){
+    //         return dp[i][j]=func(i-1,j-1,s1,s2,dp);
+    //     }
 
-        return dp[i][j]=1+Math.min(func(i,j-1,s1,s2,dp),Math.min(func(i-1,j,s1,s2,dp),func(i-1,j-1,s1,s2,dp)));
-    }
+    //     return dp[i][j]=1+Math.min(func(i,j-1,s1,s2,dp),Math.min(func(i-1,j,s1,s2,dp),func(i-1,j-1,s1,s2,dp)));
+    // }
 }
