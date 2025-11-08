39aa4ca500ae3a3cbfe0837e6e61e0adbc5b8563
AnujaShingane <anuja.shingane@cumminscollege.in>
2025-11-08 20:09:55 +0530

    Time: 19 ms (64.10%) | Memory: 54.2 MB (18.12%) - LeetSync

---
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int numDistinct(String s, String t) {
+        int n = s.length();
+        int m = t.length();
+        int[][] dp = new int[n+1][m+1];
+
+        for(int i = 0 ; i <= n ; i++)dp[i][0]=1;
+
+        for(int i = 1 ; i <= n ; i++){
+            for(int j = 1 ; j <= m ; j++){
+                if(s.charAt(i-1)==t.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1]+dp[i-1][j];
+                }
+                else  dp[i][j]=dp[i-1][j];
+            }
+        }
+        return dp[n][m];
+    }
+
+    // public int func(String s1, String s2,int i , int j,int[][] dp) {
+    //     if(i==0)return 0;
+    //     if(j==0)return 1;
+
+    //     if(dp[i][j] != -1)return dp[i][j];
+
+    //     if(s1.charAt(i-1)==s2.charAt(j-1)){
+    //         return dp[i][j] = func(s1,s2,i-1,j-1,dp)+func(s1,s2,i-1,j,dp);
+    //     }
+    //     return dp[i][j]=func(s1,s2,i-1,j,dp);
+    // }
+}
