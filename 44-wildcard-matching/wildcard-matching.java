0a4eb1a48c063f89105864df9ed1034195b0752a
AnujaShingane <anuja.shingane@cumminscollege.in>
2025-11-09 13:43:52 +0530

    Time: 14 ms (81.30%) | Memory: 63.8 MB (5.23%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.java
+++ 44-wildcard-matching/wildcard-matching.java
@@ -1,39 +1,27 @@
 class Solution {
     public boolean isMatch(String s, String p) {
-        int n = s.length(), m = p.length();
-        Boolean[][] dp = new Boolean[n + 1][m + 1];
-        return solve(s, p, n, m, dp);
+        int n = s.length();
+        int m = p.length();
+        Boolean[][] dp = new Boolean[n+1][m+1];
+
+        return func(s,p,n,m,dp);
     }
 
-    // i = length of s prefix, j = length of p prefix (1-based sizes)
-    private boolean solve(String s1, String s2, int i, int j, Boolean[][] dp) {
-        // both empty -> match
-        if (i == 0 && j == 0) return true;
-
-        // pattern empty but text not -> no match
-        if (j == 0) return false;
-
-        // text empty -> pattern must be all '*'
-        if (i == 0) {
-            for (int k = 1; k <= j; k++) {
-                if (s2.charAt(k - 1) != '*') return false;
+    public boolean func(String s1, String s2,int i , int j,Boolean[][] dp) {
+        if(i==0 && j==0)return true;
+        if(i==0 && j>=1){
+            for(int k = 1 ; k <= j ; k++){
+                if(s2.charAt(k-1) != '*')return false;
             }
             return true;
         }
+        if(j==0 && i>=1)return false;
 
-        if (dp[i][j] != null) return dp[i][j];
+        if(dp[i][j] != null)return dp[i][j];
 
-
-        boolean ans;
-        if (s2.charAt(j-1) == s1.charAt(i-1) || s2.charAt(j-1) == '?') {
-            ans = solve(s1, s2, i - 1, j - 1, dp);
-        } else if (s2.charAt(j-1) == '*') {
-            // '*' matches empty  -> (i, j-1)
-            // or matches one+    -> (i-1, j)
-            ans = solve(s1, s2, i, j - 1, dp) || solve(s1, s2, i - 1, j, dp);
-        } else {
-            ans = false;
-        }
-        return dp[i][j] = ans;
+        if(s1.charAt(i-1) == s2.charAt(j-1) || s2.charAt(j-1) == '?'){
+            return dp[i][j]=func(s1,s2,i-1,j-1,dp);
+        }else if(s2.charAt(j-1) == '*')return dp[i][j]=func(s1,s2,i,j-1,dp) || func(s1,s2,i-1,j,dp);
+        else return dp[i][j]=false;
     }
 }
