372d62b2002e5535e57238cf57f1831cae57d004
AnujaShingane <anuja.shingane@cumminscollege.in>
2025-11-13 22:29:16 +0530

    Time: 24 ms (32.05%) | Memory: 54.1 MB (6.70%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -2,25 +2,24 @@ class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
         int n = text1.length();
         int m = text2.length();
-        int[] prev = new int[m+1];
-        int[] cur = new int[m+1];
 
-        for(int i = 1 ; i <= n ; i++){
-            for(int j = 1 ; j <= m ; j++){
-                if(text1.charAt(i-1)==text2.charAt(j-1))cur[j]=1+prev[j-1];
-                else cur[j]=Math.max(cur[j-1],prev[j]);
-            }
-            prev = cur.clone();
+        int[][] dp = new int[n+1][m+1];
+        for(int[] row : dp){
+            Arrays.fill(row,-1);
         }
 
-        return cur[m];
+        return func(n,m,text1,text2,dp);
     }
 
-    // public int func(int i , int j, String s1, String s2, int[][] dp) {
-    //     if(i==0 || j==0)return 0;
-    //     if(dp[i][j] != -1)return dp[i][j];
-        
-    //     if(s1.charAt(i-1)==s2.charAt(j-1))return dp[i][j]=1+func(i-1,j-1,s1,s2,dp);
-    //     return dp[i][j]=Math.max(func(i,j-1,s1,s2,dp),func(i-1,j,s1,s2,dp));
-    // }
+    public int func(int ind1,int ind2,String s1,String s2,int[][] dp) {
+        if(ind1==0 || ind2==0)return 0;
+
+        if(dp[ind1][ind2] != -1)return dp[ind1][ind2];
+
+        if(s1.charAt(ind1-1) == s2.charAt(ind2-1)){
+            return 1+func(ind1-1,ind2-1,s1,s2,dp);
+        }
+
+        return dp[ind1][ind2]=Math.max(func(ind1-1,ind2,s1,s2,dp),func(ind1,ind2-1,s1,s2,dp));
+    }
 }
