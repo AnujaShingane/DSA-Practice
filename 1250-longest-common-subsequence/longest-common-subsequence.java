2627c526bb2519ea0321593f6f1d8c861119baa8
AnujaShingane <anuja.shingane@cumminscollege.in>
2025-11-13 20:40:15 +0530

    Time: 20 ms (74.90%) | Memory: 53.7 MB (17.34%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -3,18 +3,22 @@ class Solution {
         int n = text1.length();
         int m = text2.length();
         int[][] dp = new int[n+1][m+1];
-        for(int[] rows : dp){
-            Arrays.fill(rows,-1);
+
+        for(int i = 1 ; i <= n ; i++){
+            for(int j = 1 ; j <= m ; j++){
+                if(text1.charAt(i-1)==text2.charAt(j-1))dp[i][j]=1+dp[i-1][j-1];
+                else dp[i][j]=Math.max(dp[i][j-1],dp[i-1][j]);
+            }
         }
 
-        return func(n,m,text1,text2,dp);
+        return dp[n][m];
     }
 
-    public int func(int i , int j, String s1, String s2, int[][] dp) {
-        if(i==0 || j==0)return 0;
-        if(dp[i][j] != -1)return dp[i][j];
+    // public int func(int i , int j, String s1, String s2, int[][] dp) {
+    //     if(i==0 || j==0)return 0;
+    //     if(dp[i][j] != -1)return dp[i][j];
         
-        if(s1.charAt(i-1)==s2.charAt(j-1))return dp[i][j]=1+func(i-1,j-1,s1,s2,dp);
-        return dp[i][j]=Math.max(func(i,j-1,s1,s2,dp),func(i-1,j,s1,s2,dp));
-    }
+    //     if(s1.charAt(i-1)==s2.charAt(j-1))return dp[i][j]=1+func(i-1,j-1,s1,s2,dp);
+    //     return dp[i][j]=Math.max(func(i,j-1,s1,s2,dp),func(i-1,j,s1,s2,dp));
+    // }
 }
