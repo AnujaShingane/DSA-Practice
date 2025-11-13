63c216fb5a5d82b09dd5cd34fdd5c8d5e834846e
AnujaShingane <anuja.shingane@cumminscollege.in>
2025-11-13 21:39:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Common Substring/longest-common-substring.java
+++ Difficulty: Medium/Longest Common Substring/longest-common-substring.java
@@ -1,25 +1,38 @@
-// User function Template for Java
+import java.util.*;
 
 class Solution {
+    int ans; 
+
     public int longestCommonSubstr(String s1, String s2) {
         int n = s1.length();
         int m = s2.length();
-        
+
         int[][] dp = new int[n+1][m+1];
-        
-        for(int i = 0 ; i <= n ; i++)dp[i][0]=0;
-        for(int i = 0 ; i <= m ; i++)dp[0][i]=0;
-        
-        int ans = 0;
-        for(int i = 1 ; i <=n ; i++){
-            for(int j = 1 ; j <=m ; j++){
-                if(s1.charAt(i-1)==s2.charAt(j-1)){
-                    dp[i][j]=1+dp[i-1][j-1];
-                    ans = Math.max(ans,dp[i][j]);
-                }else dp[i][j]=0;
+        for (int[] row : dp) Arrays.fill(row, -1);
+
+        ans = 0;
+
+        // IMPORTANT: must explore all (i, j)
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                func(i, j, s1, s2, dp);
             }
         }
-        
+
         return ans;
+    }
+
+    public int func(int i, int j, String s1, String s2, int[][] dp) {
+        if (i == 0 || j == 0) return 0;
+        if (dp[i][j] != -1) return dp[i][j];
+
+        if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+            dp[i][j] = 1 + func(i - 1, j - 1, s1, s2, dp);
+            ans = Math.max(ans, dp[i][j]); // Update global max
+        } else {
+            dp[i][j] = 0; // substring break
+        }
+
+        return dp[i][j];
     }
 }
