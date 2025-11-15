41822b4356b7f80c658959a1c8b4ca3805a14203
AnujaShingane <anuja.shingane@cumminscollege.in>
2025-11-15 15:33:47 +0530

    Time: 28 ms (23.73%) | Memory: 47.1 MB (5.65%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -5,6 +5,18 @@ class Solution {
         for(int[] row : dp){
             Arrays.fill(row,-1);
         }
+
+        // for(int i = 1 ; i < n ; i++){
+        //     for(int j = 0 ; j <= amount ; j++){
+        //         int notPick = dp[i-1][j];
+        //         int pick = Integer.MAX_VALUE;
+        //         if(j>=coins[i]){
+        //             pick = 1+dp[i][j-coins[i]];
+        //         }
+        //         else dp[i][j]=Math.min(notPick,pick);
+        //     }
+        // }
+
         int ans = func(n-1,amount,coins,dp);
         if(ans >= (int)1e9)return -1;
         else return ans;
@@ -23,5 +35,15 @@ class Solution {
         }
 
         return dp[ind][target]=Math.min(notPick,pick);
+    }
+
+    private void printDP(int[][] dp) {
+        for (int i = 0; i < dp.length; i++) {
+            System.out.print("Row " + i + ": ");
+            for (int j = 0; j < dp[0].length; j++) {
+                System.out.print(dp[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
