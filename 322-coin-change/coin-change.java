c60e37ba440e8f914584166ef4d39e6a2ed3a889
AnujaShingane <anuja.shingane@cumminscollege.in>
2025-11-15 16:07:18 +0530

    Time: 15 ms (77.56%) | Memory: 47 MB (6.93%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -25,19 +25,4 @@ class Solution {
         if(ans >= (int)1e9)return -1;
         else return ans;
     }
-
-    // public int func(int ind, int target,int[] coins,int[][] dp) {
-    //     if(ind==0){
-    //         if(target%coins[ind]==0)return target/coins[ind];
-    //         else return (int)1e9;
-    //     }
-    //     if(dp[ind][target]!=-1)return dp[ind][target];
-    //     int notPick = func(ind-1,target,coins,dp);
-    //     int pick = Integer.MAX_VALUE;
-    //     if(target>=coins[ind]){
-    //         pick = 1+func(ind,target-coins[ind],coins,dp);
-    //     }
-
-    //     return dp[ind][target]=Math.min(notPick,pick);
-    // }
 }
