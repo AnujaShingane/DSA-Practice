b7a0eb243cbe8b5b06fa1cab4e1860095a3ada58
AnujaShingane <anuja.shingane@cumminscollege.in>
2025-11-15 18:10:46 +0530

    Time: 10 ms (64.71%) | Memory: 46.5 MB (62.75%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -1,25 +1,27 @@
 class Solution {
     public int change(int amount, int[] coins) {
         int n = coins.length;
-        int[][] dp = new int[n][amount+1];
+        int[] prev = new int[amount+1];
+        int[] cur = new int[amount+1];
 
         for(int i = 0 ; i <= amount ; i++){
-            if(i%coins[0]==0)dp[0][i] = 1;
-            else dp[0][i] = 0;
+            if(i%coins[0]==0)prev[i] = 1;
+            else prev[i] = 0;
         }
 
         for(int i = 1 ; i < n ; i++){
             for(int amt = 0 ; amt <= amount ; amt++){
-                int notPick = dp[i-1][amt];
+                int notPick = prev[amt];
                 int pick = 0;
                 if(amt>=coins[i]){
-                    pick = dp[i][amt-coins[i]];
+                    pick = cur[amt-coins[i]];
                 }
 
-                dp[i][amt]=notPick+pick;
+                cur[amt]=notPick+pick;
             }
+            prev = cur.clone();
         }
-        return dp[n-1][amount];
+        return prev[amount];
     }
 
     // public int func(int ind,int target,int[] coins,int[][] dp) {
