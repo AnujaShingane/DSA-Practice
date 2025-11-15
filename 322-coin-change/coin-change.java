cd23589c85d19e429d1e1b06be46c9680f7bf84a
AnujaShingane <anuja.shingane@cumminscollege.in>
2025-11-15 16:06:29 +0530

    Time: 15 ms (77.56%) | Memory: 47.1 MB (5.65%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,49 +1,43 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
-        int[][] dp = new int[n][amount+1];
-        for(int[] row : dp){
-            Arrays.fill(row,-1);
+        int[] prev = new int[amount+1];
+        int[] cur = new int[amount+1];
+
+        for(int amt = 0 ; amt <= amount ; amt++){
+            if(amt%coins[0] == 0)prev[amt] = amt/coins[0];
+            else prev[amt] = (int)1e9;
         }
 
-        // for(int i = 1 ; i < n ; i++){
-        //     for(int j = 0 ; j <= amount ; j++){
-        //         int notPick = dp[i-1][j];
-        //         int pick = Integer.MAX_VALUE;
-        //         if(j>=coins[i]){
-        //             pick = 1+dp[i][j-coins[i]];
-        //         }
-        //         else dp[i][j]=Math.min(notPick,pick);
-        //     }
-        // }
+        for(int i = 1 ; i < n ; i++){
+            for(int j = 0 ; j <= amount ; j++){
+                int notPick = prev[j];
+                int pick = Integer.MAX_VALUE;
+                if(j>=coins[i]){
+                    pick = 1+cur[j-coins[i]];
+                }
+                cur[j]=Math.min(notPick,pick);
+            }
+            prev = cur.clone();
+        }
 
-        int ans = func(n-1,amount,coins,dp);
+        int ans = prev[amount];
         if(ans >= (int)1e9)return -1;
         else return ans;
     }
 
-    public int func(int ind, int target,int[] coins,int[][] dp) {
-        if(ind==0){
-            if(target%coins[ind]==0)return target/coins[ind];
-            else return (int)1e9;
-        }
-        if(dp[ind][target]!=-1)return dp[ind][target];
-        int notPick = func(ind-1,target,coins,dp);
-        int pick = Integer.MAX_VALUE;
-        if(target>=coins[ind]){
-            pick = 1+func(ind,target-coins[ind],coins,dp);
-        }
+    // public int func(int ind, int target,int[] coins,int[][] dp) {
+    //     if(ind==0){
+    //         if(target%coins[ind]==0)return target/coins[ind];
+    //         else return (int)1e9;
+    //     }
+    //     if(dp[ind][target]!=-1)return dp[ind][target];
+    //     int notPick = func(ind-1,target,coins,dp);
+    //     int pick = Integer.MAX_VALUE;
+    //     if(target>=coins[ind]){
+    //         pick = 1+func(ind,target-coins[ind],coins,dp);
+    //     }
 
-        return dp[ind][target]=Math.min(notPick,pick);
-    }
-
-    private void printDP(int[][] dp) {
-        for (int i = 0; i < dp.length; i++) {
-            System.out.print("Row " + i + ": ");
-            for (int j = 0; j < dp[0].length; j++) {
-                System.out.print(dp[i][j] + " ");
-            }
-            System.out.println();
-        }
-    }
+    //     return dp[ind][target]=Math.min(notPick,pick);
+    // }
 }
