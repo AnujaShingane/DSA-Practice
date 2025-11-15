d0d46c75dc7f577f8d409985cdcbe4c234c542f6
AnujaShingane <anuja.shingane@cumminscollege.in>
2025-11-15 15:16:40 +0530

    Time: 29 ms (21.71%) | Memory: 47 MB (5.65%) - LeetSync

---
+++ 322-coin-change/coin-change.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int n = coins.length;
+        int[][] dp = new int[n][amount+1];
+        for(int[] row : dp){
+            Arrays.fill(row,-1);
+        }
+        int ans = func(n-1,amount,coins,dp);
+        if(ans>= (int)1e9){
+            return -1;
+        }else return ans;
+    }
+
+    public int func(int ind, int target,int[] coins,int[][] dp) {
+        if(ind==0){
+            if(target%coins[ind]==0)return target/coins[ind];
+            else return (int)1e9;
+        }
+        if(dp[ind][target]!=-1)return dp[ind][target];
+        
+        int notPick = func(ind-1,target,coins,dp);
+        int pick = Integer.MAX_VALUE;
+        if(coins[ind]<=target){
+            pick = 1 + func(ind,target-coins[ind],coins,dp);
+        }
+
+        return dp[ind][target]=Math.min(notPick,pick);
+    }
+}
