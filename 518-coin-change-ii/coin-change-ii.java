0f44ead313796a8948ffbd7c54edaae93aaa471a
AnujaShingane <anuja.shingane@cumminscollege.in>
2025-12-12 14:58:55 +0530

    Time: 15 ms (40.20%) | Memory: 57.1 MB (19.68%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -1,42 +1,26 @@
 class Solution {
     public int change(int amount, int[] coins) {
         int n = coins.length;
-        int[] prev = new int[amount+1];
-        int[] cur = new int[amount+1];
-
-        for(int i = 0 ; i <= amount ; i++){
-            if(i%coins[0]==0)prev[i] = 1;
-            else prev[i] = 0;
+        int[][] dp = new int[n][amount+1];
+        for(int[] row : dp){
+            Arrays.fill(row,-1);
         }
 
-        for(int i = 1 ; i < n ; i++){
-            for(int amt = 0 ; amt <= amount ; amt++){
-                int notPick = prev[amt];
-                int pick = 0;
-                if(amt>=coins[i]){
-                    pick = cur[amt-coins[i]];
-                }
-
-                cur[amt]=notPick+pick;
-            }
-            prev = cur.clone();
-        }
-        return prev[amount];
+        int ans = func(n-1,amount, coins,dp);
+        return ans;
     }
 
-    // public int func(int ind,int target,int[] coins,int[][] dp) {
-    //     if(ind==0){
-    //         if(target%coins[ind]==0)return 1;
-    //         else return 0;
-    //     }
-    //     if(dp[ind][target]!=-1)return dp[ind][target];
+    public int func(int ind , int amount , int[] coins,int[][] dp){
+        if(ind==0){
+            if(amount%coins[0]==0)return 1;
+            return 0;
+        }
+        if(dp[ind][amount] != -1)return dp[ind][amount];
 
-    //     int notPick = func(ind-1,target,coins,dp);
-    //     int pick = 0;
-    //     if(target>=coins[ind]){
-    //         pick = func(ind,target-coins[ind],coins,dp);
-    //     }
+        int pick = 0;
+        if(coins[ind]<=amount)pick = func(ind,amount-coins[ind],coins,dp);
+        int notpick = func(ind-1,amount,coins,dp);
 
-    //     return dp[ind][target]=notPick+pick;
-    // }
+        return dp[ind][amount] = notpick+pick;
+    }
 }
