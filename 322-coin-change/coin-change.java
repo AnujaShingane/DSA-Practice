d9a90dd29cb3c97f61969882cff0b622fd8b44c8
AnujaShingane <anuja.shingane@cumminscollege.in>
2025-12-12 14:05:45 +0530

    Time: 15 ms (79.09%) | Memory: 46.8 MB (22.25%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,22 +1,24 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
-        int[][] dp = new int[n][amount+1];
+        int[] prev = new int[amount+1];
+        int[] cur = new int[amount+1];
 
         for(int amt = 0 ; amt <= amount ; amt++){
-            if(amt%coins[0]==0)dp[0][amt]=amt/coins[0];
-            else dp[0][amt]= (int)(1e9);
+            if(amt%coins[0]==0)prev[amt]=amt/coins[0];
+            else prev[amt]= (int)(1e9);
         }
 
         for(int i = 1 ; i < n ; i++){
             for(int amt = 0 ; amt<=amount ; amt++){
-                int nottake = dp[i-1][amt];
+                int nottake = prev[amt];
                 int take = Integer.MAX_VALUE;
-                if(amt>=coins[i])take = 1+dp[i][amt-coins[i]];
-                dp[i][amt]=Math.min(nottake,take);
+                if(amt>=coins[i])take = 1+cur[amt-coins[i]];
+                cur[amt]=Math.min(nottake,take);
             }
+            prev = cur.clone();
         }
 
-        return (dp[n-1][amount]==1e9)?-1:dp[n-1][amount];
+        return (prev[amount]==1e9)?-1:prev[amount];
     }
 }
