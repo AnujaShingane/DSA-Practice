5a5ec8dc8147e88fc728b9115f7c95fd17e3bb36
AnujaShingane <anuja.shingane@cumminscollege.in>
2025-12-12 13:23:15 +0530

    Time: 27 ms (27.12%) | Memory: 46.7 MB (24.52%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -5,24 +5,23 @@ class Solution {
         for(int[] row : dp){
             Arrays.fill(row,-1);
         }
-
-        int ans =func(n-1, amount,coins, dp);
-        if(ans >= (int)1e9)return -1;
-        else return ans;
+        int ans = func(coins, amount , n-1,dp);
+        return (ans==1e9)?-1:ans;
     }
 
-    public int func(int ind, int target,int[] coins,int[][] dp) {
+    public int func(int[] coins, int target , int ind,int[][] dp){
+        if(target==0)return 0;      
         if(ind==0){
-            if(target%coins[ind]==0)return target/coins[ind];
-            else return (int)1e9;
+            if(target%coins[0]==0)return target/coins[0];
+            return (int)(1e9);
         }
         if(dp[ind][target]!=-1)return dp[ind][target];
-        int notPick = func(ind-1,target,coins,dp);
-        int pick = Integer.MAX_VALUE;
-        if(target>=coins[ind]){
-            pick = 1+func(ind,target-coins[ind],coins,dp);
-        }
 
-        return dp[ind][target]=Math.min(notPick,pick);
+        int nottake = func(coins,target, ind-1,dp);
+        int take = Integer.MAX_VALUE;
+        if(target>=coins[ind]){
+            take = 1+func(coins,target-coins[ind],ind,dp);
+        }
+        return dp[ind][target]=Math.min(take,nottake);
     }
 }
