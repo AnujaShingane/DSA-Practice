e48d57b239f686ed634e507d6825dc71a34b72fe
AnujaShingane <anuja.shingane@cumminscollege.in>
2025-12-12 13:54:19 +0530

    Time: 15 ms (79.09%) | Memory: 47 MB (15.80%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -2,25 +2,24 @@ class Solution {
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
         int[][] dp = new int[n][amount+1];
-        for(int[] row : dp){
-            Arrays.fill(row,-1);
+        if (amount == 0) return 0;
+        if (n == 0) return -1;
+
+        for(int amt = 0 ; amt<=amount ; amt++){
+            if(amt%coins[0]==0)dp[0][amt]=amt/coins[0];
+            else dp[0][amt]=(int)1e9;
+        }
+        
+        for(int i = 1 ; i < n ; i++){
+            for(int amt = 0 ; amt <= amount ; amt++){
+                int nottake = dp[i-1][amt];
+                int take = Integer.MAX_VALUE;
+                if(amt>=coins[i])take = 1+dp[i][amt-coins[i]];
+
+                dp[i][amt]=Math.min(nottake,take);
+            }
         }
 
-        int ans = func(coins,amount,n-1,dp);
-        return (ans==(int)1e9)?-1:ans;
-    }
-
-    public int func(int[] coins,int target,int ind,int[][] dp){
-        if(ind==0){
-            if(target%coins[0]==0)return target/coins[0];
-            return (int)1e9;
-        }
-        if(dp[ind][target]!=-1)return dp[ind][target];
-
-        int nottake = func(coins,target,ind-1,dp);
-        int take = Integer.MAX_VALUE;
-        if(target>=coins[ind])take = 1+func(coins,target-coins[ind],ind,dp);
-
-        return dp[ind][target]=Math.min(nottake,take);
+        return (dp[n-1][amount]==1e9)?-1:dp[n-1][amount];
     }
 }
