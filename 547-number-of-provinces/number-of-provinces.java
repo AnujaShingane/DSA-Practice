c1b64c2db429e994a6eef984ce16d52dd4d18cac
AnujaShingane <anuja.shingane@cumminscollege.in>
2025-12-19 10:56:47 +0530

    Time: 4 ms (20.96%) | Memory: 49.9 MB (6.44%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,47 +1,48 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
-        int n = isConnected.length;
+        int n = isConnected[0].length;
         boolean[] vis = new boolean[n];
-
-        ArrayList<ArrayList<Integer>> adj = buildAdj(n, isConnected);
-
         int cnt = 0;
-        for (int i = 0; i < n; i++) {
-            if (!vis[i]) {
+
+        ArrayList<ArrayList<Integer>> adj = adj(n, isConnected);
+
+        for(int i = 0 ; i < n ; i++){
+            if(vis[i]==false){
                 cnt++;
-                bfs(i, adj, vis);
+                bfs(i,vis,isConnected,adj);
             }
         }
         return cnt;
     }
 
-    private ArrayList<ArrayList<Integer>> buildAdj(int n, int[][] isConnected) {
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        for (int i = 0; i < n; i++) adj.add(new ArrayList<>());
+    public void bfs(int start, boolean[] vis,int[][] isConnected,ArrayList<ArrayList<Integer>> adj){
+        Queue<Integer> q = new LinkedList<>();
+        q.add(start);
+        vis[start]=true;
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (isConnected[i][j] == 1 && i != j) {
+        while(!q.isEmpty()){
+            int rem = q.poll();
+            for(int num : adj.get(rem)){
+                if(vis[num]==false){
+                    vis[num]=true;
+                    q.add(num);
+                }
+            }
+        }
+    }
+
+    public ArrayList<ArrayList<Integer>> adj(int n, int[][] isConnected){
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+
+        for(int i = 0 ; i < n ; i++)adj.add(new ArrayList<>());
+
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < n ; j++){
+                if(isConnected[i][j]==1 && i!=j){
                     adj.get(i).add(j);
                 }
             }
         }
         return adj;
-    }
-
-    private void bfs(int start, ArrayList<ArrayList<Integer>> adj, boolean[] vis) {
-        Queue<Integer> q = new LinkedList<>();
-        q.add(start);
-        vis[start] = true;
-
-        while (!q.isEmpty()) {
-            int node = q.poll();
-            for (int nbr : adj.get(node)) {
-                if (!vis[nbr]) {
-                    vis[nbr] = true;
-                    q.add(nbr);
-                }
-            }
-        }
     }
 }
