99ec0fbc7825f0abc07ff4df8a4be90cb34a3ddf
AnujaShingane <anuja.shingane@cumminscollege.in>
2025-12-19 12:43:31 +0530

    Time: 2 ms (99.87%) | Memory: 52.3 MB (36.53%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,59 +1,33 @@
-class Pair {
-    int row;
-    int col;
-
-    Pair(int row, int col) {
-        this.row = row;
-        this.col = col;
-    }
-}
-
 class Solution {
     public int numIslands(char[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
-        boolean[][] vis = new boolean[n][m];
-
         int cnt = 0;
+
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (grid[i][j] == '1' && vis[i][j] == false) {
+                if (grid[i][j] == '1') {
                     cnt++;
-                    bfs(n, m, i, j, vis, grid);
+                    dfs(i, j, grid);
                 }
             }
         }
         return cnt;
     }
 
-    public void bfs(int n, int m, int i, int j, boolean[][] vis, char[][] grid) {
-        Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(i, j));
-        vis[i][j] = true;
+    public void dfs(int i, int j, char[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
 
-        while (!q.isEmpty()) {
-            Pair rem = q.poll();
-            int row = rem.row;
-            int col = rem.col;
-
-            for (int delrow = -1; delrow <= 1; delrow++) {
-                for (int delcol = -1; delcol <= 1; delcol++) {
-
-                    if (Math.abs(delrow) + Math.abs(delcol) != 1) continue;
-
-                    int nrow = row + delrow;
-                    int ncol = col + delcol;
-
-                    if (nrow >= 0 && nrow < n &&
-                        ncol >= 0 && ncol < m &&
-                        vis[nrow][ncol] == false &&
-                        grid[nrow][ncol] == '1') {
-
-                        vis[nrow][ncol] = true;
-                        q.add(new Pair(nrow, ncol));
-                    }
-                }
-            }
+        if (i < 0 || j < 0 || i >= n || j >= m || grid[i][j] == '0') {
+            return;
         }
+
+        grid[i][j] = '0'; // mark visited
+
+        dfs(i - 1, j, grid); // up
+        dfs(i + 1, j, grid); // down
+        dfs(i, j - 1, grid); // left
+        dfs(i, j + 1, grid); // right
     }
 }
