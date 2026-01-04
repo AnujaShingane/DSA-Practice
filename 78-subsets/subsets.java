c4a3470842c361bebcdd5d4e6df88d00dd90bccc
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-01-04 12:02:26 +0530

    Time: 1 ms (87.78%) | Memory: 44.4 MB (7.27%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,28 +1,16 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
-        List<Integer> list = new ArrayList<>();
-        func(nums,0,list,res);
-
-        return res;
-    }
-
-    public void func(int[] nums,int i,List<Integer> list,List<List<Integer>> res) {
-        if(i>=nums.length){
-            res.add(new ArrayList<>(list));
-            return;
+        int n = nums.length;
+        int subsets = 1<<n;
+        
+        List<List<Integer>> ans = new ArrayList<>();
+        for(int num = 0; num < subsets ; num++){
+            List<Integer> list = new ArrayList<>();
+            for(int i = 0 ; i < n ; i++){
+                if((num & (1<<i)) != 0)list.add(nums[i]);
+            }
+            ans.add(list);
         }
-        list.add(nums[i]);
-        func(nums,i+1,list,res);
-        list.remove(list.size()-1);
-        func(nums,i+1,list,res);
+        return ans;
     }
 }
-
-
-//pick  notpick
-
-/*
-    notpick = func();
-    pick = list.add(nums[i]) + func();
-*/
