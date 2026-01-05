a2891574f62678e4a0bb5ef847f96dd2ff5c4fa5
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-01-05 15:15:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.java
+++ Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.java
@@ -1,41 +1,45 @@
 class Solution {
     public boolean isCyclic(int V, int[][] edges) {
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        for(int i = 0; i < V; i++) adj.add(new ArrayList<>());
-        
-        // directed graph
-        for(int[] e : edges){
-            adj.get(e[0]).add(e[1]);
-        }
-        
-        boolean[] vis = new boolean[V];
-        boolean[] pathVis = new boolean[V];
-        
-        for(int i = 0; i < V; i++){
-            if(!vis[i]){
-                if(dfs(i, vis, pathVis, adj)) return true;
-            }
-        }
+        ArrayList<Integer> topoSort = topoSort(V,edges);
+        if(topoSort.size()!=V)return true;
         return false;
     }
     
-    private boolean dfs(int node,
-                        boolean[] vis,
-                        boolean[] pathVis,
-                        ArrayList<ArrayList<Integer>> adj){
-        vis[node] = true;
-        pathVis[node] = true;
+    public ArrayList<Integer> topoSort(int V, int[][] edges) {
+        int[] indegree = new int[V];
+        Queue<Integer> q = new LinkedList<>();
+        ArrayList<ArrayList<Integer>> adj = adj(V,edges);
         
-        for(int ele : adj.get(node)){
-            if(!vis[ele]){
-                if(dfs(ele, vis, pathVis, adj)) return true;
-            }
-            else if(pathVis[ele]){
-                return true;   // 🔁 back-edge → cycle
+        for(List<Integer> list : adj){
+            for(int ele : list){
+                indegree[ele]++;
             }
         }
         
-        pathVis[node] = false; // backtrack
-        return false;
+        for(int i = 0 ; i < V ; i++){
+            if(indegree[i]==0)q.add(i);
+        }
+        
+        ArrayList<Integer> ans = new ArrayList<>();
+        while(!q.isEmpty()){
+            int ele = q.poll();
+            ans.add(ele);
+            
+            for(int ele2 : adj.get(ele)){
+                indegree[ele2]--;
+                if(indegree[ele2]==0)q.add(ele2);
+            }
+        }
+        return ans;
+    }
+    
+    public ArrayList<ArrayList<Integer>> adj(int V,int[][] edges){
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for(int i=0 ; i < V ; i++)adj.add(new ArrayList<>());
+        
+        for(int[] arr : edges){
+            adj.get(arr[0]).add(arr[1]);
+        }
+        return adj;
     }
 }
