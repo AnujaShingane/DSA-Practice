68b878aff85fb4407f0aae9f3e1d04998b7d3ec4
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-01-11 21:17:17 +0530

    Time: 50 ms (49.59%) | Memory: 47.8 MB (47.21%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -9,22 +9,36 @@ class Solution {
         if(sum%2!=0)return false;
         int target = sum/2;
 
-        Boolean[][] dp = new Boolean[n][target+1];
+        boolean[][] dp = new boolean[n][target+1];
 
-        return func(n-1,target,nums,dp);
+        if(target>=nums[0])dp[0][nums[0]]=true;
+        for(int i = 0 ; i < n ; i++)dp[i][0]=true;
+
+        for(int i = 1 ; i < n ; i++){
+            for(int tar = 1 ; tar <= target ; tar++){
+                boolean take = false;
+                if(tar>=nums[i])take=dp[i-1][tar-nums[i]];
+
+                boolean nottake = dp[i-1][tar];
+
+                dp[i][tar]=take || nottake;
+            }
+        }
+
+        return dp[n-1][target];
     }
 
-    public boolean func(int i,int target,int[] nums,Boolean[][] dp){
-        if(target==0)return true;
-        if(i==0)return target==nums[0];
+    // public boolean func(int i,int target,int[] nums,Boolean[][] dp){
+    //     if(target==0)return true;//dp[i][0]=true
+    //     if(i==0)return target==nums[0];//dp[0][nums[0]]=true
 
-        if(dp[i][target]!=null)return dp[i][target];
+    //     if(dp[i][target]!=null)return dp[i][target];
 
-        boolean take = false;
-        if(target>=nums[i])take=func(i-1,target-nums[i],nums,dp);
+    //     boolean take = false;
+    //     if(target>=nums[i])take=func(i-1,target-nums[i],nums,dp);
 
-        boolean nottake = func(i-1,target,nums,dp);
+    //     boolean nottake = func(i-1,target,nums,dp);
 
-        return dp[i][target]=take || nottake;
-    }
+    //     return dp[i][target]=take || nottake;
+    // }
 }
