a0a4ab70aadc108fba82b52d5632d017a62ab568
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-01-12 20:09:19 +0530

    Time: 20 ms (78.52%) | Memory: 54.1 MB (22.44%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -6,15 +6,29 @@ class Solution {
         for(int[] arr : dp){
             Arrays.fill(arr,-1);
         }
+        int ans = 0;
 
+        for(int i = 0 ; i <= n ; i++)dp[i][0]=0;
+        for(int i = 0 ; i <= m ; i++)dp[0][i]=0;
 
-        return func(n,m,text1,text2,dp);
+        for(int i = 1 ; i <= n ; i++){
+            for(int j = 1 ; j <= m ; j++){
+                if(text1.charAt(i-1)==text2.charAt(j-1)){
+                    ans = 1+dp[i-1][j-1];
+                }else{
+                    ans = Math.max(dp[i-1][j],dp[i][j-1]);
+                }
+                dp[i][j]=ans;
+            }
+        }
+
+        return dp[n][m];
     }
 
     public int func(int i,int j,String text1, String text2,int[][] dp) {
         int ans = 0;
 
-        if(i==0 || j == 0)return 0;//dp[][]=0;
+        if(i==0 || j == 0)return 0;//dp[0][j]=0;
         if(dp[i][j]!=-1)return dp[i][j];
 
         if(text1.charAt(i-1)==text2.charAt(j-1)){
