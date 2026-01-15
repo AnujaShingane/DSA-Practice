38a7940722f893783ea57008b489bf6aa6eadd80
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-01-15 20:20:55 +0530

    Time: 35 ms (26.39%) | Memory: 147.3 MB (44.34%) - LeetSync

---
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -0,0 +1,38 @@
+class Pair{
+    int ind;
+    int val;
+
+    Pair(int ind,int val){
+        this.ind=ind;
+        this.val = val;
+    }
+}
+
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int n = nums.length;
+        int[] arr = new int[n-k+1];
+
+        Deque<Pair> q = new ArrayDeque<>();
+        int ind=0;
+        for(int i = 0 ; i < n ; i++){
+
+            while(!q.isEmpty() && q.peekLast().val<=nums[i]){
+                q.pollLast();
+            }
+
+            if(q.isEmpty() || q.peekLast().val>nums[i]){
+                q.add(new Pair(i,nums[i]));
+            }
+
+            if(!q.isEmpty() && q.peekFirst().ind <= i-k){
+                q.pollFirst();
+            }
+
+            if(i>=k-1){
+                arr[ind++]=q.peekFirst().val;
+            }
+        }
+        return arr;
+    }
+}
