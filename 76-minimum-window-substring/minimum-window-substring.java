55e072056ede53047d4ad7594b68812face5ad7c
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-01-19 06:05:59 +0530

    Time: 18 ms (50.19%) | Memory: 47.5 MB (45.64%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,44 +1,40 @@
 class Solution {
     public String minWindow(String s, String t) {
-        if (t.length() > s.length()) return "";
-
-        int[] freq = new int[128];  // For all ASCII characters
-
-        // Step 1: Fill frequency of characters in t
-        for (char ch : t.toCharArray()) {
-            freq[ch]++;
+        int n = s.length();
+        HashMap<Character,Integer> map = new HashMap<>();
+        int count = 0;
+        int minlen = Integer.MAX_VALUE;
+        int l=0;
+        int r=0;
+        int start = 0;
+        for(int i = 0 ; i < t.length() ; i++){
+            map.put(t.charAt(i),map.getOrDefault(t.charAt(i),0)+1);
         }
 
-        int left = 0, right = 0;
-        int count = t.length();  // Total characters needed
-        int minLen = Integer.MAX_VALUE;
-        int start = 0;
+        while(r<n){
+            if(map.containsKey(s.charAt(r))){
+                if (map.get(s.charAt(r)) > 0) count++;
 
-        // Step 2: Start sliding window
-        while (right < s.length()) {
-            // Expand window
-            char rChar = s.charAt(right);
-            if (freq[rChar] > 0) count--;  // Needed char found
-            freq[rChar]--;  // Always decrement (even if negative)
-
-            // Step 3: Contract window from left if all required characters found
-            while (count == 0) {
-                // Update min window
-                if ((right - left + 1) < minLen) {
-                    minLen = right - left + 1;
-                    start = left;
-                }
-
-                // Shrink window
-                char lChar = s.charAt(left);
-                freq[lChar]++;  // Restore char
-                if (freq[lChar] > 0) count++;  // Lost a required char
-                left++;
+                map.put(s.charAt(r),map.get(s.charAt(r))-1); 
             }
 
-            right++;
+            while(count==t.length()){
+                if (r - l + 1 < minlen) {
+                    minlen = r - l + 1;
+                    start = l;
+                }
+
+                if (map.containsKey(s.charAt(l))) {
+                    map.put(s.charAt(l), map.get(s.charAt(l)) + 1);
+                    if (map.get(s.charAt(l)) > 0) count--; // break match
+                }
+                l++;
+            }
+
+            r++;
         }
 
-        return minLen == Integer.MAX_VALUE ? "" : s.substring(start, start + minLen);
+        if(minlen==Integer.MAX_VALUE)return "";
+        else return s.substring(start,start+minlen);
     }
 }
