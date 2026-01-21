eb6d3eb68dec5f1866a4eb32f6e823319ee7475d
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-01-21 20:16:58 +0530

    Time: 3 ms (15.68%) | Memory: 47.3 MB (24.80%) - LeetSync

--- 57-insert-interval/insert-interval.java
+++ 57-insert-interval/insert-interval.java
@@ -1,48 +1,33 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        int[][] newArr = new int[intervals.length+1][2];
-        if(intervals.length == 0 || intervals[0].length == 0){
-            newArr[0] = newInterval;
-            return newArr;
+        int n = intervals.length;
+        List<int[]> list1 = new ArrayList<>();
+        for(int[] arr : intervals){
+            list1.add(arr);
         }
-        int ind = intervals.length;
+        list1.add(newInterval);
 
-        for(int i = 0 ; i < intervals.length ; i++){
-            if(intervals[i][0] > newInterval[0]){
-                ind = i;
-                break;
-            }
-        }
+        Collections.sort(list1,(a,b) -> a[0]-b[0]);
 
-        for(int i = 0 ; i < ind ; i++){
-            newArr[i] = intervals[i];
-        }
-        newArr[ind] = newInterval;
-        for(int i = ind ; i < intervals.length ; i++){
-            newArr[i+1] = intervals[i];
-        }
+        List<int[]> list2 = new ArrayList<>();
+        list2.add(list1.get(0));
+        int last = list1.get(0)[1];
 
-        return merge(newArr);
-    }
+        for(int i = 1 ; i < list1.size() ; i++){
+            if(list1.get(i)[0]<=last){
+                list2.get(list2.size()-1)[1] = Math.max(list2.get(list2.size()-1)[1],list1.get(i)[1]);
+                last = list2.get(list2.size() - 1)[1];
+            }else if(list1.get(i)[0]<=last && list1.get(i)[1]<=last){
 
-    public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals , (a,b) -> Integer.compare(a[0],b[0]));
-        List<int[]> list = new ArrayList<>();
-        list.add(intervals[0]);
-
-        for(int i = 1 ; i < intervals.length ; i++){
-            int[] arr = list.get(list.size()-1);
-
-            if(arr[1] >= intervals[i][0]){
-                arr[1] = Math.max(arr[1],intervals[i][1]);
             }else{
-                list.add(intervals[i]);
+                list2.add(list1.get(i));
+                last = list1.get(i)[1];
             }
         }
 
-        int[][] res = new int[list.size()][2];
-        for(int i = 0 ; i < list.size() ; i++){
-            res[i] = list.get(i);
+        int[][] res = new int[list2.size()][2];
+        for(int i = 0 ; i < list2.size() ; i++){
+            res[i] = list2.get(i);
         }
         return res;
     }
