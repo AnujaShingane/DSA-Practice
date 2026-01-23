45afc22e4c0c27d1c962b8178512beda97693e09
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-01-23 12:25:49 +0530

    Time: 21 ms (23.90%) | Memory: 56.6 MB (30.15%) - LeetSync

--- 917-boats-to-save-people/boats-to-save-people.java
+++ 917-boats-to-save-people/boats-to-save-people.java
@@ -2,23 +2,25 @@ class Solution {
     public int numRescueBoats(int[] people, int limit) {
         int n = people.length;
         Arrays.sort(people);
+        int sum = 0;
         int i = 0;
-        int j = people.length-1;
-        int sum=0;
-        int cnt =0;
+        int j = n-1;
+        int cnt = 0;
 
         while(i<=j){
             sum = people[i]+people[j];
-            if(sum<=limit){
+
+            if(sum>limit){
                 cnt++;
-                i++;
                 j--;
             }else{
                 cnt++;
+                i++;
                 j--;
             }
             sum=0;
         }
+
         return cnt;
     }
 }
