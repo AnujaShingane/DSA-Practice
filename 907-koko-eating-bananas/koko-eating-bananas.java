6ca63330bdb33c463c68e736d3e661f754c93943
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-02-02 18:37:31 +0530

    Time: 12 ms (57.62%) | Memory: 48.4 MB (13.18%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -1,1 +1,39 @@
-class Solution { public int minEatingSpeed(int[] piles, int h) { int n = piles.length; int low = 1; int high = Integer.MIN_VALUE; for(int i = 0 ; i < n ; i++){ if(piles[i]>high)high=piles[i]; } int ans = -1; while(low<=high){ int mid = (low+high)/2; long findHrs = findHrs(piles,mid); if(findHrs<=h){ ans=mid; high=mid-1; }else{ low=mid+1; } } return ans; } public static long findHrs(int[] piles , int x){ long cnt = 0; for(int num : piles){ if(num<=x)cnt++; else{ int quo= num/x; int rem = num%x; cnt+=quo; if(rem!=0)cnt++; } } return cnt; } }
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int low = 1;
+        int high = Integer.MIN_VALUE;
+
+        for (int pile : piles) {
+            high = Math.max(high, pile);
+        }
+
+        int ans = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            long findHrs = findHrs(piles, mid);
+
+            if (findHrs <= h) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    public static long findHrs(int[] piles, int x) {
+        long cnt = 0;
+        for (int num : piles) {
+            if (num <= x) cnt++;
+            else {
+                int quo = num / x;
+                int rem = num % x;
+                cnt += quo;
+                if (rem != 0) cnt++;
+            }
+        }
+        return cnt;
+    }
+}
