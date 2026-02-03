d092aa8d017315f28cd2a2072639987bda770e6c
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-02-03 12:46:46 +0530

    Time: 12 ms (57.58%) | Memory: 48.1 MB (39.81%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -1,37 +1,38 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
+        int n = piles.length;
         int low = 1;
         int high = Integer.MIN_VALUE;
 
-        for (int pile : piles) {
-            high = Math.max(high, pile);
+        for(int num : piles){
+            if(num>high)high=num;
         }
 
         int ans = -1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            long hrsGot = hrs(piles,mid);
 
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            long findHrs = findHrs(piles, mid);
-
-            if (findHrs <= h) {
+            if(hrsGot<=h){
                 ans = mid;
-                high = mid - 1;
-            } else {
-                low = mid + 1;
+                high = mid-1;
+            }else{
+                low = mid+1;
             }
         }
         return ans;
     }
 
-    public static long findHrs(int[] piles, int x) {
+    public static long hrs(int[] piles,int bpm){
         long cnt = 0;
-        for (int num : piles) {
-            if (num <= x) cnt++;
-            else {
-                int quo = num / x;
-                int rem = num % x;
-                cnt += quo;
-                if (rem != 0) cnt++;
+
+        for(int num : piles){
+            if(num<=bpm)cnt++;
+            else{
+                int quo = num/bpm;
+                int rem = num%bpm;
+                cnt+=quo;
+                if(rem>0)cnt+=1;
             }
         }
         return cnt;
