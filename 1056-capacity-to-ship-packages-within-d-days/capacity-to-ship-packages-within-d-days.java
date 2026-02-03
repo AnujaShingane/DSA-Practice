45ddd4bd51485d03977b4adae294eaefe1958252
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-02-03 15:47:27 +0530

    Time: 11 ms (69.42%) | Memory: 50.3 MB (31.30%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
@@ -1,43 +1,46 @@
 class Solution {
-    public int shipWithinDays(int[] weights, int days) {
+    public int shipWithinDays(int[] weights, int days) {    
         int n = weights.length;
-        int low=Integer.MIN_VALUE; // max in arr
-        int high=0; // sum of array
-        for(int i = 0 ; i < n ; i++){
-            if(weights[i]>low)low=weights[i];
-            high+=weights[i];
-        }
+        int low = Integer.MIN_VALUE;  // max in array
+        int high = 0; //sum of all
         int ans = -1;
+
+        for(int num : weights){
+            low = Math.max(low,num);
+            high+=num;
+        }
 
         while(low<=high){
             int mid = (low+high)/2;
-            int findDays = findDays(weights,mid);
+            int daysGot = days(weights,mid);
 
-            if(findDays<=days){
+            if(daysGot<=days){
                 ans=mid;
                 high=mid-1;
             }else{
                 low=mid+1;
             }
         }
+
         return ans;
     }
 
-    public int findDays(int[] weights,int x){
-        int n = weights.length;
+    public static int days(int[] weights, int capacity){
         int cnt = 0;
+        int n = weights.length;
         int sum = 0;
         int i = 0;
 
         while(i<n){
             sum+=weights[i];
 
-            if(sum>x){
+            if(sum>capacity){
                 cnt++;
-                sum=weights[i];
+                sum = weights[i];
             }
             i++;
         }
+
         return cnt+1;
     }
 }
