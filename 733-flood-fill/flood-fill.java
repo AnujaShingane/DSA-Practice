6562cf544bc7917343001ba4651de698b4ef0d0c
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-02-05 15:23:14 +0530

    Time: 0 ms (100.00%) | Memory: 47 MB (21.73%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,50 +1,23 @@
-class Pair{
-    int i;
-    int j;
-
-    Pair(int i , int j ){
-        this.i = i;
-        this.j = j;
-    }
-}
-
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int n = image.length;
-        int m = image[0].length;
-        boolean[][] vis = new boolean[n][m];
+        int oldcolor = image[sr][sc];
+        if(oldcolor==color)return image;
 
-        int prevColor = image[sr][sc];
-        if (prevColor == color) return image; // important
-
-        bfs(n,m,sr,sc,prevColor,color,image,vis);
+        dfs(image,sr,sc,oldcolor,color);
         return image;
     }
 
-    public void bfs(int n , int m,int sr, int sc, int prevColor,int color,int[][] image,boolean[][] vis){
-        int prevcol = image[sr][sc];
-        Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(sr,sc));
-        vis[sr][sc] = true;
-        image[sr][sc] = color;
-        int[] delrow = {0,-1,0,1};
-        int[] delcol = {-1,0,1,0};
-
-        while(!q.isEmpty()){
-            Pair rem = q.poll();
-            int r = rem.i;
-            int c = rem.j;
-
-            for(int k = 0 ; k < 4 ; k++){
-                int nr = r+delrow[k];
-                int nc = c+delcol[k];
-                    
-                if(nr>=0 && nr<n && nc>=0 && nc<m && vis[nr][nc]==false && image[nr][nc]==prevcol){
-                    vis[nr][nc]=true;
-                    image[nr][nc]=color;
-                    q.add(new Pair(nr,nc));
-                }
-            }
+    public static void dfs(int[][] image,int sr,int sc,int oldcolor,int color){
+        if(sr<0 || sr>=image.length || sc<0 || sc>=image[0].length){
+            return;
         }
-    }
+        if(image[sr][sc] != oldcolor)return;
+
+        image[sr][sc] = color;
+
+        dfs(image,sr+1,sc,oldcolor,color);
+        dfs(image,sr,sc+1,oldcolor,color);
+        dfs(image,sr-1,sc,oldcolor,color);
+        dfs(image,sr,sc-1,oldcolor,color);
+    } 
 }
