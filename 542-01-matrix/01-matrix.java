d0dc106c0c5530ab2a599576ec6a61cadd9f8cd1
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-02-06 15:47:37 +0530

    Time: 14 ms (76.44%) | Memory: 50.1 MB (5.62%) - LeetSync

--- 542-01-matrix/01-matrix.java
+++ 542-01-matrix/01-matrix.java
@@ -1,68 +1,39 @@
-import java.util.*;
-
-class Pair {
-    int i, j;
-    Pair(int i, int j) {
-        this.i = i;
-        this.j = j;
-    }
-}
-
-class PairDist {
-    Pair p;
-    int dist;
-    PairDist(Pair p, int dist) {
-        this.p = p;
-        this.dist = dist;
-    }
-}
-
-
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
         int n = mat.length;
         int m = mat[0].length;
 
-        boolean[][] vis = new boolean[n][m];
-        Queue<PairDist> q = new LinkedList<>();
-
         int[][] ans = new int[n][m];
+        boolean[][] vis = new boolean[n][m];
+
+        Queue<int[]> q = new LinkedList<>();
         for(int i = 0 ; i < n ; i++){
             for(int j = 0 ; j < m ; j++){
-                ans[i][j] = 0;
-            }
-        }
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (mat[i][j] == 0) {
-                    vis[i][j] = true;
-                    q.add(new PairDist(new Pair(i, j), 0));
-                    ans[i][j]= 0;
+                if(mat[i][j]==0){
+                    ans[i][j]=0;
+                    q.add(new int[]{i,j,0});
+                    vis[i][j]=true;
                 }
             }
         }
 
-        int[] delr = {-1, 0, 1, 0};
-        int[] delc = {0, 1, 0, -1};
+        int[][] dir = {{0,-1},{-1,0},{0,1},{1,0}};
 
-        while (!q.isEmpty()) {
-            PairDist pd = q.poll();
-            int row = pd.p.i;
-            int col = pd.p.j;
-            int dist = pd.dist;
+        while(!q.isEmpty()){
+            int[] arr = q.poll();
+            int r = arr[0];
+            int c = arr[1];
+            int dist = arr[2];
 
-            for (int k = 0; k < 4; k++) {
-                int dr = row + delr[k];
-                int dc = col + delc[k];
+            for(int[] d : dir){
+                int nr = r + d[0];
+                int nc = c + d[1];
 
-                if (dr >= 0 && dr < n && dc >= 0 && dc < m &&
-                    !vis[dr][dc] && mat[dr][dc] == 1) {
+                if(nr<0 || nc<0 || nr>=n || nc>=m || vis[nr][nc])continue;
 
-                    vis[dr][dc] = true;
-                    ans[dr][dc] =dist + 1;
-                    q.add(new PairDist(new Pair(dr, dc), dist + 1));
-                }
+                vis[nr][nc]=true;
+                ans[nr][nc]=dist+1;
+                q.add(new int[]{nr,nc,dist+1});
             }
         }
 
