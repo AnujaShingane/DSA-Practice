d24e72a4219951b422fdc9d4ad0f6c4c21f87071
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-02-06 17:32:38 +0530

    Time: 2 ms (91.09%) | Memory: 44.4 MB (24.38%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,75 +1,49 @@
-class Pair{
-    int i;
-    int j;
-
-    Pair(int i , int j ){
-        this.i = i;
-        this.j = j;
-    }
-}
-
-class PairTime{
-    Pair p;
-    int time;
-
-    PairTime(Pair p,int time){
-        this.p = p;
-        this.time = time;
-    }
-}
-
 class Solution {
     public int orangesRotting(int[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
 
-        Queue<PairTime> q = new LinkedList<>();
-        int[][] vis = new int[n][m];
-
-        for(int ii = 0 ; ii < n ; ii++){
-            for(int jj = 0 ; jj < m ; jj++){
-                vis[ii][jj] = grid[ii][jj];
-            }
-        }
+        int[][] ans = new int[n][m];
+        Queue<int[]> q = new LinkedList<>();
 
         for(int i = 0 ; i < n ; i++){
             for(int j = 0 ; j < m ; j++){
-                if(grid[i][j] == 2){
-                    q.add(new PairTime(new Pair(i,j),0));
-                    vis[i][j] = 2;
+                if(grid[i][j]==2){
+                    q.add(new int[]{i,j,0});
                 }
+                ans[i][j]=grid[i][j];
             }
         }
 
-        int maxt = 0;
-        int[] delr = {-1, 0, 1, 0};
-        int[] delc = {0, 1, 0, -1};
+        int[][] dir = {{0,-1},{-1,0},{0,1},{1,0}};
 
+        int min = 0;
         while(!q.isEmpty()){
-            PairTime rem = q.poll();
-            Pair p = rem.p;
-            int row = p.i;
-            int col = p.j;
-            int time = rem.time;
+            int[] arr = q.poll();
+            int r = arr[0];
+            int c = arr[1];
+            int time = arr[2];
 
-            maxt = Math.max(maxt,time);
+            for(int[] d : dir){
+                int nr = r + d[0];
+                int nc = c + d[1];
 
-            for(int i = 0 ; i < 4 ; i++){
-                int dr = row + delr[i];
-                int dc = col + delc[i];
-
-                if(dr>=0 && dr<n && dc>=0 && dc<m && vis[dr][dc]==1){
-                    vis[dr][dc] = 2;
-                    q.add(new PairTime(new Pair(dr,dc),time+1));
+                if(nr<0 || nc<0 || nr>=n || nc>=m || ans[nr][nc]==0 || ans[nr][nc]==2){
+                    continue;
                 }
+
+                ans[nr][nc] = 2;
+                q.add(new int[]{nr,nc,time+1});
+                min = Math.max(min,time+1);
+            }
+        }
+        
+        for(int[] array : ans){
+            for(int ele : array){
+                if(ele==1)return -1;
             }
         }
 
-        for(int i = 0 ; i < n ; i++){
-            for(int j = 0 ; j < m ; j++){
-                if(vis[i][j]==1)return -1;
-            }
-        }
-        return maxt;
+        return min;
     }
 }
