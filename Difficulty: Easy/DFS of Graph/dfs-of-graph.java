2dd63e10143b61601e8b8c1cdd521b9015ddae14
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-02-06 19:11:59 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/DFS of Graph/dfs-of-graph.java
+++ Difficulty: Easy/DFS of Graph/dfs-of-graph.java
@@ -1,56 +1,22 @@
-//{ Driver Code Starts
-// Initial Template for Java
-import java.io.*;
-import java.lang.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine().trim());
-        while (T-- > 0) {
-            String[] s = br.readLine().trim().split(" ");
-            int V = Integer.parseInt(s[0]);
-            int E = Integer.parseInt(s[1]);
-            ArrayList<ArrayList<Integer>> adj = new ArrayList<ArrayList<Integer>>();
-            for (int i = 0; i < V; i++) adj.add(new ArrayList<Integer>());
-            for (int i = 0; i < E; i++) {
-                String[] S = br.readLine().trim().split(" ");
-                int u = Integer.parseInt(S[0]);
-                int v = Integer.parseInt(S[1]);
-                adj.get(u).add(v);
-                adj.get(v).add(u);
-            }
-            Solution obj = new Solution();
-            ArrayList<Integer> ans = obj.dfsOfGraph(adj);
-            for (int i = 0; i < ans.size(); i++) System.out.print(ans.get(i) + " ");
-            System.out.println();
-
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
 class Solution {
-    public static void dfs(int i,ArrayList<ArrayList<Integer>> adj,ArrayList<Integer>result,boolean[] vis){
-        vis[i] = true;
-        result.add(i);
+    public ArrayList<Integer> dfs(ArrayList<ArrayList<Integer>> adj) {
+        int n = adj.size();
+        ArrayList<Integer> ans = new ArrayList<>();
+        int[] vis = new int[n];
         
-        for(int ele : adj.get(i)){
-            if(vis[ele] == false){
-                dfs(ele,adj,result,vis);
+        dfs(0,adj,vis,ans);
+        
+        return ans;
+    }
+    
+    public void dfs(int node,ArrayList<ArrayList<Integer>> adj,int[] vis,ArrayList<Integer> ans) {
+        vis[node]=1;
+        ans.add(node);
+        
+        for(int ele : adj.get(node)){
+            if(vis[ele]==0){
+                dfs(ele,adj,vis,ans);
             }
         }
-    }
-
-    public ArrayList<Integer> dfsOfGraph(ArrayList<ArrayList<Integer>> adj) {
-        ArrayList<Integer>result = new ArrayList<>();
-        boolean[] vis = new boolean[adj.size()];
-        vis[0] = true;
-        dfs(0,adj,result,vis);
-        return result;
     }
 }
