aaf0f77658e9e29b1ee1d6d8bee423b98390746f
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-02-07 21:56:41 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (27.07%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
@@ -16,16 +16,15 @@
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> result = new ArrayList<>();
-        inorderHelper(root,result);
+        inorderTraversal(root,result);
         return result;
     }
 
-    public void inorderHelper(TreeNode root,List<Integer> result){
-        if(root == null){
-            return;
-        }
-        inorderHelper(root.left,result);
-        result.add(root.val);
-        inorderHelper(root.right,result);        
+    public void inorderTraversal(TreeNode node,List<Integer> result){
+        if(node==null)return;
+
+        inorderTraversal(node.left,result);
+        result.add(node.val);
+        inorderTraversal(node.right,result);
     }
 }
