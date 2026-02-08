5e8041986ce48b8035101b3ae20a6b793f326b79
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-02-08 12:56:20 +0530

    Time: 0 ms (100.00%) | Memory: 45.7 MB (57.98%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.java
+++ 110-balanced-binary-tree/balanced-binary-tree.java
@@ -18,16 +18,15 @@ class Solution {
         return height(root)!=-1;
     }
 
-    public int height(TreeNode node){
-        if(node==null)return 0;
+    public int height(TreeNode root) {
+        if(root==null)return 0;
 
-        int lh = height(node.left);
+        int lh = height(root.left);
         if(lh==-1)return -1;
-        int rh = height(node.right);
+        int rh = height(root.right);
         if(rh==-1)return -1;
 
         if(Math.abs(lh-rh)>1)return -1;
-
         return 1+Math.max(lh,rh);
     }
 }
