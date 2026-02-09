8fec1a0de420877d67b97ec70b42bc83b9ac966c
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-02-09 19:47:56 +0530

    Time: 30 ms (7.40%) | Memory: 50.8 MB (83.13%) - LeetSync

--- 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.java
+++ 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.java
@@ -2,14 +2,15 @@ class Solution {
     public int kthSmallest(int[][] matrix, int k) {
         PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
 
-        for(int i = 0 ; i < matrix.length ; i++){
-            for(int j = 0 ; j < matrix[0].length ; j++){
-                pq.add(matrix[i][j]);
-                if(pq.size() > k){
+        for(int[] arr : matrix){
+            for(int ele : arr){
+                pq.add(ele);
+                if(pq.size()>k){
                     pq.poll();
                 }
             }
         }
+
         return pq.peek();
     }
 }
