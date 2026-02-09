f646a225af33ce4e2d57f894389c624694480c5b
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-02-09 21:04:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Merge k Sorted Arrays/merge-k-sorted-arrays.java
+++ Difficulty: Medium/Merge k Sorted Arrays/merge-k-sorted-arrays.java
@@ -1,57 +1,18 @@
-//{ Driver Code Starts
-//Initial Template for Java
-
-import java.util.*;
-import java.io.*;
-
-class GFG{
-	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
-		int t = sc.nextInt();
-		while(t > 0){
-			int n = sc.nextInt();
-			int[][] a = new int[n][n];
-			for(int i = 0; i < n; i++)
-				for(int j = 0; j < n; j++)
-					a[i][j] = sc.nextInt();
-			Solution T = new Solution();
-			ArrayList<Integer> arr= T.mergeKArrays(a, n);
-			for(int i = 0; i < n*n ; i++)
-			    System.out.print(arr.get(i)+" ");
-		    System.out.println();
-		    
-		    t--;
-		
-System.out.println("~");
-}
-	}
-}
-
-
-// } Driver Code Ends
-
-
-//User function Template for Java
-
-
-class Solution
-{
-    //Function to merge k sorted arrays.
-    public static ArrayList<Integer> mergeKArrays(int[][] arr,int K){
-        int n = arr.length;
-        ArrayList<Integer> list = new ArrayList<>();
+class Solution {
+    public ArrayList<Integer> mergeArrays(int[][] mat) {
+        ArrayList<Integer> res = new ArrayList<>();
         PriorityQueue<Integer> pq = new PriorityQueue<>();
         
-        for(int i = 0 ; i < n ; i++){
-            for(int j = 0 ; j < n ; j++){
-                pq.add(arr[i][j]);
+        for(int[] arr : mat){
+            for(int ele : arr){
+                pq.add(ele);
             }
         }
         
-        while(pq.size() != 0){
-            list.add(pq.remove());
+        while(!pq.isEmpty()){
+            res.add(pq.poll());
         }
         
-        return list;
+        return res;
     }
 }
