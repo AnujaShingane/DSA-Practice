8ad55b9553c0ad611cb7966fb72188759c2b94b9
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-02-12 17:55:35 +0530

    Time: 113 ms (11.61%) | Memory: 85.6 MB (10.44%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -1,69 +1,60 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
         int n = heights.length;
-        int[] JNleftG = new int[n];
-        int[] JNrightG = new int[n];
         int maxArea = 0;
+        int[] JNleftGreater = new int[n];
+        int[] JNrightGreater = new int[n];
 
-        JNleftG = justNextLeftGreater(heights);
-        JNrightG = justNextRightGreater(heights);
+        JNleftGreater = JNleftGreaterr(heights);
+        JNrightGreater = JNrightGreaterr(heights);
 
         for(int i = 0 ; i < n ; i++){
-            int width = JNleftG[i]+JNrightG[i]+1;
-
+            int width = JNleftGreater[i]+JNrightGreater[i]+1;
             maxArea = Math.max(maxArea,width*heights[i]);
         }
 
         return maxArea;
     }
 
-    /*
-    func ex :-
-
-    for -> 2 1 5 6 2 3 
-    JNleftG -> 0 1 0 0 1 0
-    JNrightG -> 0 4 1 0 1 0
-    */
-
-    public int[] justNextLeftGreater(int[] heights) {
+    public static int[] JNleftGreaterr(int[] heights){
         int n = heights.length;
-        int[] left = new int[n];
         Stack<Integer> st = new Stack<>();
+        int[] res = new int[n];
 
-        for (int i = 0; i < n; i++) {
-            while (!st.isEmpty() && heights[st.peek()] >= heights[i]) {
+        for(int i = 0 ; i < n ; i++){
+            while(!st.isEmpty() && heights[st.peek()]>=heights[i]){
                 st.pop();
             }
 
-            if (st.isEmpty())
-                left[i] = i;          // can extend till index 0
-            else
-                left[i] = i - st.peek() - 1;
-
+            if(st.isEmpty()){
+                res[i] = i;
+            }else{
+                res[i] = i-st.peek()-1;
+            }
             st.push(i);
         }
-        return left;
+        return res;
     }
 
-
-    public int[] justNextRightGreater(int[] heights) {
+    public static int[] JNrightGreaterr(int[] heights){
         int n = heights.length;
-        int[] right = new int[n];
+        int[] res = new int[n];
         Stack<Integer> st = new Stack<>();
 
-        for (int i = n - 1; i >= 0; i--) {
-            while (!st.isEmpty() && heights[st.peek()] >= heights[i]) {
+        for(int i = n-1 ; i >= 0 ; i--){
+            while(!st.isEmpty() && heights[st.peek()]>=heights[i]){
                 st.pop();
             }
 
-            if (st.isEmpty())
-                right[i] = n - 1 - i;     // can extend till end
-            else
-                right[i] = st.peek() - i - 1;
+            if(st.isEmpty()){
+                res[i] = n-i-1;
+            }else{
+                res[i] = st.peek()-i-1;
+            }
 
             st.push(i);
         }
-        return right;
-    }
 
+        return res;
+    }
 }
