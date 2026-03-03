9879e42f5bab4ea608fadd627eebc7e4dcd3ac00
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-03-03 14:12:16 +0530

    Time: 2 ms (91.08%) | Memory: 44 MB (92.34%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -3,8 +3,8 @@ class Pair{
     int j;
 
     Pair(int i,int j){
-        this.i=i;
-        this.j=j;
+        this.i = i;
+        this.j = j;
     }
 }
 
@@ -12,8 +12,8 @@ class Solution {
     public int orangesRotting(int[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
-        boolean[][] vis = new boolean[n][m];
         Queue<Pair> q = new LinkedList<>();
+        boolean[][] vis = new boolean[n][m];
         int fresh = 0;
         int min = 0;
 
@@ -28,14 +28,14 @@ class Solution {
             }
         }
 
-        if(fresh==0)return 0;
+        if(fresh == 0)return 0;
 
         int[] di = {-1,0,1,0};
         int[] dj = {0,1,0,-1};
 
         while(!q.isEmpty()){
             int size = q.size();
-            boolean rotted = false;
+            boolean rotten = false;
 
             while(size--!=0){
                 Pair p = q.poll();
@@ -48,14 +48,13 @@ class Solution {
 
                     if(newi>=0 && newi<n && newj>=0 && newj<m && !vis[newi][newj] && grid[newi][newj]==1){
                         vis[newi][newj]=true;
-                        grid[newi][newj]=2;
                         q.add(new Pair(newi,newj));
+                        rotten = true;
                         fresh--;
-                        rotted=true;
                     }
                 }
             }
-            if(rotted)min++;
+            if(rotten)min++;
         }
 
         return (fresh==0) ? min : -1;
