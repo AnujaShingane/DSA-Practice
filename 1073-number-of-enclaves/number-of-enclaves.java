38cb16e848b9e8d38625ddf631bf069bf4ebc8e7
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-03-03 17:34:21 +0530

    Time: 6 ms (98.99%) | Memory: 62 MB (21.49%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.java
+++ 1073-number-of-enclaves/number-of-enclaves.java
@@ -2,68 +2,41 @@ class Solution {
     public int numEnclaves(int[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
-        boolean[][] vis = new boolean[n][m];
 
-        //hori boundries
         for(int i = 0 ; i < m ; i++){
-            if(grid[0][i] == 1){
-                vis[0][i] = true;
-                bfs(n,m,0,i,vis,grid); // here mark all vis
-            }
-
-            if(grid[n-1][i] == 1){
-                vis[n-1][i] = true;
-                bfs(n,m,n-1,i,vis,grid); // here mark all vis
-            }
+            if(grid[0][i]==1)dfs(0,i,n,m,grid);
         }
 
-        //vert boundries
-        for(int i = 0 ; i < n ; i++){
-            if(grid[i][0] == 1){
-                vis[i][0] = true;
-                bfs(n,m,i,0,vis,grid); // here mark all vis
-            }
+        for(int j = 0 ; j < n ; j++){
+            if(grid[j][m-1]==1)dfs(j,m-1,n,m,grid);
+        }
 
-            if(grid[i][m-1] == 1){
-                vis[i][m-1] = true;
-                bfs(n,m,i,m-1,vis,grid); // here mark all vis
-            }
+        for(int k = 0 ; k < m ; k++){
+            if(grid[n-1][k]==1)dfs(n-1,k,n,m,grid);
+        }
+
+        for(int l = 0 ; l < n ; l++){
+            if(grid[l][0]==1)dfs(l,0,n,m,grid);
         }
 
         int cnt = 0;
-        for(int i = 0 ; i < n ; i++){
-            for(int j = 0 ; j < m ; j++){
-                if(grid[i][j] == 1 && !vis[i][j]){
-                    cnt++;
-                }
+        for(int[] arr : grid){
+            for(int num : arr){
+                if(num==1)cnt++;
             }
         }
+
         return cnt;
     }
 
-    public void bfs(int n,int m,int i,int j,boolean[][] vis,int[][] grid){
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{i,j});
+    public void dfs(int r,int c,int n,int m,int[][] grid) {
+        if(r<0 || r>=n || c<0 || c>=m || grid[r][c]==0)return;
 
-        int[] delr = {-1, 0, 1, 0};
-        int[] delc = {0, 1, 0, -1};
+        grid[r][c] = 0;
 
-        while(!q.isEmpty()){
-            int[] rem = q.poll();
-            int row = rem[0];
-            int col = rem[1];
-
-            for(int r = 0 ; r < 4 ; r++){
-                int nr = row + delr[r];
-                int nc = col + delc[r];
-
-                if (nr >= 0 && nr < n && nc >= 0 && nc < m &&
-                    grid[nr][nc] == 1 && !vis[nr][nc]) {
-                    
-                    vis[nr][nc]=true;
-                    q.add(new int[]{nr,nc});
-                }
-            }
-        }
+        dfs(r+1,c,n,m,grid);
+        dfs(r-1,c,n,m,grid);
+        dfs(r,c+1,n,m,grid);
+        dfs(r,c-1,n,m,grid);
     }
 }
