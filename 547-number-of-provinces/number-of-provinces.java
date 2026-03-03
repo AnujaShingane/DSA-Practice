5df3717535057acff64d1509ee93187d576296d0
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-03-03 09:51:02 +0530

    Time: 3 ms (30.94%) | Memory: 50 MB (7.13%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,48 +1,50 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
-        int n = isConnected[0].length;
+        int n = isConnected.length;
+        int m = isConnected[0].length;
+
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        adj = adjecencyList(isConnected,adj);
+
         boolean[] vis = new boolean[n];
         int cnt = 0;
 
-        ArrayList<ArrayList<Integer>> adj = adj(n, isConnected);
-
         for(int i = 0 ; i < n ; i++){
-            if(vis[i]==false){
+            if(!vis[i]){
                 cnt++;
-                bfs(i,vis,isConnected,adj);
+                dfs(i,adj,vis);
             }
         }
+
         return cnt;
     }
 
-    public void bfs(int start, boolean[] vis,int[][] isConnected,ArrayList<ArrayList<Integer>> adj){
-        Queue<Integer> q = new LinkedList<>();
-        q.add(start);
-        vis[start]=true;
+    public void dfs(int node,ArrayList<ArrayList<Integer>> adj,boolean[] vis){
+        vis[node]=true;
 
-        while(!q.isEmpty()){
-            int rem = q.poll();
-            for(int num : adj.get(rem)){
-                if(vis[num]==false){
-                    vis[num]=true;
-                    q.add(num);
-                }
+        for(int ele : adj.get(node)){
+            if(!vis[ele]){
+                dfs(ele,adj,vis);
             }
         }
     }
 
-    public ArrayList<ArrayList<Integer>> adj(int n, int[][] isConnected){
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+    public ArrayList<ArrayList<Integer>> adjecencyList(int[][] isConnected,ArrayList<ArrayList<Integer>> adj){
+        int n = isConnected.length;
+        int m = isConnected[0].length;
 
-        for(int i = 0 ; i < n ; i++)adj.add(new ArrayList<>());
+        for(int z = 0 ; z < n ; z++){
+            adj.add(new ArrayList<>());
+        }
 
         for(int i = 0 ; i < n ; i++){
-            for(int j = 0 ; j < n ; j++){
-                if(isConnected[i][j]==1 && i!=j){
+            for(int j = 0 ; j < m ; j++){
+                if(i!=j && isConnected[i][j]==1){
                     adj.get(i).add(j);
                 }
             }
         }
+
         return adj;
     }
 }
