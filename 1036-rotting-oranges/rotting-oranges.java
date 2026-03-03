8c232239fa493a3ebbcb46c3ba70b63d7d11676f
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-03-03 11:26:30 +0530

    Time: 2 ms (91.08%) | Memory: 44.3 MB (40.32%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,49 +1,63 @@
+class Pair{
+    int i;
+    int j;
+
+    Pair(int i,int j){
+        this.i=i;
+        this.j=j;
+    }
+}
+
 class Solution {
     public int orangesRotting(int[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
-
-        int[][] ans = new int[n][m];
-        Queue<int[]> q = new LinkedList<>();
+        boolean[][] vis = new boolean[n][m];
+        Queue<Pair> q = new LinkedList<>();
+        int fresh = 0;
+        int min = 0;
 
         for(int i = 0 ; i < n ; i++){
             for(int j = 0 ; j < m ; j++){
                 if(grid[i][j]==2){
-                    q.add(new int[]{i,j,0});
+                    q.add(new Pair(i,j));
                 }
-                ans[i][j]=grid[i][j];
+                if(grid[i][j]==1){
+                    fresh++;
+                }
             }
         }
 
-        int[][] dir = {{0,-1},{-1,0},{0,1},{1,0}};
+        if(fresh==0)return 0;
 
-        int min = 0;
+        int[] di = {-1,0,1,0};
+        int[] dj = {0,1,0,-1};
+
         while(!q.isEmpty()){
-            int[] arr = q.poll();
-            int r = arr[0];
-            int c = arr[1];
-            int time = arr[2];
+            int size = q.size();
+            boolean rotted = false;
 
-            for(int[] d : dir){
-                int nr = r + d[0];
-                int nc = c + d[1];
+            while(size--!=0){
+                Pair p = q.poll();
+                int i = p.i;
+                int j = p.j;
 
-                if(nr<0 || nc<0 || nr>=n || nc>=m || ans[nr][nc]==0 || ans[nr][nc]==2){
-                    continue;
+                for(int z = 0 ; z < 4 ; z++){
+                    int newi = i+di[z];
+                    int newj = j+dj[z];
+
+                    if(newi>=0 && newi<n && newj>=0 && newj<m && !vis[newi][newj] && grid[newi][newj]==1){
+                        vis[newi][newj]=true;
+                        grid[newi][newj]=2;
+                        q.add(new Pair(newi,newj));
+                        fresh--;
+                        rotted=true;
+                    }
                 }
-
-                ans[nr][nc] = 2;
-                q.add(new int[]{nr,nc,time+1});
-                min = Math.max(min,time+1);
             }
-        }
-        
-        for(int[] array : ans){
-            for(int ele : array){
-                if(ele==1)return -1;
-            }
+            if(rotted)min++;
         }
 
-        return min;
+        return (fresh==0) ? min : -1;
     }
 }
