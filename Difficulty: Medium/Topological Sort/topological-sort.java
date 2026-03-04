ed3503241e8e4bd778ca003082c7a109778e9613
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-03-04 21:38:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Topological Sort/topological-sort.java
+++ Difficulty: Medium/Topological Sort/topological-sort.java
@@ -1,9 +1,8 @@
 class Solution {
     public ArrayList<Integer> topoSort(int V, int[][] edges) {
-        Stack<Integer> st = new Stack<>();
+        ArrayList<ArrayList<Integer>> adj = adjecencyList(V,edges);
         boolean[] vis = new boolean[V];
-        
-        ArrayList<ArrayList<Integer>> adj = adj(V,edges);
+        Stack<Integer> st = new Stack<>();
         
         for(int i = 0 ; i < V ; i++){
             if(!vis[i]){
@@ -15,27 +14,32 @@ class Solution {
         while(!st.isEmpty()){
             ans.add(st.pop());
         }
-        return ans;
-    }
-    
-    public ArrayList<ArrayList<Integer>> adj(int V,int[][] edges){
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        for(int i=0 ; i < V ; i++)adj.add(new ArrayList<>());
         
-        for(int[] arr : edges){
-            adj.get(arr[0]).add(arr[1]);
-        }
-        return adj;
+        return ans;
     }
     
     public void dfs(int node,boolean[] vis,ArrayList<ArrayList<Integer>> adj,Stack<Integer> st){
         vis[node]=true;
+        
         for(int ele : adj.get(node)){
             if(!vis[ele]){
                 dfs(ele,vis,adj,st);
-                vis[ele]=true;
             }
         }
-        st.push(node);//backtracking
-    } 
+        
+        st.push(node);
+    }
+    
+    public ArrayList<ArrayList<Integer>> adjecencyList(int V,int[][] edges){
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for(int i = 0 ; i < V ; i++){
+            adj.add(new ArrayList<>());
+        }
+        
+        for(int[] arr : edges){
+            adj.get(arr[0]).add(arr[1]);
+        }
+        
+        return adj;
+    }
 }
