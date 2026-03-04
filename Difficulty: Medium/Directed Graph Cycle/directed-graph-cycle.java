285a044fa1938339907c45a8b4048e39b62602e0
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-03-04 21:01:23 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.java
+++ Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.java
@@ -1,42 +1,45 @@
 class Solution {
     public boolean isCyclic(int V, int[][] edges) {
+        ArrayList<ArrayList<Integer>> adj = adjecencyList(V,edges);
+        boolean[] vis = new boolean[V];
+        boolean[] pathVis = new boolean[V];
+        
+        for(int i = 0 ; i < V ; i++){
+            if(!vis[i]){
+                if(dfs(i,-1,vis,pathVis,adj))return true;
+            }
+        }
+        
+        return false;
+    }
+    
+    public boolean dfs(int node,int parent, boolean[] vis, boolean[] pathVis,ArrayList<ArrayList<Integer>> adj){
+        vis[node]=true;
+        pathVis[node] = true;
+        
+        for(int ele : adj.get(node)){
+            if(!vis[ele]){
+                if(dfs(ele,node,vis,pathVis,adj))return true;
+            }
+            else if(vis[ele] && pathVis[ele]){
+                return true;
+            }
+        }
+        
+        pathVis[node]=false;
+        return false;
+    }
+    
+    public ArrayList<ArrayList<Integer>> adjecencyList(int V,int[][] edges){
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
         for(int i = 0 ; i < V ; i++){
             adj.add(new ArrayList<>());
         }
-        adj = adjList(V,edges,adj);
         
-        int[] vis = new int[V];
-        int[] pathVis = new int[V];
-        
-        for(int i = 0 ; i < V ; i++){
-            if(dfs(i,adj,vis,pathVis))return true;
-        }
-        return false;
-    }
-    
-    public boolean dfs(int node, ArrayList<ArrayList<Integer>> adj,int[] vis , int[] pathVis) {
-        vis[node]=1;
-        pathVis[node]=1;
-        
-        //traverse for all node
-        for(int ele : adj.get(node)){
-            if(vis[ele]==0){
-                if(dfs(ele,adj,vis,pathVis))return true;
-            }else{
-                if(pathVis[ele]==1)return true;
-            }
-        }
-        
-        pathVis[node]=0; //backtrack // making 0 while returning
-        return false;
-    }
-    
-    public ArrayList<ArrayList<Integer>> adjList(int n, int[][] edges,ArrayList<ArrayList<Integer>> adj) {
         for(int[] arr : edges){
             adj.get(arr[0]).add(arr[1]);
         }
-
+        
         return adj;
     }
 }
