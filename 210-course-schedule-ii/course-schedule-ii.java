ef5086802152f72c51675eec5f9fd5c0e62b8f6d
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-03-04 23:06:33 +0530

    Time: 5 ms (82.05%) | Memory: 48.1 MB (20.05%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -1,50 +1,62 @@
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        ArrayList<Integer> topoSort = topoSort(numCourses,prerequisites);
-        int[] ans = new int[topoSort.size()];
-        for(int i = 0 ; i < topoSort.size() ; i++){
-            ans[i]=topoSort.get(i);
-        }
-        
-        if(topoSort.size()!=numCourses)return new int[]{};
+        ArrayList<ArrayList<Integer>> adj = adjecencyList(numCourses,prerequisites);
+        int[] ans = topoSort(numCourses,prerequisites);
+
         return ans;
     }
 
-    public ArrayList<Integer> topoSort(int V, int[][] edges) {
-        int[] indegree = new int[V];
-        Queue<Integer> q = new LinkedList<>();
-        ArrayList<ArrayList<Integer>> adj = adj(V,edges);
+    public int[] topoSort(int numCourses, int[][] prerequisites) {
+        ArrayList<ArrayList<Integer>> adj = adjecencyList(numCourses,prerequisites);
+        boolean[] vis = new boolean[numCourses];
+        boolean[] pathVis = new boolean[numCourses];
+        Stack<Integer> st = new Stack<>();
         
-        for(List<Integer> list : adj){
-            for(int ele : list){
-                indegree[ele]++;
+        for(int i = 0 ; i < numCourses ; i++){
+            if(!vis[i]){
+                if(dfs(i,-1,vis,pathVis,adj,st)){
+                    return new int[0];
+                }
             }
         }
         
-        for(int i = 0 ; i < V ; i++){
-            if(indegree[i]==0)q.add(i);
+        int[] ans = new int[numCourses];
+        int i = 0;
+        while(!st.isEmpty()){
+            ans[i++] = st.pop();
         }
-        
-        ArrayList<Integer> ans = new ArrayList<>();
-        while(!q.isEmpty()){
-            int ele = q.poll();
-            ans.add(ele);
-            
-            for(int ele2 : adj.get(ele)){
-                indegree[ele2]--;
-                if(indegree[ele2]==0)q.add(ele2);
-            }
-        }
+
         return ans;
     }
     
-    public ArrayList<ArrayList<Integer>> adj(int V,int[][] edges){
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        for(int i=0 ; i < V ; i++)adj.add(new ArrayList<>());
+    public boolean dfs(int node,int parent, boolean[] vis, boolean[] pathVis,ArrayList<ArrayList<Integer>> adj,Stack<Integer> st){
+        vis[node]=true;
+        pathVis[node] = true;
         
-        for(int[] arr : edges){
+        for(int ele : adj.get(node)){
+            if(!vis[ele]){
+                if(dfs(ele,node,vis,pathVis,adj,st))return true;
+            }
+            else if(vis[ele] && pathVis[ele]){
+                return true;
+            }
+        }
+        
+        pathVis[node]=false;
+        st.push(node);
+        return false;
+    }
+
+    public ArrayList<ArrayList<Integer>> adjecencyList(int numCourses,int[][] prerequisites){
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for(int i = 0 ; i < numCourses ; i++){
+            adj.add(new ArrayList<>());
+        }
+
+        for(int[] arr : prerequisites){
             adj.get(arr[1]).add(arr[0]);
         }
+
         return adj;
     }
 }
