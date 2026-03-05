3bd452011be3161c9d0a7f6b2ee7f1db7a50bcc4
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-03-05 19:43:33 +0530

    Time: 3 ms (23.74%) | Memory: 46.6 MB (17.24%) - LeetSync

--- 684-redundant-connection/redundant-connection.java
+++ 684-redundant-connection/redundant-connection.java
@@ -1,37 +1,78 @@
 class Solution {
 
-    int[] parent;
+    List<Integer> cycle = new ArrayList<>();
 
     public int[] findRedundantConnection(int[][] edges) {
+        int V = edges.length;
+        ArrayList<ArrayList<Integer>> adj = adjacencyList(V, edges);
+        List<Integer> list = findCycle(V, adj);
 
-        int n = edges.length;
-        parent = new int[n + 1];
+        for(int i = V-1 ; i >= 0 ; i--){
+            int u = edges[i][0];
+            int v = edges[i][1];
 
-        for(int i = 1; i <= n; i++){
-            parent[i] = i;
-        }
-
-        for(int[] edge : edges){
-
-            int u = edge[0];
-            int v = edge[1];
-
-            int pu = find(u);
-            int pv = find(v);
-
-            if(pu == pv){
-                return edge;
+            if(list.contains(u) && list.contains(v)){
+                return new int[]{u,v};
             }
-
-            parent[pu] = pv;
         }
 
-        return new int[0];
+        return new int[]{};
     }
 
-    public int find(int x){
-        if(parent[x] == x) return x;
+    public List<Integer> findCycle(int V, ArrayList<ArrayList<Integer>> adj) {
+        boolean[] vis = new boolean[V+1];
+        int[] parent = new int[V+1];
+        Arrays.fill(parent, -1);
 
-        return parent[x] = find(parent[x]);
+        for(int i = 1; i <= V; i++){
+            if(!vis[i]){
+                if(dfs(i, -1, vis, parent, adj)){
+                    return cycle;
+                }
+            }
+        }
+
+        return new ArrayList<>();
+    }
+
+    public boolean dfs(int node, int par, boolean[] vis, int[] parent,
+                       ArrayList<ArrayList<Integer>> adj){
+
+        vis[node] = true;
+        for(int ele : adj.get(node)){
+            if(!vis[ele]){
+                parent[ele] = node;
+                if(dfs(ele, node, vis, parent, adj))return true;
+            }
+            else if(ele != par){
+                cycle.add(ele);
+                int cur = node;
+
+                while(cur != ele){
+                    cycle.add(cur);
+                    cur = parent[cur];
+                }
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public ArrayList<ArrayList<Integer>> adjacencyList(int V,int[][] edges){
+
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+
+        for(int i = 0 ; i <= V ; i++){
+            adj.add(new ArrayList<>());
+        }
+
+        for(int[] arr : edges){
+
+            adj.get(arr[0]).add(arr[1]);
+            adj.get(arr[1]).add(arr[0]);
+        }
+
+        return adj;
     }
 }
