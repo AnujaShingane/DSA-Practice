0ff03e5369388ce87405e090373c1c7cf163257a
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-03-19 23:23:20 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Dijkstra Algorithm/dijkstra-algorithm.java
+++ Difficulty: Medium/Dijkstra Algorithm/dijkstra-algorithm.java
@@ -1,63 +1,63 @@
 class Pair{
-    int dist;
     int node;
+    int wt;
     
-    Pair(int dist, int node){
+    Pair(int node,int wt){
         this.node = node;
-        this.dist = dist;
+        this.wt = wt;
     }
 }
 
 class Solution {
     public int[] dijkstra(int V, int[][] edges, int src) {
+        ArrayList<ArrayList<Pair>> adj = adjecencyList(V,edges);
         PriorityQueue<Pair> pq = new PriorityQueue<>(
-            (a,b) -> a.dist-b.dist
+            (a,b) -> a.wt-b.wt
         );
-        
         int[] dist = new int[V];
         for(int i = 0 ; i < V ; i++){
-            dist[i]=(int)(1e9);
+            dist[i] = (int)(1e9);
         }
+        
+        pq.add(new Pair(src,0));
         dist[src]=0;
-        pq.add(new Pair(0,src));
-        
-        ArrayList<ArrayList<Pair>> adj = new ArrayList<>();
-        for(int i = 0 ; i < V ; i++){
-            adj.add(new ArrayList<>());
-        }
-        
-        for(int[] arr : edges){
-            int u = arr[0];
-            int v = arr[1];
-            int wt = arr[2];
-            
-            adj.get(u).add(new Pair(wt,v));
-            adj.get(v).add(new Pair(wt,u));
-        }
         
         while(!pq.isEmpty()){
             Pair p = pq.poll();
-            int dis = p.dist;
             int n = p.node;
+            int dis = p.wt;
             
             for(Pair ele : adj.get(n)){
-                int adjNodeDist = ele.dist;
-                int adjNode = ele.node;
+                int node = ele.node;
+                int distance = ele.wt;
+                int finalDist = dis+distance;
                 
-                int finalDist = adjNodeDist + dis;
-                if(finalDist<dist[adjNode]){
-                    dist[adjNode]=finalDist;
-                    pq.add(new Pair(finalDist,adjNode));
+                if(dist[node]>finalDist){
+                    dist[node] = finalDist;
+                    pq.add(new Pair(node,finalDist));
                 }
             }
         }
         
         for(int i = 0 ; i < V ; i++){
             if(dist[i]==(int)(1e9)){
-                dist[i] = -1;
+                dist[i]=-1;
             }
         }
         
         return dist;
+    }
+    
+    public ArrayList<ArrayList<Pair>> adjecencyList(int V , int[][] edges){
+        ArrayList<ArrayList<Pair>> adj = new ArrayList<>();
+        for(int i = 0 ; i < V ; i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int[] arr : edges){
+            adj.get(arr[0]).add(new Pair(arr[1],arr[2]));
+            adj.get(arr[1]).add(new Pair(arr[0],arr[2]));
+        }
+        
+        return adj;
     }
 }
