2d7b3eaa8f3e297c287514b48b81a23dc80f65ce
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-03-25 20:01:07 +0530

    Time: 94 ms (21.78%) | Memory: 72.7 MB (30.39%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,44 +1,32 @@
 class Solution {
+    Boolean[][] dp;
+
     public boolean canPartition(int[] nums) {
         int n = nums.length;
         int sum = 0;
 
-        for(int i = 0 ; i < n ; i++){
-            sum+=nums[i];
-        }
-        if(sum%2!=0)return false;
-        int target = sum/2;
+        for(int num : nums) sum += num;
 
-        boolean[][] dp = new boolean[n][target+1];
+        if(sum % 2 != 0) return false;
 
-        if(target>=nums[0])dp[0][nums[0]]=true;
-        for(int i = 0 ; i < n ; i++)dp[i][0]=true;
+        int target = sum / 2;
+        dp = new Boolean[n][target + 1];
 
-        for(int i = 1 ; i < n ; i++){
-            for(int tar = 1 ; tar <= target ; tar++){
-                boolean take = false;
-                if(tar>=nums[i])take=dp[i-1][tar-nums[i]];
-
-                boolean nottake = dp[i-1][tar];
-
-                dp[i][tar]=take || nottake;
-            }
-        }
-
-        return dp[n-1][target];
+        return f(n - 1, target, nums);
     }
 
-    // public boolean func(int i,int target,int[] nums,Boolean[][] dp){
-    //     if(target==0)return true;//dp[i][0]=true
-    //     if(i==0)return target==nums[0];//dp[0][nums[0]]=true
+    boolean f(int i, int target, int[] arr){
+        if(target == 0) return true;
+        if(i == 0) return arr[0] == target;
 
-    //     if(dp[i][target]!=null)return dp[i][target];
+        if(dp[i][target] != null) return dp[i][target];
 
-    //     boolean take = false;
-    //     if(target>=nums[i])take=func(i-1,target-nums[i],nums,dp);
+        boolean notTake = f(i - 1, target, arr);
 
-    //     boolean nottake = func(i-1,target,nums,dp);
+        boolean take = false;
+        if(arr[i] <= target)
+            take = f(i - 1, target - arr[i], arr);
 
-    //     return dp[i][target]=take || nottake;
-    // }
+        return dp[i][target] = take || notTake;
+    }
 }
