4d08d9482c35af20feb98d2db0c6ca4b65863321
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-03-25 23:03:39 +0530

    Time: 92 ms (23.22%) | Memory: 66.9 MB (38.93%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,25 +1,27 @@
 class Solution {
     Boolean[][] dp;
-
     public boolean canPartition(int[] nums) {
         int n = nums.length;
         int sum = 0;
 
-        for(int num : nums) sum += num;
+        for(int num : nums){
+            sum += num;
+        }
+        int target = sum/2;
+        dp = new Boolean[n][target+1];
 
+        // Step 1: If sum is odd → not possible
         if(sum % 2 != 0) return false;
 
-        int target = sum / 2;
-        dp = new Boolean[n][target + 1];
-
-        return f(n - 1, target, nums);
+        // Step 2: Target = sum / 2
+        return f(n - 1, sum / 2, nums);
     }
-
-    boolean f(int i, int target, int[] arr){
+    
+    public boolean f(int i, int target, int[] arr){
         if(target == 0) return true;
         if(i == 0) return arr[0] == target;
 
-        if(dp[i][target] != null) return dp[i][target];
+        if(dp[i][target]!=null)return dp[i][target];
 
         boolean notTake = f(i - 1, target, arr);
 
