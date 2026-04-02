5977e97ae5db4c71b01316f4cd619b5dd122f350
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-04-02 21:04:55 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subset Sum Problem/subset-sum-problem.java
+++ Difficulty: Medium/Subset Sum Problem/subset-sum-problem.java
@@ -1,20 +1,19 @@
 class Solution {
-
     static Boolean isSubsetSum(int arr[], int sum) {
         int n = arr.length;
-        Boolean[][] dp = new Boolean[n][sum+1];
-        return f(n-1,sum,arr,dp);
+        Boolean[][] dp = new Boolean[n+1][sum+1];
+        return func(n-1,sum,arr,dp);
     }
     
-    public static boolean f(int ind,int target,int[] arr,Boolean[][] dp){
+    static boolean func(int ind,int target,int[] arr,Boolean[][] dp){
         if(target==0)return true;
-        if(ind==0)return arr[0]==target;
+        if(ind==0)return arr[ind]==target;
         
         if(dp[ind][target]!=null)return dp[ind][target];
         
-        boolean nottake = f(ind-1,target,arr,dp);
+        boolean nottake = func(ind-1,target,arr,dp);
         boolean take = false;
-        if(target>=arr[ind])take = f(ind-1,target-arr[ind],arr,dp);
+        if(arr[ind]<=target)take = func(ind-1,target-arr[ind],arr,dp);
         
         return dp[ind][target] = take || nottake;
     }
