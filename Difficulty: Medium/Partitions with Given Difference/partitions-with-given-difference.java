9ca9e93bdfbb00820dcec0ee05dcce9a94435f95
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-04-04 00:49:40 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Partitions with Given Difference/partitions-with-given-difference.java
+++ Difficulty: Medium/Partitions with Given Difference/partitions-with-given-difference.java
@@ -1,35 +1,37 @@
 class Solution {
-    static int mod = (int) (1e9 + 7);
-    public int countPartitions(int[] arr, int diff) {
-        int n = arr.length;
-        int totalSum=0;
-        for(int i = 0 ; i < n ; i++)totalSum+=arr[i];
+    public int countPartitions(int[] nums, int diff) {
+        int n = nums.length;
+        int sum = 0;
+        for(int ele : nums){
+            sum+=ele;
+        }
         
-        if(totalSum < diff || (totalSum + diff) % 2 != 0)
-            return 0;
-            
-        int target = (totalSum+diff)/2;
-        
-        int[][] dp = new int[n][target+1];    
-        for(int[] ar : dp)Arrays.fill(ar,-1);
-        
-        return func(n-1,target,diff,arr,dp);
-    }
-    
-    public int func(int i,int target,int diff,int[] arr,int[][] dp) {
-        if(i==0){
-            if(target==0 && arr[0]==0)return 2;
-            if(target==0 || target==arr[0])return 1;
+        if ((diff + sum) % 2 != 0 || Math.abs(diff) > sum) {
             return 0;
         }
         
-        if(dp[i][target]!=-1)return dp[i][target];
+        int target = (diff + sum)/2;
+        int[][] dp = new int[n][target+1];
+        for(int[] arr : dp){
+            Arrays.fill(arr,-1);
+        }
         
+        return func(n-1,nums,target,dp);
+    }
+    
+    public int func(int ind,int[] nums,int target,int[][] dp) {
+        if(ind==0){
+            if(target==0 && nums[0]==0)return 2;
+            if(target==0 || nums[0]==target) return 1;
+            else return 0;
+        }
+        
+        if(dp[ind][target]!=-1)return dp[ind][target];
+
+        int nottake = func(ind-1,nums,target,dp);
         int take = 0;
-        if(target>=arr[i])take = func(i-1,target-arr[i],diff,arr,dp);
-        
-        int nottake = func(i-1,target,diff,arr,dp);
-        
-        return dp[i][target]=(take+nottake)%mod;
+        if(target>=nums[ind])take = func(ind-1,nums,target-nums[ind],dp);
+
+        return dp[ind][target] = take + nottake;
     }
 }
