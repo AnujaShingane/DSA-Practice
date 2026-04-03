cfaf58e2824fa3a3d7695db5c4a56756343c17e8
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-04-03 12:02:54 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Perfect Sum Problem/perfect-sum-problem.java
+++ Difficulty: Medium/Perfect Sum Problem/perfect-sum-problem.java
@@ -1,27 +1,27 @@
 class Solution {
-    public static int mod = (int)(1e9+7);
-    // Function to calculate the number of subsets with a given sum
     public int perfectSum(int[] nums, int target) {
         int n = nums.length;
         int[][] dp = new int[n][target+1];
-        for(int[] arr : dp)Arrays.fill(arr,-1);
+        for(int[] arr : dp){
+            Arrays.fill(arr,-1);
+        }
         
         return func(n-1,nums,target,dp);
     }
     
-    public int func(int i,int[] nums,int target,int[][] dp){
-        if(i<0){
-            if(target==0)return 1;
-            else return 0;
+    public int func(int ind,int[] nums,int target,int[][] dp) {
+        if(ind==0){
+            if(target==0 && nums[0] == 0)return 2;
+            if(target == 0 || target == nums[0])return 1;
+            return 0;
         }
         
-        if(dp[i][target]!=-1)return dp[i][target];
+        if(dp[ind][target]!=-1)return dp[ind][target];
         
+        int nottake = func(ind-1,nums,target,dp);
         int take = 0;
-        if(target>=nums[i])take = func(i-1,nums,target-nums[i],dp);
+        if(target>=nums[ind])take = func(ind-1,nums,target-nums[ind],dp);
         
-        int nottake = func(i-1,nums,target,dp);
-        
-        return dp[i][target]=((take)%mod+(nottake)%mod)%mod;
+        return dp[ind][target] = take+nottake;
     }
 }
