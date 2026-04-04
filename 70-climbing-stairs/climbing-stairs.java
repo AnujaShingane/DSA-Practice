a277a292e359b1c4b8d16332be73383ccd6fe75c
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-04-04 20:23:13 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (9.67%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -2,15 +2,19 @@ class Solution {
     public int climbStairs(int n) {
         int[] dp = new int[n+1];
         Arrays.fill(dp,-1);
+
         return func(n,dp);
     }
 
-    public int func(int n,int[] dp){
-        if(n==0)return 1;
-        if(n<0)return 0;
-        if(dp[n]!= -1)return dp[n];
-        int one = func(n-1,dp);
-        int two = func(n-2,dp);
-        return dp[n]=one + two;
+    public int func(int ind,int[] dp) {
+        if(ind == 0)return 1;
+        if(ind==1)return 1;
+
+        if(dp[ind]!= -1)return dp[ind];
+
+        int left = func(ind-1,dp);
+        int right = func(ind-2,dp);
+
+        return dp[ind] = left + right;
     }
 }
