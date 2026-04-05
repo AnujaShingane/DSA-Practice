6dd28be55d0718d88b4520b9f4d389aba7593152
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-04-05 13:30:10 +0530

    Time: 83 ms (28.88%) | Memory: 72.7 MB (27.09%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -2,27 +2,26 @@ class Solution {
     public boolean canPartition(int[] nums) {
         int n = nums.length;
         int sum = 0;
-        for(int ele : nums)sum+=ele;
+        for(int ele : nums){
+            sum+=ele;
+        }
         if(sum%2!=0)return false;
         int target = sum/2;
-        boolean[][] dp = new boolean[n][sum+1];
+        Boolean[][] dp = new Boolean[n][target+1]; 
 
-        for(int i = 0 ; i < n ; i++){
-            dp[i][0] = true;
-        }
+        return func(n-1,nums,target,dp);
+    }
 
-        if(target>=nums[0])dp[0][nums[0]] = true;
+    public boolean func(int ind,int[] nums,int target,Boolean[][] dp) {
+        if(target == 0)return true;
+        if(ind==0)return nums[0]==target;
 
-        for(int ind = 1 ; ind < n ; ind++){
-            for(int t = 0 ; t <= sum ; t++){
-                boolean nottake = dp[ind-1][t];
-                boolean take = false;
-                if(nums[ind]<= t)take = dp[ind-1][t-nums[ind]];
+        if(dp[ind][target]!=null)return dp[ind][target];
 
-                dp[ind][t] = take || nottake;
-            }
-        }
+        boolean take = false;
+        if(nums[ind]<=target)take = func(ind-1,nums,target-nums[ind],dp);
+        boolean nottake = func(ind-1,nums,target,dp);
 
-        return dp[n-1][target];
+        return dp[ind][target] = take || nottake;
     }
 }
