57e1208950bffd3858cfb1d4ad2cfea01bbc276e
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-04-05 23:48:28 +0530

    Time: 16 ms (35.63%) | Memory: 57.4 MB (18.76%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -2,24 +2,31 @@ class Solution {
     public int change(int amount, int[] coins) {
         int n = coins.length;
         int[][] dp = new int[n][amount+1];
-        for(int[] arr : dp)Arrays.fill(arr,-1);
-
-        return func(n-1,amount,coins, dp);
+        for(int[] arr1 : dp){
+            Arrays.fill(arr1,-1);
+        }
+        
+        int ans = func(n-1,coins,amount,dp);
+        return ans;
     }
 
-    public int func(int i,int amt, int[] coins,int[][] dp) {
-        if(i==0){
-            if(amt%coins[i]==0)return 1;
-            return 0;
+    static int func(int ind,int[] num,int amt,int[][] dp) {
+        if(amt==0){
+            return 1;
         }
 
-        if(dp[i][amt]!=-1)return dp[i][amt];
+        if(ind==0){
+            if(amt==num[0])return 1;
+        }
+
+        if(ind<0)return 0;
+
+        if(dp[ind][amt]!=-1)return dp[ind][amt];
 
         int take = 0;
-        if(amt>=coins[i])take = func(i,amt-coins[i],coins,dp);
+        if(num[ind]<= amt)take = func(ind,num,amt-num[ind],dp);
+        int nottake = func(ind-1,num,amt,dp);
 
-        int nottake = func(i-1,amt,coins,dp);
-
-        return dp[i][amt]=take+nottake;
+        return dp[ind][amt] = take + nottake;
     }
 }
