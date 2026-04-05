7da0cf2064ca01dd1733b659616bfe2c79d9460d
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-04-05 23:40:04 +0530

    Time: 26 ms (28.66%) | Memory: 46.8 MB (38.00%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,32 +1,33 @@
-import java.util.*;
-
 class Solution {
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
         int[][] dp = new int[n][amount+1];
-        
-        for(int[] row : dp){
-            Arrays.fill(row, -1);
+        for(int[] arr1 : dp){
+            Arrays.fill(arr1,-1);
         }
         
-        int ans = func(n-1, coins, amount, dp);
+        int ans = func(n-1,coins,amount,dp);
         return ans >= (int)1e9 ? -1 : ans;
     }
 
-    static int func(int ind, int[] num, int amt, int[][] dp) {
-        if(ind == 0){
-            if(amt % num[0] == 0) return amt / num[0];
+    static int func(int ind,int[] num,int amt,int[][] dp) {
+        // if(ind==0){
+        //     if(tar==0 && num[0]==0)return 2;
+        //     if(tar==0 || num[0]==tar)return 1;
+        //     else return 0;
+        // }
+
+        if(ind==0){
+            if(amt%num[0]==0)return amt/num[0];
             else return (int)1e9;
         }
 
-        if(dp[ind][amt] != -1) return dp[ind][amt];
+        if(dp[ind][amt]!=-1)return dp[ind][amt];
 
         int take = (int)1e9;
-        if(num[ind] <= amt)
-            take = 1 + func(ind, num, amt - num[ind], dp);
+        if(num[ind]<= amt)take = 1 + func(ind,num,amt-num[ind],dp);
+        int nottake = func(ind-1,num,amt,dp);
 
-        int nottake = func(ind-1, num, amt, dp);
-
-        return dp[ind][amt] = Math.min(take, nottake);
+        return dp[ind][amt] = Math.min(take,nottake);
     }
 }
