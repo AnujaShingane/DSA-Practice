f1faaa2ebbf6ff2b8e0e7b81f905813ce0dffcac
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-04-06 12:40:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest Common Subsequence/longest-common-subsequence.java
@@ -0,0 +1,28 @@
+class Solution {
+    static int match = 0;
+    static int notmatch = 0;
+    
+    static int lcs(String s1, String s2) {
+        int n1 = s1.length();
+        int n2 = s2.length();
+        int[][] dp = new int[n1+1][n2+1];
+        for(int[] arr : dp){
+            Arrays.fill(arr,-1);
+        }
+        
+        return func(n1-1,n2-1,s1,s2,dp);
+    }
+    
+    static int func(int ind1,int ind2,String s1,String s2,int[][] dp) {
+        if(ind1<0 || ind2<0)return 0;
+        
+        if(dp[ind1][ind2]!=-1)return dp[ind1][ind2];
+        
+        int match = 0;
+        if(s1.charAt(ind1)==s2.charAt(ind2)){
+            return dp[ind1][ind2] =  1 + func(ind1-1,ind2-1,s1,s2,dp);
+        }
+        
+        return dp[ind1][ind2] = Math.max(func(ind1-1,ind2,s1,s2,dp),func(ind1,ind2-1,s1,s2,dp));
+    }
+}
