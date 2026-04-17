1a52ddad94f59ac8c6d33f199e45187a74a2d58f
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-04-17 14:37:24 +0530

    Time: 73 ms (84.39%) | Memory: 98.4 MB (51.15%) - LeetSync

---
+++ 883-car-fleet/car-fleet.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        int n = position.length;
+        int[][] arr = new int[n][2];
+
+        for(int i = 0 ; i < n ; i++){
+            arr[i][0] = position[i];
+            arr[i][1] = speed[i];
+        }
+        
+        Arrays.sort(arr,(a,b)->b[0]-a[0]);
+
+        double bdy = (double)(target-arr[0][0])/arr[0][1];
+        int cnt = 1;
+
+        for(int i = 1 ; i < n ; i++){
+            double num = (double)(target-arr[i][0])/arr[i][1];
+            if(num>bdy){
+                cnt++;
+                bdy = num;
+            }
+        }
+
+        return cnt;
+    }
+}
