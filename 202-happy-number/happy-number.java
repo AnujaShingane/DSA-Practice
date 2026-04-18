28294e658b21eaf425448bdd21caec2856939238
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-04-18 14:14:08 +0530

    Time: 3 ms (3.46%) | Memory: 43 MB (7.05%) - LeetSync

--- 202-happy-number/happy-number.java
+++ 202-happy-number/happy-number.java
@@ -3,21 +3,18 @@ class Solution {
         String s = String.valueOf(n);
         StringBuilder sb = new StringBuilder();
 
-        HashSet<String> set = new HashSet<>();
-
+        int sum = 0;
         int ans = 0;
-
-        while (!s.equals("1") && !set.contains(s)) {
+        HashSet<String> set = new HashSet<>(); 
+        while(!s.equals("1") && !set.contains(s)){
             set.add(s);
 
-            for (char ch : s.toCharArray()) {
+            for(char ch : s.toCharArray()){
                 int num = ch - '0';
                 ans += num * num;
             }
-
             sb.append(ans);
             s = sb.toString();
-
             ans = 0;
             sb.setLength(0);
         }
