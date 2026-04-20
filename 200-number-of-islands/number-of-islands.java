11691d66f8f56cf7f11dadd7defd541a6f79ae1e
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-04-20 12:07:40 +0530

    Time: 3 ms (86.54%) | Memory: 52.5 MB (24.38%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -2,13 +2,12 @@ class Solution {
     public int numIslands(char[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
-        boolean[][] vis = new boolean[n][m];
         int cnt = 0;
 
         for(int i = 0 ; i < n ; i++){
             for(int j = 0 ; j < m ; j++){
-                if(grid[i][j]=='1' && !vis[i][j]){
-                    dfs(i,j,n,m,vis,grid);
+                if(grid[i][j] == '1'){
+                    dfs(i,j,n,m,grid);
                     cnt++;
                 }
             }
@@ -17,14 +16,14 @@ class Solution {
         return cnt;
     }
 
-    public void dfs(int i,int j,int n , int m,boolean[][] vis,char[][] grid){
-        if(i<0 || i>=n || j<0 || j>=m || vis[i][j] || grid[i][j]=='0')return;
+    public void dfs(int i, int j ,int n , int m, char[][] grid){
+        if(i<0 || i>=n || j<0 || j>=m || grid[i][j] == '2' || grid[i][j]=='0')return;
 
-        vis[i][j]=true;
+        grid[i][j] = '2';
 
-        dfs(i+1,j,n,m,vis,grid);
-        dfs(i-1,j,n,m,vis,grid);
-        dfs(i,j+1,n,m,vis,grid);
-        dfs(i,j-1,n,m,vis,grid);
+        dfs(i-1,j,n,m,grid);
+        dfs(i,j-1,n,m,grid);
+        dfs(i,j+1,n,m,grid);
+        dfs(i+1,j,n,m,grid);
     }
 }
