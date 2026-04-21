5c6f4819f8fd2d1c46e3ee8f5eb99b4b356b102b
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-04-21 16:34:09 +0530

    Time: 14 ms (38.77%) | Memory: 48.7 MB (82.55%) - LeetSync

--- 744-network-delay-time/network-delay-time.java
+++ 744-network-delay-time/network-delay-time.java
@@ -1,26 +1,68 @@
 class Solution {
     public int networkDelayTime(int[][] times, int n, int k) {
-        int[] dist = new int[n+1];
-        Arrays.fill(dist,(int)(1e8));
-        dist[k]=0;
+        ArrayList<ArrayList<Pair>> adj = adjList(n,times);
+        int[] arr = dijkstra(n,adj,k);
+        int max = 0;
 
-        for(int i = 0 ; i < n-1 ; i++){
-            for(int[] edge : times){
-                int u = edge[0];
-                int v = edge[1];
-                int wt = edge[2];
+        for(int i=1;i<=n;i++){
+            if(arr[i]==Integer.MAX_VALUE) return -1;
+            max = Math.max(max, arr[i]);
+        }
 
-                if(dist[u]!=(int)(1e8) && dist[u]+wt<dist[v]){
-                    dist[v]=dist[u]+wt;
+        return max;
+    }
+
+    class Pair {
+        int node, dist;
+        Pair(int n, int d) {
+            node = n;
+            dist = d;
+        }
+    }
+
+    public int[] dijkstra(int n1, ArrayList<ArrayList<Pair>> adj, int src) {
+        int[] dist = new int[n1+1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[src] = 0;
+
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> a.dist - b.dist);
+        pq.offer(new Pair(src, 0));
+
+        while (!pq.isEmpty()) {
+            Pair curr = pq.poll();
+            int node = curr.node;
+            int d = curr.dist;
+
+            if(d>dist[node])continue;
+
+            //relaxation of edges
+            for (Pair nei : adj.get(node)) {
+                int n = nei.node;
+                int dis = nei.dist;
+                int newDist = dis+d;
+
+                if(newDist<dist[n]){
+                    dist[n] = newDist;
+                    pq.offer(new Pair(n,newDist));
                 }
             }
         }
+        return dist;
+    }
 
-        int ans = Integer.MIN_VALUE;
-        for(int i = 1 ; i < n+1 ; i++){
-            if(dist[i]==(int)(1e8))return -1;
-            ans = Math.max(ans,dist[i]);
+
+    public ArrayList<ArrayList<Pair>> adjList(int n, int[][] times) {
+
+        ArrayList<ArrayList<Pair>> adj = new ArrayList<>();
+
+        for (int i = 0; i <= n; i++) {
+            adj.add(new ArrayList<>());
         }
-        return ans;
+
+        for (int[] arr : times) {
+            adj.get(arr[0]).add(new Pair(arr[1],arr[2]));
+        }
+
+        return adj;
     }
 }
