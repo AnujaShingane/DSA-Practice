fd7fee9199a6e11d38998a4cef004e63298457d7
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-04-27 22:18:27 +0530

    Time: 2 ms (90.31%) | Memory: 44 MB (84.28%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,8 +1,7 @@
 class Pair{
-    int i;
-    int j;
+    int i, j;
 
-    Pair(int i,int j){
+    Pair(int i, int j){
         this.i = i;
         this.j = j;
     }
@@ -10,53 +9,54 @@ class Pair{
 
 class Solution {
     public int orangesRotting(int[][] grid) {
+
         int n = grid.length;
         int m = grid[0].length;
+
         Queue<Pair> q = new LinkedList<>();
-        boolean[][] vis = new boolean[n][m];
         int fresh = 0;
         int min = 0;
 
-        for(int i = 0 ; i < n ; i++){
-            for(int j = 0 ; j < m ; j++){
-                if(grid[i][j]==2){
-                    q.add(new Pair(i,j));
-                }
-                if(grid[i][j]==1){
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+
+                if(grid[i][j] == 2)
+                    q.offer(new Pair(i,j));
+
+                else if(grid[i][j] == 1)
                     fresh++;
-                }
             }
         }
-
-        if(fresh == 0)return 0;
 
         int[] di = {-1,0,1,0};
         int[] dj = {0,1,0,-1};
 
-        while(!q.isEmpty()){
+        while(!q.isEmpty() && fresh > 0){
+
             int size = q.size();
-            boolean rotten = false;
 
-            while(size--!=0){
+            while(size-- > 0){
+
                 Pair p = q.poll();
-                int i = p.i;
-                int j = p.j;
 
-                for(int z = 0 ; z < 4 ; z++){
-                    int newi = i+di[z];
-                    int newj = j+dj[z];
+                for(int k = 0; k < 4; k++){
 
-                    if(newi>=0 && newi<n && newj>=0 && newj<m && !vis[newi][newj] && grid[newi][newj]==1){
-                        vis[newi][newj]=true;
-                        q.add(new Pair(newi,newj));
-                        rotten = true;
+                    int ni = p.i + di[k];
+                    int nj = p.j + dj[k];
+
+                    if(ni>=0 && ni<n && nj>=0 && nj<m && grid[ni][nj]==1){
+
+                        grid[ni][nj] = 2; // mark rotten
                         fresh--;
+
+                        q.offer(new Pair(ni,nj));
                     }
                 }
             }
-            if(rotten)min++;
+
+            min++;
         }
 
-        return (fresh==0) ? min : -1;
+        return fresh == 0 ? min : -1;
     }
 }
