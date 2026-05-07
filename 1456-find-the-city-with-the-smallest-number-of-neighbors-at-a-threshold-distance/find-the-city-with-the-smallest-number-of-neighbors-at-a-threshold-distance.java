c61eca60036757aa35884cb47f6efe2a5e398994
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-05-08 00:44:34 +0530

    Time: 8 ms (88.65%) | Memory: 46.5 MB (59.33%) - LeetSync

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
@@ -1,39 +1,29 @@
 class Solution {
     public int findTheCity(int n, int[][] edges, int distanceThreshold) {
-        int[][] mat = shortestDist(n,edges);
+        int[][] mat = shortestDistance(n , edges);
         int cnt = 0;
         int minCnt = Integer.MAX_VALUE;
         int city = 0;
 
-        for(int i = 0 ; i < mat.length ; i++){
-            for(int j = 0 ; j < mat[0].length ; j++){
-                if(mat[i][j]<=distanceThreshold){
-                    cnt++;
-                }
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < n ; j++){
+                if(mat[i][j]<=distanceThreshold)cnt++;
             }
             if(cnt<=minCnt){
                 minCnt = cnt;
                 city = i;
             }
-            cnt = 0;
+            cnt=0;
         }
 
         return city;
     }
 
-    public int[][] shortestDist(int n, int[][] edges){
-        int n1 = edges.length;
-        int n2 = edges[0].length;
+    public int[][] shortestDistance(int n , int[][] edges){
         int[][] matrix = new int[n][n];
 
         for(int[] arr : matrix){
-            Arrays.fill(arr,(int)1e9);
-        }
-
-        for(int i = 0 ; i < n ; i++){
-            for(int j = 0 ; j < n ;j++){
-                if(i==j)matrix[i][j]=0;
-            }
+            Arrays.fill(arr,(int)(1e9));
         }
 
         for(int[] edge : edges){
@@ -45,14 +35,18 @@ class Solution {
             matrix[v][u] = wt;
         }
 
+        for(int i = 0 ; i < n ; i++){
+            matrix[i][i] = 0;
+        }
+
         for(int via = 0 ; via < n ; via++){
             for(int i = 0 ; i < n ; i++){
                 for(int j = 0 ; j < n ; j++){
-                    matrix[i][j] = Math.min(matrix[i][j],matrix[i][via]+matrix[via][j]);
+                    matrix[i][j] = Math.min(matrix[i][j], matrix[i][via]+matrix[via][j]);
                 }
             }
         }
 
         return matrix;
-    }
+    } 
 }
