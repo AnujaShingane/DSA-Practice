2d5f689c88d390e7430639c3dcba16ea250b1e85
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-05-10 11:00:29 +0530

    Time: 4 ms (17.25%) | Memory: 46.9 MB (93.01%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,50 +1,60 @@
+import java.util.*;
+
+class DisjointSet{
+  ArrayList<Integer> rank = new ArrayList<>();
+  ArrayList<Integer> parent = new ArrayList<>();
+  
+  DisjointSet(int n){
+    for(int i = 0 ; i <= n ; i++){
+      rank.add(0);
+      parent.add(i);
+    }
+  }
+  
+  public int findUPar(int node){
+    if(node == parent.get(node))return node;
+    
+    int ulp = findUPar(parent.get(node));
+    parent.set(node,ulp);
+    return parent.get(node);
+  }
+  
+  public void unionByParent(int u , int v){
+    int ulp_u = findUPar(u);
+    int ulp_v = findUPar(v);
+    
+    if(ulp_v==ulp_u)return;
+    
+    if(rank.get(ulp_u)<rank.get(ulp_v)){
+      parent.set(ulp_u,ulp_v);
+    }else if(rank.get(ulp_u)>rank.get(ulp_v)){
+      parent.set(ulp_v,ulp_u);
+    }else{
+      parent.set(ulp_u,ulp_v);
+      rank.set(ulp_u,rank.get(ulp_u)+1);
+    }
+  }
+}
+
 class Solution {
     public int findCircleNum(int[][] isConnected) {
         int n = isConnected.length;
-        int m = isConnected[0].length;
-
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        adj = adjecencyList(isConnected,adj);
-
-        boolean[] vis = new boolean[n];
-        int cnt = 0;
+        DisjointSet ds = new DisjointSet(n+1);
 
         for(int i = 0 ; i < n ; i++){
-            if(!vis[i]){
+            int node = i+1;
+            for(int j = 0 ; j < isConnected[0].length ; j++){
+                if(isConnected[i][j]==1)ds.unionByParent(node,j+1);
+            }
+        }
+
+        int cnt = 0;
+        for(int i = 1 ; i <= n ; i++){
+            if(ds.findUPar(i) == i){
                 cnt++;
-                dfs(i,adj,vis);
             }
         }
 
         return cnt;
-    }
-
-    public void dfs(int node,ArrayList<ArrayList<Integer>> adj,boolean[] vis){
-        vis[node]=true;
-
-        for(int ele : adj.get(node)){
-            if(!vis[ele]){
-                dfs(ele,adj,vis);
-            }
-        }
-    }
-
-    public ArrayList<ArrayList<Integer>> adjecencyList(int[][] isConnected,ArrayList<ArrayList<Integer>> adj){
-        int n = isConnected.length;
-        int m = isConnected[0].length;
-
-        for(int z = 0 ; z < n ; z++){
-            adj.add(new ArrayList<>());
-        }
-
-        for(int i = 0 ; i < n ; i++){
-            for(int j = 0 ; j < m ; j++){
-                if(i!=j && isConnected[i][j]==1){
-                    adj.get(i).add(j);
-                }
-            }
-        }
-
-        return adj;
     }
 }
