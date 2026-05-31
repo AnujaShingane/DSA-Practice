83b577cd094008ce5f5b27f2316bbb91fa69ee69
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-05-31 23:24:11 +0530

    Time: 1 ms (13.68%) | Memory: 42.3 MB (27.62%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,17 +1,21 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        int[][] dp = new int[m][n];
-
-        for(int i = 0 ; i < m ; i++)dp[i][0] = 1;
-        for(int j = 0 ; j < n ; j++)dp[0][j] = 1;
-
-        for(int i = 1 ; i < m ; i++){
-            for(int j = 1; j < n ; j++){
-                int up=dp[i-1][j];
-                int down = dp[i][j-1];
-                dp[i][j] = up+down;
-            }
+        int[][] dp = new int[m+1][n+1];
+        for(int[] row : dp){
+            Arrays.fill(row,-1);
         }
-        return dp[m-1][n-1];
+
+        return func(m-1,n-1,dp,m,n);
+    }
+
+    public int func(int i,int j,int[][] dp,int m , int n) {
+        if(i<0 || i>=m || j<0 || j>=n)return 0;
+        if(i==0 && j==0)return 1;
+        if(dp[i][j]!=-1)return dp[i][j];
+
+        int left = func(i,j-1,dp,m,n);
+        int top = func(i-1,j,dp,m,n);
+
+        return dp[i][j] = left+top;
     }
 }
