978daae3af5ccfbb0703ecf9da418d7f04a67c3b
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-05-31 23:01:13 +0530

    Time: 1 ms (99.91%) | Memory: 50.2 MB (70.50%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -5,7 +5,7 @@ class Solution {
 
         int[][] dp = new int[n+1][m+1];
         for(int[] row : dp){
-            Arrays.fill(row,Integer.MAX_VALUE);
+            Arrays.fill(row,(int)1e9);
         }
 
         return func(n-1,m-1,grid,dp);
@@ -14,7 +14,7 @@ class Solution {
     public int func(int i,int j,int[][] grid,int[][] dp) {
         if(i<0 || i>=grid.length || j<0 || j>=grid[0].length)return (int)1e9;
         if(i==0 && j==0)return grid[i][j];
-        if(dp[i][j] != Integer.MAX_VALUE)return dp[i][j];
+        if(dp[i][j] != (int)1e9)return dp[i][j];
 
         int top = grid[i][j] + func(i-1,j,grid,dp);
         int left = grid[i][j] + func(i,j-1,grid,dp);
