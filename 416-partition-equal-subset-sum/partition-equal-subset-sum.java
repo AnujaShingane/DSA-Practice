903d1fe4a9933d31a92ad8b9439c4022f3a3657f
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-06-04 18:19:56 +0530

    Time: 84 ms (28.16%) | Memory: 72.4 MB (34.61%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,42 +1,30 @@
 class Solution {
     public boolean canPartition(int[] nums) {
         int n = nums.length;
-        int sum = 0;
-        for(int ele : nums){
-            sum+=ele;
-        }
-        if(sum%2!=0)return false;
-        int target = sum/2;
-        boolean[][] dp = new boolean[n][target+1];
+        int sum1 = 0;
 
         for(int i = 0 ; i < n ; i++){
-            dp[i][0] = true;
-        }
-        if(nums[0]<=target)dp[0][nums[0]]=true;
-
-        for(int i = 1 ; i < n ; i++){
-            for(int t = 0 ; t <= target ; t++){
-                boolean take = false;
-                if(nums[i]<=t)take = dp[i-1][t-nums[i]];
-                boolean nottake = dp[i-1][t];
-
-                dp[i][t] = take || nottake;        
-            }
+            sum1 += nums[i];
         }
 
-        return dp[n-1][target];
+        if(sum1%2 != 0)return false;
+
+        int sum = sum1/2;
+
+        Boolean[][] dp = new Boolean[n][sum+1];
+        
+        return func(n-1,nums,sum,dp);
     }
 
-    public boolean func(int ind,int[] nums,int target,Boolean[][] dp) {
-        if(target == 0)return true;
-        if(ind==0)return nums[0]==target;
-
+    static Boolean func(int ind,int[] arr,int target,Boolean[][] dp) {
+        if(target==0)return true;
+        if(ind==0)return target==arr[0];
         if(dp[ind][target]!=null)return dp[ind][target];
-
+        
+        boolean nottake = func(ind-1,arr,target,dp);
         boolean take = false;
-        if(nums[ind]<=target)take = func(ind-1,nums,target-nums[ind],dp);
-        boolean nottake = func(ind-1,nums,target,dp);
-
+        if(target>=arr[ind])take = func(ind-1,arr,target-arr[ind],dp);
+        
         return dp[ind][target] = take || nottake;
     }
 }
