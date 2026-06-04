d4d307a4a2b6ada1bd4555cf036ae38b2a9da262
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-06-04 18:26:57 +0530

    Time: 48 ms (53.77%) | Memory: 47.6 MB (49.86%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -11,20 +11,23 @@ class Solution {
 
         int sum = sum1/2;
 
-        Boolean[][] dp = new Boolean[n][sum+1];
-        
-        return func(n-1,nums,sum,dp);
-    }
+        boolean[][] dp = new boolean[n][sum+1];
 
-    static Boolean func(int ind,int[] arr,int target,Boolean[][] dp) {
-        if(target==0)return true;
-        if(ind==0)return target==arr[0];
-        if(dp[ind][target]!=null)return dp[ind][target];
+        for(int i = 0 ; i < n ; i++){
+            dp[i][0] = true;
+        }
+        if(nums[0] <= sum)dp[0][nums[0]] = true;
+
+        for(int i = 1 ; i < n ; i++){
+            for(int target = 1 ; target <= sum ; target++){
+                boolean nottake = dp[i-1][target];
+                boolean take = false;
+                if(target>=nums[i])take = dp[i-1][target-nums[i]];
+                
+                dp[i][target] = take || nottake;
+            }
+        }
         
-        boolean nottake = func(ind-1,arr,target,dp);
-        boolean take = false;
-        if(target>=arr[ind])take = func(ind-1,arr,target-arr[ind],dp);
-        
-        return dp[ind][target] = take || nottake;
+        return dp[n-1][sum];
     }
 }
