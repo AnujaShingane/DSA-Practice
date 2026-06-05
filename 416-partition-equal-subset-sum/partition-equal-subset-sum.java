52326d2981cadafc288d3135aa8a43ed13ed8d1d
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-06-05 08:25:39 +0530

    Time: 50 ms (46.91%) | Memory: 47.7 MB (45.83%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -2,18 +2,31 @@ class Solution {
     public boolean canPartition(int[] nums) {
         int n = nums.length;
         int sum1 = 0;
-
         for(int i = 0 ; i < n ; i++){
             sum1 += nums[i];
         }
-
         if(sum1%2 != 0)return false;
-
         int sum = sum1/2;
 
-        Boolean[][] dp = new Boolean[n][sum+1];
+        boolean[][] dp = new boolean[n][sum+1];
+
+        for(int i = 0 ; i < n ; i++){
+            dp[i][0] = true;
+        }
+        if(nums[0] <= sum/2)dp[0][nums[0]] = true;
+
+        for(int i = 1 ; i < n ; i++){
+            for(int target = 1 ; target <= sum ; target++){
+                boolean nottake = dp[i-1][target];
+                boolean take = false;
+                if(target>=nums[i])take = dp[i-1][target-nums[i]];
+                
+                dp[i][target] = take || nottake;
+            }
+        }
+
         
-        return func(n-1,nums,sum,dp);
+        return dp[n-1][sum];
     }
 
     static Boolean func(int ind,int[] arr,int target,Boolean[][] dp) {
