b0bd1c1ba014960bdde0177cf2161437e23eed9b
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-06-05 08:02:47 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
@@ -1,25 +1,25 @@
 class Solution {
     public int knapsack(int W, int val[], int wt[]) {
         int n = val.length;
-        int[][] dp = new int[n+1][W+1];
-        
-        for(int w = wt[0] ; w <= W ; w++){
-            dp[0][w] = val[0];
+        int[][] dp = new int[n][W+1];
+        for(int[] row : dp){
+            Arrays.fill(row,-1);
         }
         
-        for(int i = 1 ; i < n ; i++){
-            for(int w = 0 ; w <= W ; w++){
-                int nottake = dp[i-1][w];
-        
-                int take = Integer.MIN_VALUE;
-                if(wt[i]<=w){
-                    take = val[i] + dp[i-1][w-wt[i]];
-                }
-                
-                dp[i][w] = Math.max(take,nottake);
-            }
+        return func(n-1,W,val,wt,dp);
+    }
+    
+    public int func(int ind,int W,int[] val,int[] wt,int[][] dp) {
+        if(ind==0){
+            if(wt[ind]<=W)return val[ind];
+            return 0;
         }
+        if(dp[ind][W]!=-1)return dp[ind][W];
         
-        return dp[n-1][W];
+        int take = (int)(-1e9);
+        if(wt[ind]<=W)take = val[ind] + func(ind-1,W-wt[ind],val,wt,dp);
+        int nottake = func(ind-1,W,val,wt,dp);
+        
+        return dp[ind][W] = Math.max(take,nottake);
     }
 }
