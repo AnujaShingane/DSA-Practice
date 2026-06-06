e5100b4d11de1f838c0eb3111e200ef293cfe30d
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-06-06 22:29:30 +0530

    Time: 21 ms (92.70%) | Memory: 54.1 MB (36.47%) - LeetSync

--- 1170-shortest-common-supersequence/shortest-common-supersequence.java
+++ 1170-shortest-common-supersequence/shortest-common-supersequence.java
@@ -1,57 +1,50 @@
 class Solution {
-    int[][] dp;
     public String shortestCommonSupersequence(String str1, String str2) {
-        int n1 = str1.length();
-        int n2 = str2.length();
-        lcs(str1,str2);
-        StringBuilder sb = new StringBuilder();
+        int n = str1.length();
+        int m = str2.length();
 
-        int i = n1;
-        int j = n2;
-        while(i>0 && j>0){
-            if(str1.charAt(i-1)==str2.charAt(j-1)){
-                sb.append(str1.charAt(i-1));
-                i=i-1;
-                j=j-1;
-            }else{
-                if(dp[i-1][j]>dp[i][j-1]){
-                    sb.append(str1.charAt(i-1));
-                    i=i-1;
-                }else{
-                    sb.append(str2.charAt(j-1));
-                    j=j-1;
-                }
-            }
-        }
-
-        while(i>0){
-            sb.append(str1.charAt(i-1));
-            i=i-1;
-        }
-
-        while(j>0){
-            sb.append(str2.charAt(j-1));
-            j=j-1;
-        }
-
-        return sb.reverse().toString();
-    }
-
-    public int lcs(String s1,String s2) {
-        int n1 = s1.length();
-        int n2 = s2.length();
-        dp = new int[n1+1][n2+1];
-
-        for(int i = 1; i <= n1 ; i++){
-            for(int j = 1 ; j <= n2 ; j++){
-                if(s1.charAt(i-1)==s2.charAt(j-1)){
-                    dp[i][j] = 1 + dp[i-1][j-1];
+        int[][] dp = new int[n+1][m+1];
+        
+        for(int i = 1 ; i <= n ; i++){
+            for(int j = 1 ; j <= m ; j++){
+                if(str1.charAt(i-1)==str2.charAt(j-1)){
+                    dp[i][j] = 1+dp[i-1][j-1];
                 }else{
                     dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
                 }
             }
         }
 
-        return dp[n1][n2];
+        StringBuilder sb = new StringBuilder();
+        int i = n;
+        int j = m;
+
+        while(i>0 && j>0){
+            if(str1.charAt(i-1)==str2.charAt(j-1)){
+                sb.append(str1.charAt(i-1));
+                i--;
+                j--;
+            }else{
+                if(dp[i][j-1]>dp[i-1][j]){
+                    sb.append(str2.charAt(j-1));
+                    j--;
+                }else{
+                    sb.append(str1.charAt(i-1));
+                    i--;
+                }
+            }
+        }
+
+        while(i>0){
+            sb.append(str1.charAt(i-1));
+            i--;
+        }
+
+        while(j>0){
+            sb.append(str2.charAt(j-1));
+            j--;
+        }
+
+        return sb.reverse().toString();
     }
 }
