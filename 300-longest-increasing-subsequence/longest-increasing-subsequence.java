561fd68bff6a0406145865a5ae1aa30eee7256d9
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-06-10 11:42:51 +0530

    Time: 376 ms (11.80%) | Memory: 237 MB (17.38%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,25 +1,18 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
-        int[][] dp = new int[n+1][n+1];
-        
-        for(int i = n-1 ; i >= 0 ; i--){
-            for(int prev = i-1 ; prev >= -1 ; prev--){
-                int nottake = dp[i+1][prev+1];
-                int take = 0;
-                if(prev==-1 || nums[i]>nums[prev]) take = 1 + dp[i+1][i+1];
-
-                dp[i][prev+1] = Math.max(take,nottake);
-            }
+        int[][] dp = new int[n][n+1];
+        for(int[] row : dp){
+            Arrays.fill(row,-1);
         }
 
-        return dp[0][0];
+        return func(0,-1,nums,dp);
     }
 
     public int func(int ind,int prev,int[] nums,int[][] dp) {
         if(ind==nums.length)return 0;
         if(dp[ind][prev+1]!=-1)return dp[ind][prev+1];
-
+        
         int nottake = func(ind+1,prev,nums,dp);
         int take = 0;
         if(prev==-1 || nums[ind]>nums[prev]) take = 1 + func(ind+1,ind,nums,dp);
