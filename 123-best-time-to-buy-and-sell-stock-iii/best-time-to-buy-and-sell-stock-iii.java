5afb8ffe72ace23cb3be5dee974082664e64a69a
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-06-12 09:15:37 +0530

    Time: 199 ms (5.05%) | Memory: 135.8 MB (11.93%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
@@ -1,24 +1,31 @@
 class Solution {
+    int profit = 0;
     public int maxProfit(int[] prices) {
         int n = prices.length;
         int[][][] dp = new int[n][2][3];
-        for(int[][] mat : dp){
-            for(int[] row : mat){
-                Arrays.fill(row,-1);
+        for(int[][] row : dp){
+            for(int[] subrow : row){
+                Arrays.fill(subrow,-1);
             }
         }
 
-        return func(0,1,prices,dp,0);
+        return func(0,1,2,prices,dp);
     }
 
-    public int func(int i , int j , int[] prices,int[][][] dp,int cnt) {
-        if(i==prices.length)return 0;
-        if(dp[i][j][cnt] != -1)return dp[i][j][cnt];
+    public int func(int ind,int buy,int cap,int[] prices,int[][][] dp) {
+        if(ind==prices.length)return 0;
+        if(cap==0)return 0;
+        if(dp[ind][buy][cap]!=-1)return dp[ind][buy][cap];
 
-        int profit = 0;
-    
-        if(j==1 && cnt<2)profit = Math.max((-prices[i]+func(i+1,0,prices,dp,cnt)),(0+func(i+1,1,prices,dp,cnt)));
-        else if(j==0 && cnt<2) profit = Math.max((prices[i]+func(i+1,1,prices,dp,cnt+1)),(0+func(i+1,0,prices,dp,cnt)));
-        return dp[i][j][cnt] = profit;
+        if(buy==1){
+            profit = Math.max(-prices[ind] + func(ind+1,0,cap,prices,dp),
+                                0 + func(ind+1,1,cap,prices,dp));
+        }
+        else{
+            profit = Math.max(prices[ind] + func(ind+1,1,cap-1,prices,dp),
+                                0 + func(ind+1,0,cap,prices,dp));
+        }
+
+        return dp[ind][buy][cap] = profit;
     }
 }
