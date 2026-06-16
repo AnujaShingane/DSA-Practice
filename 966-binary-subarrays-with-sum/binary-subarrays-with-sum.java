e312be6a45fe12e112a54b34c7ee905c2e7d936c
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-06-16 19:42:58 +0530

    Time: 1 ms (100.00%) | Memory: 51.1 MB (39.62%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
@@ -1,22 +1,26 @@
 class Solution {
     public int numSubarraysWithSum(int[] nums, int goal) {
-        return atMost(nums, goal) - atMost(nums, goal - 1);
+        return func(nums,goal) - func(nums,goal-1);
     }
 
-    private int atMost(int[] nums, int k) {
-        if (k < 0) return 0;
+    public int func(int[] nums,int goal) {
+        int l = 0;
+        int r = 0;
+        int sum = 0;
+        int cnt = 0;
+        int n = nums.length;
+        if(goal<0)return 0;
 
-        int left = 0, sum = 0, count = 0;
-
-        for (int right = 0; right < nums.length; right++) {
-            sum += nums[right];
-
-            while (sum > k) {
-                sum -= nums[left++];
+        while(r<n){
+            sum+=nums[r];
+            while(sum>goal){
+                sum-=nums[l];
+                l++;
             }
-
-            count += right - left + 1;
+            if(sum<=goal)cnt+=(r-l+1);
+            r++;
         }
-        return count;
+
+        return cnt;
     }
 }
