38dce124748f4d01ba1700c971620df37a5a3996
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-06-16 20:09:51 +0530

    Time: 13 ms (72.53%) | Memory: 55.2 MB (98.86%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
@@ -1,34 +1,35 @@
 class Solution {
     public int numberOfSubarrays(int[] nums, int k) {
-        return number(nums, k) - number(nums, k-1);
-    }
-
-    public int number(int[] nums, int k) {
         int n = nums.length;
-        int l = 0 , r = 0;
-        int cnt = 0;
-        int sum = 0;
-
-        if(k < 0) return 0;
+        int[] arr = nums.clone();
 
         for(int i = 0 ; i < n ; i++){
-            if(nums[i]%2 == 0)nums[i] = 0;
-            else nums[i] = 1;
+            if(arr[i]%2 == 0){
+                arr[i]=0;
+            }else arr[i] = 1;
         }
 
-        while(r < n){
-            sum += nums[r];
+        return func(arr,k) - func(arr,k-1);
+    }
 
-            while(sum > k){
-                sum -= nums[l];
+    public int func(int[] nums,int goal) {
+        int l = 0;
+        int r = 0;
+        int sum = 0;
+        int cnt = 0;
+        int n = nums.length;
+        if(goal<0)return 0;
+
+        while(r<n){
+            sum+=nums[r];
+            while(sum>goal){
+                sum-=nums[l];
                 l++;
             }
-
-            if(sum <= k){
-                cnt += r-l+1;
-            }
+            if(sum<=goal)cnt+=(r-l+1);
             r++;
         }
+
         return cnt;
     }
 }
