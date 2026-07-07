dfb892dfeaaf7bce5e19a92cf12c87f6075947e7
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-07-07 12:01:41 +0530

    Time: 2 ms (72.66%) | Memory: 47.7 MB (56.49%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -2,39 +2,51 @@ class Solution {
     public void solve(char[][] board) {
         int n = board.length;
         int m = board[0].length;
+        boolean[][] vis = new boolean[n][m];
+
+        for(int i = 0 ; i < n ; i++){
+            if(!vis[i][0] && board[i][0] == 'O'){
+                dfs(i,0,n,m,vis,board);
+            }
+        }
 
         for(int i = 0 ; i < m ; i++){
-            if(board[0][i]=='O')dfs(0,i,n,m,board);
+            if(!vis[0][i] && board[0][i] == 'O'){
+                dfs(0,i,n,m,vis,board);
+            }
         }
 
-        for(int j = 0 ; j < n ; j++){
-            if(board[j][m-1]=='O')dfs(j,m-1,n,m,board);
+        for(int i = 0 ; i < n ; i++){
+            if(!vis[i][m-1] && board[i][m-1] == 'O'){
+                dfs(i,m-1,n,m,vis,board);
+            }
         }
 
-        for(int k = 0 ; k < m ; k++){
-            if(board[n-1][k]=='O')dfs(n-1,k,n,m,board);
-        }
-
-        for(int l = 0 ; l < n ; l++){
-            if(board[l][0]=='O')dfs(l,0,n,m,board);
+        for(int i = 0 ; i < m ; i++){
+            if(!vis[n-1][i] && board[n-1][i] == 'O'){
+                dfs(n-1,i,n,m,vis,board);
+            }
         }
 
         for(int i = 0 ; i < n ; i++){
             for(int j = 0 ; j < m ; j++){
-                if(board[i][j]=='O')board[i][j]='X';
-                if(board[i][j]=='Y')board[i][j]='O';
+                if(board[i][j]=='O'){
+                    board[i][j] = 'X';
+                }
+                if(board[i][j]=='|')board[i][j] = 'O';
             }
         }
     }
 
-    public void dfs(int r,int c,int n,int m,char[][] board){
-        if(r<0 || r>=n || c<0 || c>=m || board[r][c]!='O')return;
+    public void dfs(int row,int col,int n, int m, boolean[][] vis,char[][] board) {
+        if(row >= n || row < 0 || col >= m || col < 0 || vis[row][col] || board[row][col] != 'O') return;
+        
+        vis[row][col] = true;
+        board[row][col] = '|';
 
-        board[r][c] = 'Y';
-
-        dfs(r+1,c,n,m,board);
-        dfs(r-1,c,n,m,board);
-        dfs(r,c+1,n,m,board);
-        dfs(r,c-1,n,m,board);
+        dfs(row+1,col,n,m,vis,board);
+        dfs(row,col-1,n,m,vis,board);
+        dfs(row-1,col,n,m,vis,board);
+        dfs(row,col+1,n,m,vis,board);
     }
 }
