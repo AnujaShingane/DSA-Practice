ef2fc100ba6c04db0fd209cdb9bc41825a835d1e
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-07-08 11:43:51 +0530

    Time: 4 ms (91.84%) | Memory: 47.1 MB (64.78%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,52 +1,46 @@
-class Solution {
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
-        ArrayList<ArrayList<Integer>> adj = adjList(numCourses,prerequisites);
-        boolean cycle = isCycle(numCourses,adj);
-
-        if(!cycle)return true;
-        return false;
-    }
-
-    public boolean isCycle(int n, ArrayList<ArrayList<Integer>> adj) {
-        int[] indegree = new int[n];
-
-        for (int i = 0; i < n; i++) {
-            for (int nei : adj.get(i)) {
-                indegree[nei]++;
-            }
-        }
-
-        Queue<Integer> q = new LinkedList<>();
-        for (int i = 0; i < n; i++) {
-            if (indegree[i] == 0) q.offer(i);
-        }
-
-        int count = 0;
-        while (!q.isEmpty()) {
-            int node = q.poll();
-            count++;
-
-            for (int nei : adj.get(node)) {
-                indegree[nei] -= 1;
-                if (indegree[nei] == 0) {
-                    q.offer(nei);
-                }
-            }
-        }
-
-        return count != n; // cycle if not all processed
-    }
-
-    public ArrayList<ArrayList<Integer>> adjList(int numCourses,int[][] prerequisites){
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        for(int i = 0 ; i < numCourses ; i++){
-            adj.add(new ArrayList<>());
-        }
-
-        for(int[] arr : prerequisites){
-            adj.get(arr[1]).add(arr[0]);
-        }
-
-        return adj;
-    }
+class Solution { 
+    public boolean canFinish(int numCourses, int[][] prerequisites) { 
+        int n = numCourses; 
+        List<List<Integer>> adj = adjList(prerequisites,numCourses); 
+        boolean[] vis = new boolean[n]; 
+        boolean[] pathvis = new boolean[n]; 
+ 
+        for(int i = 0 ; i < n ; i++){ 
+            if(!vis[i]){ 
+                if(cycleDetection(i,vis,pathvis,adj))return false; 
+            } 
+        } 
+ 
+        return true; 
+    } 
+ 
+    public boolean cycleDetection(int node,boolean[] vis,boolean[] 
+pathvis,List<List<Integer>> adj) { 
+        vis[node] = true; 
+        pathvis[node] = true; 
+ 
+        for(int nei : adj.get(node)){ 
+            if(!vis[nei]){ 
+                if(cycleDetection(nei, vis, pathvis, adj)) return true; 
+            }else if(vis[nei] && pathvis[nei]){ 
+                return true; 
+            } 
+        } 
+ 
+        pathvis[node] = false; 
+        return false; 
+    } 
+ 
+    public List<List<Integer>> adjList(int[][] prerequisites,int n){ 
+        List<List<Integer>> adj = new ArrayList<>(); 
+        for(int i = 0 ; i < n ; i++){ 
+            adj.add(new ArrayList<>()); 
+        } 
+ 
+        for(int[] arr : prerequisites){ 
+            adj.get(arr[1]).add(arr[0]); 
+        } 
+ 
+        return adj; 
+    } 
 }
