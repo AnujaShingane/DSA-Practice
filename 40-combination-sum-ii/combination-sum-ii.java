8713aaefbffc5a938b5d7618a976add7094c0a03
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-07-19 12:37:02 +0530

    Time: 7 ms (38.83%) | Memory: 45.2 MB (68.27%) - LeetSync

---
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        int n = candidates.length;
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(candidates);
+
+        func(0,new ArrayList<>(),res,target,candidates);
+
+        return res;
+    }
+
+    public void func(int ind,List<Integer> ds,List<List<Integer>> res,int target, int[] candidates) {
+        if(target==0){
+            res.add(new ArrayList<>(ds));
+            return;
+        }
+
+        if(target<0)return;
+        
+        for(int i = ind ; i < candidates.length ; i++){
+            if(i>ind && candidates[i]==candidates[i-1])continue;
+
+            //take
+            ds.add(candidates[i]);
+            func(i+1,ds,res,target-candidates[i],candidates);
+            ds.remove(ds.size()-1);
+        }
+    }
+}
