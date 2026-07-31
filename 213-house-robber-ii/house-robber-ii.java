a3c7af89bafa748e95deed188e2c2fe01288ace4
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-07-31 12:52:45 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (23.54%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -2,31 +2,31 @@ class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
         if(n==1)return nums[0];
-        ArrayList<Integer> nums1 = new ArrayList<>();
-        ArrayList<Integer> nums2 = new ArrayList<>();
+        ArrayList<Integer> arr1 = new ArrayList<>();
+        ArrayList<Integer> arr2 = new ArrayList<>();
 
-        for(int i = 0 ; i < n ; i++){
-            if(i!=0)nums1.add(nums[i]);
-            if(i!=n-1)nums2.add(nums[i]);
+        for(int i = 0 ; i < n-1 ; i++){
+            arr1.add(nums[i]);
         }
-
-        return Math.max(ans(nums1),ans(nums2));
-    }
-
-    public int ans(ArrayList<Integer> nums) {
-        int n = nums.size();
-        int[] dp = new int[n];
-
-        dp[0] = nums.get(0);
-
         for(int i = 1 ; i < n ; i++){
-            int pick = nums.get(i);
-            if(i>1)pick = nums.get(i) + dp[i-2];
-            int notpick = dp[i-1];
-
-            dp[i] = Math.max(pick,notpick);
+            arr2.add(nums[i]);
         }
 
-        return dp[n-1];
+        int[] dp1 = new int[n];
+		Arrays.fill(dp1,-(int)(1e9));
+        int[] dp2 = new int[n];
+		Arrays.fill(dp2,-(int)(1e9));
+
+        return Math.max(func(arr1.size()-1,arr1,dp1),func(arr2.size()-1,arr2,dp2));
     }
+
+    public static int func(int ind,ArrayList<Integer> nums,int[] dp) {
+		if(ind<0)return 0;
+		if(dp[ind]!=-(int)(1e9))return dp[ind];
+
+		int take = nums.get(ind) + func(ind-2,nums,dp);
+		int nottake = func(ind-1,nums,dp);
+
+		return dp[ind] = Math.max(take,nottake);
+	}
 }
