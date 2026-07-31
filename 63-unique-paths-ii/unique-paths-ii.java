d00d8bfca80ebd788ff926aac96e5b5f2f56e13b
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-08-01 00:30:27 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (65.10%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -2,22 +2,22 @@ class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         int m = obstacleGrid.length;
         int n = obstacleGrid[0].length;
-        int[][] dp = new int[m+1][n+1];
-        for(int[] row : dp){
-            Arrays.fill(row,-1);
+        int[][] dp = new int[m][n];
+        for(int[] arr : dp){
+            Arrays.fill(arr,-1);
         }
 
-        return func(m-1,n-1,dp,m,n,obstacleGrid);
+        return func(m-1,n-1,obstacleGrid,dp);
     }
 
-    public int func(int i,int j,int[][] dp,int m , int n,int[][] obstacleGrid) {
-        if(i<0 || i>=m || j<0 || j>=n || obstacleGrid[i][j] == 1)return 0;
-        if(i==0 && j==0)return 1;
-        if(dp[i][j]!=-1)return dp[i][j];
+    public int func(int row,int col,int[][] obstacleGrid,int[][] dp) {
+        if(row<0 || col<0 || obstacleGrid[row][col]==1)return 0;
+        if(row==0 && col==0)return 1;
+        if(dp[row][col]!=-1)return dp[row][col];
 
-        int left = func(i,j-1,dp,m,n,obstacleGrid);
-        int top = func(i-1,j,dp,m,n,obstacleGrid);
+        int up = func(row-1,col,obstacleGrid,dp);
+        int down = func(row,col-1,obstacleGrid,dp);
 
-        return dp[i][j] = left+top;
+        return dp[row][col] = up+down;
     }
 }
