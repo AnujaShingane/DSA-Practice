1f6a22fb4e39dcfea081d70953d65f46f7a6f1e4
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-08-03 18:56:38 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (21.05%) - LeetSync

---
+++ 2102-find-the-middle-index-in-array/find-the-middle-index-in-array.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int findMiddleIndex(int[] nums) {
+        return pivotIndex(nums);
+    }
+
+    public int pivotIndex(int[] nums) {
+        int n = nums.length;
+        int[] prefix = new int[n+1];
+        prefix[0] = 0;
+
+        for(int i = 1 ; i < n+1 ; i++){
+            prefix[i] = prefix[i-1]+nums[i-1];
+        }
+
+        for(int i = 1 ; i < n+1 ; i++){
+            if(prefix[i-1]==prefix[n]-prefix[i]){
+                return i-1;
+            }
+        }
+
+        return -1;
+    }
+}
