855d95ba57a3c60b3caf2291494a6d701510da63
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-08-04 13:29:20 +0530

    Time: 22 ms (43.93%) | Memory: 52.8 MB (14.23%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
@@ -1,26 +1,21 @@
 class Solution {
     public int numSubarraysWithSum(int[] nums, int goal) {
-        return func(nums,goal) - func(nums,goal-1);
-    }
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
 
-    public int func(int[] nums,int goal) {
-        int l = 0;
-        int r = 0;
         int sum = 0;
-        int cnt = 0;
-        int n = nums.length;
-        if(goal<0)return 0;
+        int count = 0;
 
-        while(r<n){
-            sum+=nums[r];
-            while(sum>goal){
-                sum-=nums[l];
-                l++;
+        for (int num : nums) {
+            sum += num;
+
+            if(map.containsKey(sum-goal)){
+                count += map.get(sum-goal);
             }
-            if(sum<=goal)cnt+=(r-l+1);
-            r++;
+
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
         }
 
-        return cnt;
+        return count;
     }
 }
