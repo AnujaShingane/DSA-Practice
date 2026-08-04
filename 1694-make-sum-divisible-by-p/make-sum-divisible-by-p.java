c1089436ede72065de5d92e7258516584f138572
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-08-04 17:56:58 +0530

    Time: 37 ms (6.54%) | Memory: 98.4 MB (78.27%) - LeetSync

--- 1694-make-sum-divisible-by-p/make-sum-divisible-by-p.java
+++ 1694-make-sum-divisible-by-p/make-sum-divisible-by-p.java
@@ -1,31 +1,31 @@
 class Solution {
     public int minSubarray(int[] nums, int p) {
+        int n = nums.length;
+        HashMap<Integer,Integer> map = new HashMap<>();
+
         long total = 0;
-        for (int num : nums) {
+        for(int num : nums){
             total += num;
         }
-        int rem = (int)(total % p);
-        if (rem == 0) return 0;  // already divisible
+        int rem = (int)(total%p);
 
-        Map<Integer, Integer> map = new HashMap<>();
-        map.put(0, -1); // base case
+        if(rem==0)return 0;
 
+        map.put(0,-1);
+        int len = n;
         long prefix = 0;
-        int ans = nums.length;
 
-        for (int i = 0; i < nums.length; i++) {
-            prefix += nums[i];
-            int curr = (int)(prefix % p);
+        for(int i = 0 ; i < n ; i++){
+            prefix = (prefix+nums[i])%p;
+            int need = (int)((prefix-rem+p)%p);
 
-            // we want a prefix that differs by "rem"
-            int needed = (curr - rem + p) % p;
-            if (map.containsKey(needed)) {
-                ans = Math.min(ans, i - map.get(needed));
+            if(map.containsKey(need)){
+                len = Math.min(len,i-map.get(need));
             }
 
-            map.put(curr, i); // store/overwrite latest index
+            map.put((int)prefix,i);
         }
 
-        return ans == nums.length ? -1 : ans;
+        return (len==n) ? -1 : len;
     }
 }
