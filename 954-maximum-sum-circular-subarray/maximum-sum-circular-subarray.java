3c524c7e756fc5723ea3cf55282b32e4d0e00ac7
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-08-05 15:19:49 +0530

    Time: 6 ms (79.79%) | Memory: 51.2 MB (42.55%) - LeetSync

---
+++ 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int maxSubarraySumCircular(int[] nums) {
+        int one = normalKadane(nums);
+        int two = circularKadane(nums);
+
+        if(two<=0)return one;
+
+        return Math.max(one,two);
+    }
+
+    public int normalKadane(int[] nums){
+        int n = nums.length;
+        int currSum = nums[0];
+        int maxSum = nums[0];
+        
+        for(int i = 1 ; i < n ; i++){
+            currSum = Math.max(nums[i],nums[i]+currSum);
+            maxSum = Math.max(currSum,maxSum);
+        }
+
+        return maxSum;
+    }
+
+    public int circularKadane(int[] nums){
+        int n = nums.length;
+        int currSum = nums[0];
+        int minSum = nums[0];
+
+        int totalSum = 0;
+        for(int num : nums){
+            totalSum+=num;
+        }
+        
+        for(int i = 1 ; i < n ; i++){
+            currSum = Math.min(nums[i],nums[i]+currSum);
+            minSum = Math.min(currSum,minSum);
+        }
+
+        return totalSum-minSum;
+    }
+}
