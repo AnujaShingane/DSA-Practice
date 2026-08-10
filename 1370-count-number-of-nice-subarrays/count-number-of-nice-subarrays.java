3e09f0bf478a153b557e131fc0da794241887911
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-08-10 06:11:12 +0530

    Time: 42 ms (24.28%) | Memory: 61.6 MB (7.48%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
@@ -1,33 +1,25 @@
 class Solution {
     public int numberOfSubarrays(int[] nums, int k) {
         int n = nums.length;
-        int[] arr = nums.clone();
+        int[] arr = new int[n];
 
         for(int i = 0 ; i < n ; i++){
-            if(arr[i]%2 == 0){
-                arr[i]=0;
-            }else arr[i] = 1;
+            arr[i] = nums[i]%2;
         }
 
-        return func(arr,k) - func(arr,k-1);
-    }
-
-    public int func(int[] nums,int goal) {
-        int l = 0;
-        int r = 0;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        map.put(0,1);
         int sum = 0;
         int cnt = 0;
-        int n = nums.length;
-        if(goal<0)return 0;
 
-        while(r<n){
-            sum+=nums[r];
-            while(sum>goal){
-                sum-=nums[l];
-                l++;
+        for(int i = 0 ; i < n ; i++){
+            sum += arr[i];
+
+            if(map.containsKey(sum-k)){
+                cnt += map.get(sum-k);
             }
-            if(sum<=goal)cnt+=(r-l+1);
-            r++;
+
+            map.put(sum,map.getOrDefault(sum,0)+1);
         }
 
         return cnt;
