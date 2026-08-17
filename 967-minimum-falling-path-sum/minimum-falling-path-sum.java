25cf94f0a5dbdd8af2bd4cafbd77d4cc2311ece7
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-08-17 12:17:21 +0530

    Time: 1 ms (99.88%) | Memory: 47.2 MB (46.87%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.java
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.java
@@ -1,23 +1,30 @@
 class Solution {
     public int minFallingPathSum(int[][] matrix) {
         int n = matrix.length;
-        int[][] dp = new int[n][n];
-
-        for (int j = 0; j < n; j++) dp[0][j] = matrix[0][j];
-
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                int up = matrix[i][j] + dp[i - 1][j];
-                int left = matrix[i][j] + (j > 0 ? dp[i - 1][j - 1] : (int)(1e9));
-                int right = matrix[i][j] + (j + 1 < n ? dp[i - 1][j + 1] : (int)(1e9));
-
-                dp[i][j] = Math.min(up, Math.min(left, right));
-            }
-        }
+        int m = matrix[0].length;
+        int[][] dp = new int[n][m];
+        for(int[] arr : dp)Arrays.fill(arr,(int)(1e9));
 
         int ans = Integer.MAX_VALUE;
-        for (int j = 0; j < n; j++) ans = Math.min(ans, dp[n - 1][j]);
+        for(int i = 0 ; i < n ; i++){
+            ans = Math.min(ans,func(0,i,matrix,dp));
+        }
 
         return ans;
+    }
+
+    public int func(int row,int col,int[][] matrix,int[][] dp) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        if(col<0 || col>=m)return (int)(1e9);
+        if(row==n-1)return matrix[row][col];
+        if(dp[row][col]!=(int)(1e9))return dp[row][col];
+        
+        int down = matrix[row][col] + func(row+1,col,matrix,dp);
+        int left =matrix[row][col] + func(row+1,col-1,matrix,dp);
+        int right = matrix[row][col] + func(row+1,col+1,matrix,dp);
+
+        return dp[row][col] = Math.min(down,Math.min(left,right));
     }
 }
