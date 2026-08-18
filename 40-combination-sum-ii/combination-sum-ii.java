21a6ba1f2c5d0f46dea90f445cfb5fa510e02155
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-08-19 03:02:29 +0530

    Time: 7 ms (36.86%) | Memory: 45.2 MB (53.48%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.java
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -1,30 +1,28 @@
 class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        int n = candidates.length;
         Arrays.sort(candidates);
-        List<List<Integer>> res = new ArrayList<>();
 
-        func(0,new ArrayList<>(),res,target,candidates);
+        List<List<Integer>> ans = new ArrayList<>();
 
-        return res;
+        func(0,candidates,target,ans,new ArrayList<>());
+
+        return ans;
     }
 
-    public void func(int ind,List<Integer> ds,List<List<Integer>> res,int target,int[] candidates) {
+    public void func(int ind,int[] candidates,int target,List<List<Integer>> ans,List<Integer> list) {
         if(target==0){
-            res.add(new ArrayList<>(ds));
+            ans.add(new ArrayList<>(list));
             return;
         }
 
         if(target<0)return;
 
         for(int i = ind ; i < candidates.length ; i++){
-            //skip duplicates
             if(i>ind && candidates[i]==candidates[i-1])continue;
 
-            //take
-            ds.add(candidates[i]);
-            func(i+1,ds,res,target-candidates[i],candidates);
-            ds.remove(ds.size()-1);
-        }    
+            list.add(candidates[i]);
+            func(i+1,candidates,target-candidates[i],ans,list);
+            list.remove(list.size()-1);
+        }
     }
 }
