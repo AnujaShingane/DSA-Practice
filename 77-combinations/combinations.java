e3da3ca33b52cc01ca3e694eda692144799c7d65
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-08-19 03:08:49 +0530

    Time: 19 ms (49.68%) | Memory: 97.9 MB (83.31%) - LeetSync

--- 77-combinations/combinations.java
+++ 77-combinations/combinations.java
@@ -1,30 +1,22 @@
 class Solution {
     public List<List<Integer>> combine(int n, int k) {
-        int[] arr = new int[n];
-        for(int i = 1 ; i <= n ; i++){
-            arr[i-1] = i;
-        }
-        List<List<Integer>> res = new ArrayList<>();
+        List<List<Integer>> ans = new ArrayList<>();
 
-        func(0,new ArrayList<>(),res,k,arr);
+        func(1,n,k,ans,new ArrayList<>());
 
-        return res;
+        return ans;
     }
 
-    public void func(int ind,List<Integer> ds,List<List<Integer>> res,int k,int[] arr) {
-        if(k==0){
-            res.add(new ArrayList<>(ds));
+    public void func(int ind,int n,int k,List<List<Integer>> ans,List<Integer> list) {
+        if(list.size()==k){
+            ans.add(new ArrayList<>(list));
             return;
         }
 
-        if(ind==arr.length)return;
-
-        //take
-        ds.add(arr[ind]);
-        func(ind+1,ds,res,k-1,arr);
-        ds.remove(ds.size()-1);
-
-        //nottake
-        func(ind+1,ds,res,k,arr);
+        for(int i = ind ; i <= n ; i++){
+            list.add(i);
+            func(i+1,n,k,ans,list);
+            list.remove(list.size()-1);
+        }
     }
 }
