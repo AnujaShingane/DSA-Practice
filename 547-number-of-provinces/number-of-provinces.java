c92d663b8116732c4d239933a022d0650d73e6ec
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-09-11 00:08:45 +0530

    Time: 2 ms (41.42%) | Memory: 47.4 MB (31.69%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,48 +1,61 @@
+class DisjointSet{
+    int[] rank;
+    int[] parent;
+
+    DisjointSet(int n){
+        rank = new int[n+1];
+        parent = new int[n+1];
+
+        for(int i = 0 ; i <= n ; i++){
+            rank[i]=0;
+            parent[i] = i;
+        }
+    }
+
+    public int findUPar(int node){
+        if(node==parent[node]){
+            return node;
+        }
+
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    public void unionByRank(int u,int v){
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+
+        if(rank[ulp_u]<rank[ulp_v]){
+            parent[ulp_u]=ulp_v;
+        }else if(rank[ulp_u]>rank[ulp_v]){
+            parent[ulp_v]=ulp_u;
+        }else{
+            parent[ulp_u]=ulp_v;
+            rank[ulp_v]+=1;
+        }
+    }
+}
+
 class Solution {
     public int findCircleNum(int[][] isConnected) {
         int n = isConnected.length;
         int m = isConnected[0].length;
-        List<List<Integer>> adj = adjList(isConnected);
-        boolean[] vis = new boolean[n+1];
-        int cnt = 0;
-
-        for(int i = 1 ; i <= n ; i++){
-            if(!vis[i]){
-                cnt++;
-                dfs(i,adj,vis);
-            }
-        }
-
-        return cnt;
-    }
-
-    public void dfs(int node,List<List<Integer>> adj,boolean[] vis) {
-        vis[node]=true;
-
-        for(int nei : adj.get(node)){
-            if(!vis[nei]){
-                dfs(nei,adj,vis);
-            }
-        }
-    }
-
-    public List<List<Integer>> adjList(int[][] isConnected){
-        int n = isConnected.length;
-        int m = isConnected[0].length;
-        List<List<Integer>> adj = new ArrayList<>();
-        for(int i = 0 ; i <= n ; i++){
-            adj.add(new ArrayList<>());
-        }
+        DisjointSet ds = new DisjointSet(n);
 
         for(int i = 0 ; i < n ; i++){
             for(int j = 0 ; j < m ; j++){
-                if(isConnected[i][j]==1){
-                    adj.get(i+1).add(j+1);
-                    adj.get(j+1).add(i+1);
+                if(i!=j && isConnected[i][j]==1){
+                    ds.unionByRank(i+1,j+1);
                 }
             }
         }
 
-        return adj;
+        int cnt = 0;
+        for(int i = 1 ; i <= n ; i++){
+            if(ds.findUPar(i)==i){
+                cnt++;
+            }
+        }
+
+        return cnt;
     }
 }
