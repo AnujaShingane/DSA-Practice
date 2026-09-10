af34416412149b813533da88ff1b66a0d081fdb3
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-09-10 23:54:47 +0530

    Time: 6 ms (5.09%) | Memory: 48.9 MB (21.23%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,71 +1,48 @@
-class DisjointSet {
-
-    int[] size;
-    int[] parent;
-
-    public DisjointSet(int n) {
-
-        size = new int[n];
-        parent = new int[n];
-
-        for (int i = 0; i < n; i++) {
-            size[i] = 1;
-            parent[i] = i;
-        }
-    }
-
-    public int findUPar(int node) {
-
-        if (node == parent[node]) {
-            return node;
-        }
-
-        // Path compression
-        return parent[node] = findUPar(parent[node]);
-    }
-
-    public void unionBySize(int u, int v) {
-
-        int ulp_u = findUPar(u);
-        int ulp_v = findUPar(v);
-
-        if (ulp_u == ulp_v) {
-            return;
-        }
-
-        if (size[ulp_u] < size[ulp_v]) {
-
-            parent[ulp_u] = ulp_v;
-            size[ulp_v] = size[ulp_v] + size[ulp_u];
-
-        } else {
-
-            parent[ulp_v] = ulp_u;
-            size[ulp_u] = size[ulp_u] + size[ulp_v];
-        }
-    }
-}
-
 class Solution {
     public int findCircleNum(int[][] isConnected) {
         int n = isConnected.length;
-        DisjointSet ds = new DisjointSet(n);
-
-        for(int i = 0 ; i < n ; i++){
-            for(int j = 0 ; j < n ; j++){
-                if(isConnected[i][j]==1){
-                    ds.unionBySize(i,j);
-                }
-            }
-        }
-
+        int m = isConnected[0].length;
+        List<List<Integer>> adj = adjList(isConnected);
+        boolean[] vis = new boolean[n+1];
         int cnt = 0;
-        for(int i = 0 ; i < n ; i++){
-            if(ds.findUPar(i)==i){
+
+        for(int i = 1 ; i <= n ; i++){
+            if(!vis[i]){
                 cnt++;
+                dfs(i,adj,vis);
             }
         }
 
         return cnt;
+    }
+
+    public void dfs(int node,List<List<Integer>> adj,boolean[] vis) {
+        vis[node]=true;
+
+        for(int nei : adj.get(node)){
+            if(!vis[nei]){
+                dfs(nei,adj,vis);
+            }
+        }
+    }
+
+    public List<List<Integer>> adjList(int[][] isConnected){
+        int n = isConnected.length;
+        int m = isConnected[0].length;
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i = 0 ; i <= n ; i++){
+            adj.add(new ArrayList<>());
+        }
+
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < m ; j++){
+                if(isConnected[i][j]==1){
+                    adj.get(i+1).add(j+1);
+                    adj.get(j+1).add(i+1);
+                }
+            }
+        }
+
+        return adj;
     }
 }
