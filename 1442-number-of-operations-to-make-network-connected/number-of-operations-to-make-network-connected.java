b55230028e8ba57cb4ee335a3b9cd0cb30385473
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-09-11 00:37:07 +0530

    Time: 7 ms (54.69%) | Memory: 66.5 MB (44.36%) - LeetSync

--- 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.java
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.java
@@ -1,66 +1,62 @@
-import java.util.*;
-
 class DisjointSet{
-  ArrayList<Integer> rank = new ArrayList<>();
-  ArrayList<Integer> parent = new ArrayList<>();
-  
-  DisjointSet(int n){
-    for(int i = 0 ; i <= n ; i++){
-      rank.add(0);
-      parent.add(i);
+    int[] rank;
+    int[] parent;
+
+    DisjointSet(int n){
+        rank = new int[n];
+        parent = new int[n];
+
+        for(int i = 0 ; i < n ; i++){
+            rank[i]=0;
+            parent[i]=i;
+        }
     }
-  }
-  
-  public int findUPar(int node){
-    if(node == parent.get(node))return node;
-    
-    int ulp = findUPar(parent.get(node));
-    parent.set(node,ulp);
-    return parent.get(node);
-  }
-  
-  public void unionByParent(int u , int v){
-    int ulp_u = findUPar(u);
-    int ulp_v = findUPar(v);
-    
-    if(ulp_v==ulp_u)return;
-    
-    if(rank.get(ulp_u)<rank.get(ulp_v)){
-      parent.set(ulp_u,ulp_v);
-    }else if(rank.get(ulp_u)>rank.get(ulp_v)){
-      parent.set(ulp_v,ulp_u);
-    }else{
-      parent.set(ulp_u,ulp_v);
-      rank.set(ulp_u,rank.get(ulp_u)+1);
+
+    public int findUPar(int node){
+        if(node==parent[node]){
+            return node;
+        }
+
+        return parent[node] = findUPar(parent[node]);
     }
-  }
+
+    public void unionByRank(int u,int v){
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+
+        if(rank[ulp_u]<rank[ulp_v]){
+            parent[ulp_u]=ulp_v;
+        }else if(rank[ulp_u]>rank[ulp_v]){
+            parent[ulp_v]=ulp_u;
+        }else{
+            parent[ulp_u]=ulp_v;
+            rank[ulp_v]+=1;
+        }
+    }
 }
 
 class Solution {
     public int makeConnected(int n, int[][] connections) {
         DisjointSet ds = new DisjointSet(n);
-        int cntExtras = 0;
+        int cntExtra = 0;
 
         for(int[] arr : connections){
-            int u = arr[0];
-            int v = arr[1];
-
-            if(ds.findUPar(u)==ds.findUPar(v)){
-                cntExtras++;
+            if(ds.findUPar(arr[0])==ds.findUPar(arr[1])){
+                cntExtra++;
             }else{
-                ds.unionByParent(u,v);
+                ds.unionByRank(arr[0],arr[1]);
             }
         }
 
-        int cntC = 0;
+        int components = 0;
         for(int i = 0 ; i < n ; i++){
             if(ds.findUPar(i)==i){
-                cntC++;
+                components++;
             }
         }
 
-        int ans = cntC-1;
-        if(cntExtras >= ans)return ans;
-        else return -1;
+        int need = components-1;
+        if(need>cntExtra)return -1;
+        return need;
     }
 }
