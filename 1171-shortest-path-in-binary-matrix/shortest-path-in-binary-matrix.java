844c468cc69d06dc51166874501c3528f63d95b8
AnujaShingane <anuja.shingane@cumminscollege.in>
2026-09-11 12:20:39 +0530

    Time: 13 ms (81.30%) | Memory: 47.6 MB (59.41%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
@@ -1,42 +1,45 @@
+class Pair{
+    int[] ij;
+    int dist;
+
+    Pair(int[] ij , int dist){
+        this.ij = ij;
+        this.dist = dist;
+    }
+}
+
 class Solution {
     public int shortestPathBinaryMatrix(int[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
+        Queue<Pair> q = new LinkedList<>();
+        if(grid[0][0]!=0 || grid[n-1][m-1]!=0)return -1;
+        q.offer(new Pair(new int[]{0,0},1));
 
-        if(grid[0][0]==1 || grid[n-1][m-1]==1)return -1;
+        int[] di = {-1, -1, -1,  0, 0, 1, 1, 1};
+        int[] dj = {-1,  0,  1, -1, 1,-1, 0, 1};
+        int ndist = 0;
 
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{0,0});
-        grid[0][0]=1;
-        
-        int[][] dir = {
-            {-1,-1},{-1,0},{-1,1},
-            {0,-1},       {0,1},
-            {1,-1},{1,0},{1,1}
-        };
-
-        int pathlen = 1;
         while(!q.isEmpty()){
-            int size = q.size();
-
-            for(int i = 1 ; i <= size ; i++){
-                int[] arr = q.poll();
-                int r = arr[0];
-                int c = arr[1];
-
-                if(r==n-1 && c==m-1)return pathlen;
-
-                for(int[] d : dir){
-                    int nr = r + d[0];
-                    int nc = c + d[1];
-
-                    if(nr<0 || nc<0 || nr>=n || nc>=m || grid[nr][nc]==1)continue;
-
-                    grid[nr][nc]=1;
-                    q.add(new int[]{nr,nc});
-                }
+            Pair p = q.poll();
+            int[] ij = p.ij;
+            int i = ij[0];
+            int j = ij[1];
+            int dist = p.dist;
+            if (i == n - 1 && j == m - 1) {
+                return dist;
             }
-            pathlen++;
+
+            for(int z = 0 ; z < 8 ; z++){
+                int ni = i+di[z];
+                int nj = j+dj[z];
+                if(ni<0 || ni>=n || nj<0 || nj>=m)continue;
+                if(grid[ni][nj]!=0)continue;
+                ndist = dist+1;
+
+                grid[ni][nj]=ndist;
+                q.offer(new Pair(new int[]{ni,nj},ndist));
+            } 
         }
 
         return -1;
